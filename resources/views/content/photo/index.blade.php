34edc39eef2d2957c209c064e4ffb6759e70cc0b
Kristjan Jansen <kristjan.jansen@gmail.com>
2015-06-29 15:35:15 +0300

    cards

--- resources/views/content/photo/index.blade.php
+++ resources/views/content/photo/index.blade.php
@@ -10,11 +10,30 @@
   
         @foreach ($contents as $index => $content)
 
-            <div class="col-sm-4" style="margin-bottom: 1.5em">
+            <div class="col-sm-4" style="margin-bottom: 1.5em;">
 
                 @include('image.landscape', ['image' => $content->imagePath()])
+                {{--
+                <div class="row">
+                    
+                    <div class="col-xs-2 col-sm-3">
+                        <a href="/user/{{ $content->user->id }}">
+                            @include('image.circle', ['image' => $content->user->imagePath()])
+                        </a>
+                    </div>
 
+                    <div class="col-xs-10 col-sm-9">
+                        {{ $content->title }}
+                        <p>by @include('user.item', ['user' => $content->user])
+                        at {{ $content->created_at->format('d.m.Y') }}:
+                        </p>
+                    </div>
+            
+                </div>
+                --}}
             </div>
+
+
             
             @if (($index + 1) % 3 == 0) </div><div class="row"> @endif
 
