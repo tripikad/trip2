90c9f6fdb57ab6c433b640253bf01e648310deb4
Kristjan Jansen <kristjan.jansen@gmail.com>
2015-06-29 15:23:52 +0300

    cards

--- resources/views/user/image.blade.php
+++
@@ -1,7 +0,0 @@
-@if (! isset($nolink)) <a href="/user/{{ $user->id }}"> @endif
-
-@include('image.circle', [
-    'image' => 'http://trip.ee/files/pictures/' . ($user->image ? $user->image : 'picture_none.png'),
-])
-
-@if (! isset($nolink)) </a> @endif
