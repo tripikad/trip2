2033b2a77fed3fc99ba21a7312f9d6de0f4e406b
kristjanjansen <kristjan.jansen@gmail.com>
2015-07-03 01:13:07 +0300

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -4,7 +4,7 @@ rm public/images/user/small/*
 for i in $(find public/images/user -name "*.jpg" -or -name "*.png" -or -name "*.gif" -type f)
 do
     file=$(echo $i | cut -d'/' -f4)
-    convert $i -resize 200x200 public/images/user/small/$file
+    convert $i -resize 200 public/images/user/small/$file
 
 done
 
@@ -12,7 +12,7 @@ rm public/images/news/small/*
 for i in $(find public/images/news -name "*.jpg" -or -name "*.png" -or -name "*.gif" -type f)
 do
     file=$(echo $i | cut -d'/' -f4)
-    convert $i -resize 400x400 public/images/news/small/$file
+    convert $i -resize 350 public/images/news/small/$file
 
 done
 
@@ -20,7 +20,7 @@ rm public/images/offer/small/*
 for i in $(find public/images/offer -name "*.jpg" -or -name "*.png" -or -name "*.gif" -type f)
 do
     file=$(echo $i | cut -d'/' -f4)
-    convert $i -resize 400x400 public/images/offer/small/$file
+    convert $i -resize 350 public/images/offer/small/$file
 
 done
 
@@ -28,6 +28,6 @@ rm public/images/photo/small/*
 for i in $(find public/images/photo -name "*.jpg" -or -name "*.png" -or -name "*.gif" -type f)
 do
     file=$(echo $i | cut -d'/' -f4)
-    convert $i -resize 400x400 public/images/photo/small/$file
+    convert $i -resize 350 public/images/photo/small/$file
 
 done
