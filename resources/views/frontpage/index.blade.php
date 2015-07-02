1473904ee9a9a1a97ab64f99f232d612caf791d8
kristjanjansen <kristjan.jansen@gmail.com>
2015-07-03 01:10:03 +0300

    Update index.blade.php

--- resources/views/frontpage/index.blade.php
+++ resources/views/frontpage/index.blade.php
@@ -6,11 +6,11 @@
 
 @foreach($fronts as $type => $front) 
 
-    <div style="margin: 30px 0 0 0;">
+    @include('component.placeholder', [
+        'text' => $front['title'],
+        'height' => 60
+    ])
 
-        @include('component.placeholder', ['text' => $front['title']])
-
-    </div>
 
     @include("content.$type.front", [
         'title' => $front['title'],
