0b55e551f40b23bd9a76c8ac92b82ab199d3430d
Kristjan Jansen <kristjan.jansen@gmail.com>
2015-07-07 00:13:36 +0300

    ad placeholder

---
+++ resources/views/component/ad.blade.php
@@ -0,0 +1,19 @@
+<div style="
+    padding-bottom: 2%;
+    width: 100%;
+    background-color: {{ ['#3C1642', '#086788', '#C62E65', '#F28123', '#D34E24'][rand(0,4)] }};
+">
+
+    <h3 style="
+        font-weight: normal;
+        font-family: sans-serif;
+        opacity: 0.8;
+        text-align: center;
+        margin: 0;
+        padding-top: 3.5%;
+        color: white;
+    ">
+    {{ $text or 'Sample Ad' }}
+    <h3>
+
+</div>
