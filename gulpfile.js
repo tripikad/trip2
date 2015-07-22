9d08f595e47ea90e07c242dfe3ef6f5cd521c132
Kristjan Jansen <kristjan.jansen@gmail.com>
2015-07-22 17:11:53 +0300

    latest

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,26 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 
-gulp.task('css',function() {
+gulp.task('sass', function() {
 
     gulp.src([
-        './node_modules/selectize/dist/css/selectize.bootstrap3.css'
+        './resources/assets/sass/variables_bootstrap.scss',
+        './node_modules/bootstrap-sass/assets/stylesheets/bootstrap/_variables.scss',
+        './resources/assets/sass/variables.scss',
+        './node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss',
+
+        './node_modules/selectize/dist/css/selectize.bootstrap3.css',
+
+        './resources/assets/sass/**/*.scss',
     ])
+    .pipe(concat('main.scss'))
+    .pipe(sass({includePaths: [
+        './node_modules/bootstrap-sass/assets/stylesheets',
+    ]
+    }))
     .pipe(gulp.dest('./public/css'));
 
 });
-
-gulp.task('sass', function () {
-    gulp.src([
-        './resources/assets/sass/main.scss',
-    ])
-    .pipe(sass())
-    .pipe(gulp.dest('./public/css'));
-
-});
-
 
 gulp.task('js', function() {
  
@@ -42,4 +44,4 @@ gulp.task('watch', function () {
 
 });
 
-gulp.task('default', ['css', 'sass', 'js']);
+gulp.task('default', ['sass', 'js']);
