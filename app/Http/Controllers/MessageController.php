54394b6c3dee1ef18c141be70b2552e07db034f2
Kristjan Jansen <kristjan.jansen@gmail.com>
2015-08-22 12:02:24 +0300

    message mailer, closes #22

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Mail;
+
+use App\Message;
+use App\User;
+
 class MessageController extends Controller
 {
 
@@ -15,8 +20,29 @@ class MessageController extends Controller
 
         $fields = ['user_id_from' => $user_id_from, 'user_id_to' => $user_id_to];
 
-        $message = \App\Message::create(array_merge($request->all(), $fields));
+        $message = Message::create(array_merge($request->all(), $fields));
         
+        $user_to = User::find($user_id_to);
+
+        if ($user_to->notify_message) {
+
+            $user_from = User::find($user_id_from);
+
+            Mail::send('email.message.store', [
+                'new_message' => $message, // 'message' variable is reseved by mailer
+                'user_from' => $user_from,
+                'user_to' => $user_to
+            ], function ($mail) use ($user_from, $user_to) {
+            
+                $mail->to($user_to->email)
+                    ->subject(trans('message.store.email.subject', [
+                        'user' => $user_from->name
+                    ]));
+        
+            });
+
+        }
+
         return redirect()->route('user.show.messages.with', [
             $user_id_from, $user_id_to, '#message-' . $message->id
         ]);
--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -5,4 +5,7 @@ return [
     'create.field.body.title' => 'Message',
     'create.submit.title' => 'Send private message',
 
+    'store.email.subject' => ':user has sent you a private message',
+    'store.email.body' => 'See the full message here :url',
+
 ];
