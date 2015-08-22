cc04dd3325a9bb487316db39b7250a07b570e81d
kkallasm <kaspar68@gmail.com>
2015-08-22 12:21:41 +0300

    Add config for menus #26

---
+++ resources/lang/en/menu.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'header.news' => 'News',
+    'header.flights' => 'Flights',
+    'header.travelmates' => 'Travelmates',
+    'header.forum' => 'Forum',
+    'header.photos' => 'Photos',
+    'header.blogs' => 'Blogs',
+    'header.offers' => 'Offers',
+    'header.internal' => '...',
+
+    'footer.trip_ee' => 'What is Trip.ee',
+    'footer.contact' => 'Contact',
+    'footer.eula' => 'End-user license agreement',
+    'footer.advertising' => 'Advertising',
+    'footer.facebook' => 'Facebook',
+    'footer.twitter' => 'Twitter',
+
+    ];
--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -1,1 +1,25 @@
-@include('component.placeholder', ['text' => 'Footer', 'height' => 150])
+<ul class="list-inline">
+
+	@foreach (config('menu.footer') as $key => $data)
+            
+    <li>
+       		
+    		<a href="{{ $data['url'] }}"
+
+    			@if (isset($data['external']) && $data['external'])
+
+    				target="_blank"
+
+    			@endif
+
+    		>
+
+    			{{ trans("menu.footer.$key") }}
+
+    		</a>
+    	
+    </li>
+            
+    @endforeach
+
+</ul>
