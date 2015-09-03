4791535b59d46bab2f18d6e8a8da7dba343e646d
kristjanjansen <kristjan.jansen@gmail.com>
2015-09-03 08:02:08 +0300

    cleaning up authtest

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -14,31 +14,31 @@ class AuthTest extends TestCase
     {
 
         $this->visit('/register')
-             ->type('TestKasutaja', 'name')
-             ->type('TestKasutaja@test.ee', 'email')
-             ->type('salasona', 'password')
-             ->type('salasona', 'password_confirmation')
+             ->type('testuser', 'name')
+             ->type('testuser@example.com', 'email')
+             ->type('password', 'password')
+             ->type('password', 'password_confirmation')
              ->check('eula')
              ->press('Register')
              ->seePageIs('/');
 
         $this->see(trans('auth.register.sent.status'))
-             ->seeInDatabase('users', ['name' => 'TestKasutaja', 'verified' => 0]);
-
+             ->seeInDatabase('users', ['name' => 'testuser', 'verified' => 0]);
 
         //name already taken
+
         $this->visit('/register')
-             ->type('TestKasutaja', 'name')
-             ->type('TestKasutaja@test.ee', 'email')
-             ->type('salasona', 'password')
-             ->type('salasona', 'password_confirmation')
+             ->type('testuser', 'name')
+             ->type('testuser@example.com', 'email')
+             ->type('password', 'password')
+             ->type('password', 'password_confirmation')
              ->check('eula')
              ->press('Register')
              ->seePageIs('/register')
              ->see(trans('validation.unique',['attribute' => 'name']));
 
 
-        $user = User::whereName('TestKasutaja')->first();
+        $user = User::whereName('testuser')->first();
 
 /*
         // You can't login until you confirm your email address.
@@ -50,12 +50,11 @@ class AuthTest extends TestCase
              ->seePageIs('/login')
              ->see(trans('login.failed.status'));
              //->see('Failed to log you in');
- */       
 
-/*
         $this->visit("register/confirm/{$user->registration_token}")
              ->seeInDatabase('users', ['name' => 'TestKasutaja', 'verified' => 1]); 
 */
+
     }
 
 }
