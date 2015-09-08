e666380a9fc56a598d0e21f67f1dc4d79dc06fee
kristjanjansen <kristjan.jansen@gmail.com>
2015-09-08 16:33:01 +0300

    updates

---
+++ resources/views/component/comment/text.blade.php
@@ -0,0 +1,22 @@
+<ul class="list-inline">
+
+    <li>
+    
+        {!! view('component.user.link', ['user' => $comment->user]) !!}
+
+    </li>
+
+    <li>
+
+        <a
+            class="permalink"
+            href="#comment-{{ $comment->id }}"
+            alt="{{ $comment->created_at }}"
+        >
+            {{ $comment->created_at->diffForHumans() }}
+
+        </a>
+
+    </li>
+
+</ul>
