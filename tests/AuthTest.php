ad24f98ad03c92edcd927e8d6c6b4a3740648301
kristjanjansen <kristjan.jansen@gmail.com>
2015-09-14 08:31:06 +0300

    working some more on auth tests

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -24,6 +24,19 @@ class AuthTest extends TestCase
             ->see(trans('auth.register.sent.info'))
             ->seeInDatabase('users', ['name' => 'user', 'verified' => 0]);
 
+        $this->visit('/')
+            ->click(trans('menu.header.login'))
+            ->type('user', 'name')
+            ->type('user', 'password')
+            ->press(trans('auth.login.submit.title'))
+            ->seePageIs('/login')
+            ->see(trans('auth.login.failed.info'));
+
+        $this->visit($this->getVerificationLink('user'))
+            ->seePageIs('login')
+            ->see(trans('auth.register.confirmed.info'));
+
+
     }
 
     public function test_user_can_login()
@@ -44,6 +57,13 @@ class AuthTest extends TestCase
             ->see(trans('auth.login.login.info'))
             ->see(trans('menu.header.logout'));
 
+    }
+
+    public function getVerificationLink($name) {
+
+        $token = User::whereName($name)->first()->registration_token;
+
+        return '/register/confirm/' . $token;
     }
 
 
