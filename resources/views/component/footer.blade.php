e43715fc27dd3185b2723957de432c0a81716462
mikkpokk <mikk@nicorex.ee>
2015-09-20 17:35:10 +0300

    Add © and year to the footer #182

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -1,5 +1,6 @@
 <div class="component-footer">
 
+	@if (config('menu.footer'))
     <ul class="list-inline">
 
     	@foreach (config('menu.footer') as $key => $data)
@@ -25,5 +26,16 @@
         @endforeach
 
     </ul>
+	@endif
 
+	<hr>
+	<div class="footer">
+		<p>
+			@if(strstr(trans('content.footer.copyright'), '[current_year]'))
+				{{ @str_replace("[current_year]", \Carbon\Carbon::now()->format('Y'), trans("content.footer.copyright")) }}
+			@else
+				{{ trans('content.footer.copyright') }}
+			@endif
+		</p>
+	</div>
 </div>
