8e2739cf7cdd5ec674752498d3687a7b394ee3de
mikkpokk <mikk@nicorex.ee>
2015-09-21 16:30:12 +0300

    1tab = 4spaces

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -1,24 +1,24 @@
 <div class="component-footer">
-	<div class="menu utils-border-bottom">
-		<ul class="list-inline">
+    <div class="menu utils-border-bottom">
+        <ul class="list-inline">
 
-			@foreach (config('menu.footer') as $key => $data)
+            @foreach (config('menu.footer') as $key => $data)
 
-			<li>
-					<a href="{{ $data['url'] }}"
-						@if (isset($data['external']) && $data['external'])
-							target="_blank"
-						@endif
-					>
-						{{ trans("menu.footer.$key") }}
-					</a>
-			</li>
+            <li>
+                <a href="{{ $data['url'] }}"
+                    @if (isset($data['external']) && $data['external'])
+                        target="_blank"
+                    @endif
+                >
+                    {{ trans("menu.footer.$key") }}
+                </a>
+            </li>
 
-			@endforeach
+            @endforeach
 
-		</ul>
-	</div>
-	<div class="copyright">
-		{{ trans('content.footer.copyright', ['current_year' =>  \Carbon\Carbon::now()->year]) }}
-	</div>
+        </ul>
+    </div>
+    <div class="copyright">
+        {{ trans('content.footer.copyright', ['current_year' =>  \Carbon\Carbon::now()->year]) }}
+    </div>
 </div>
