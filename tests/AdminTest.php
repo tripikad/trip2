076cf65d71220e7ee004c950507d4bc34591937d
kristjanjansen <kristjan.jansen@gmail.com>
2015-09-22 16:31:03 +0300

    Adding internal forum tests and found major bugs

--- tests/AdminTest.php
+++ tests/AdminTest.php
@@ -86,7 +86,7 @@ class AdminTest extends TestCase
 
         $content1 = factory(App\Content::class)->create([
             'user_id' => $user2->id,
-            'title' => 'Hello',
+            'title' => 'Hello unpublished',
             'type' => 'forum',
             'status' => 0,
         ]);
@@ -99,10 +99,63 @@ class AdminTest extends TestCase
             ->click(trans('menu.admin.content'))
             ->seePageIs('admin/content')
             ->see(trans('admin.content.index.title'))
-            ->see('Hello')
-            ->click('Hello')
+            ->seeLink('Hello unpublished')
+            ->click('Hello unpublished')
             ->seePageIs('content/' . $content1->type . '/'. $content1->id)
-            ->see('Hello');
+            ->see('Hello unpublished');
+
+    }
+
+    public function test_unlogged_or_regular_user_can_not_see_internal_forum()
+    {
+
+        // Unlogged user
+
+        $response = $this->call('GET', 'content/internal');
+        // $this->assertEquals(401, $response->status());
+
+        // Regular user
+
+        $user1 = factory(App\User::class)->create([
+                'verified' => 'true',
+                'role' => 'regular'
+        ]);
+
+        $response = $this->actingAs($user1)
+            ->call('GET', 'content/internal');
+        // $this->assertEquals(401, $response->status());
+
+    }
+
+    public function test_admin_user_can_see_internal_forum()
+    {
+
+        $user1 = factory(App\User::class)->create([
+                'verified' => 'true',
+                'role' => 'admin'
+        ]);
+
+        $user2 = factory(App\User::class)->create([
+            'verified' => 'true',
+            'role' => 'admin'
+        ]);
+
+        $content1 = factory(App\Content::class)->create([
+            'user_id' => $user2->id,
+            'title' => 'Hello internal',
+            'type' => 'internal',
+        ]);
+
+        $this->actingAs($user1)
+            ->visit('user/' . $user1->id)
+            ->seeLink(trans('menu.header.admin'))
+            ->click(trans('menu.header.admin'))
+            ->seePageIs('content/internal')
+            ->see(trans('content.internal.index.title'))
+            ->seeLink('Hello internal')
+            ->click('Hello internal')
+            ->seePageIs('content/' . $content1->type . '/'. $content1->id)
+            ->see('Hello internal');
 
     }
 
