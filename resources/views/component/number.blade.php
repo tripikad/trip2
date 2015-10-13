df731cbbaaa829a1bcf9f7cf98be6a0e009db5ae
kristjanjansen <kristjan.jansen@gmail.com>
2015-10-13 16:41:13 +0300

    removing number for now

--- resources/views/component/number.blade.php
+++ resources/views/component/number.blade.php
@@ -1,14 +1,3 @@
-{{--
-
-Any properties can be combined. Numbers fill proportionally their container width
-
-@include('component.number', [
-    'number' => '1',
-    'options' => '-neutral'
-])
-
---}}
-
 <div class="component-number {{ $options or ''}}">
     
     <div class="content">
