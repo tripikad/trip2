7307be3c8301745ed29793298abb326871eb01a1
kristjanjansen <kristjan.jansen@gmail.com>
2015-10-14 11:39:48 +0300

    remove leftovers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Database\Schema\Blueprint;
-
-use App\Content;
-use App\Destination;
 
 class AppServiceProvider extends ServiceProvider
 {
