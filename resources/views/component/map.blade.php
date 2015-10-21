a57321c1779d056f0ee0aeeb1927840063c7689c
kristjanjansen <kristjan.jansen@gmail.com>
2015-10-21 19:27:36 +0300

    fixing map component

--- resources/views/component/map.blade.php
+++ resources/views/component/map.blade.php
@@ -1,4 +1,20 @@
+{{--
+
+description: Background map
+
+code: |
+
+    @include('component.map', [
+        'map_top' => 0,
+        'map_left' => 0
+    ])
+
+--}}
+
 <div class="c-map {{ $modifiers or '' }}">
+    
     <div class="c-map__location" style="top: {{ $map_top }}; left: {{ $map_left }};"></div>
-    <?php // echo file_get_contents('images/dummy/map.svg'); ?>
+    
+    <img src="/svg/map.svg" />
+
 </div>
