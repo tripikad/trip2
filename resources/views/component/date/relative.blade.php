4e4ad8953c36d470dca6a937b99b5868fe300320
Mikk Pokk <mikk@nicorex.ee>
2015-10-22 04:34:45 +0300

    Revert "Date components temporary validation and faker"

--- resources/views/component/date/relative.blade.php
+++ resources/views/component/date/relative.blade.php
@@ -3,12 +3,8 @@
 description: Date.relative component.  Use date component to print out date or time. For example, date.relative prints out X days ago, X seconds ago, X months ago etc.
 
 code: |
-    @include('component.date.relative', ['date' => \Carbon\Carbon::now()])
+    @include('component.date.relative', ['date' => \Carbon\Carbon::now()]);
 
 --}}
-
-@if((bool)strtotime($date)===false)
-    {{ $date = date("Y-m-d H:i:s", rand( strtotime('2015-01-01 00:00:00'), strtotime(date('Y-m-d H:i:s')) )) }}
-@endif;
 
 @if(isset($date)) {{ Date::parse($date)->diffForHumans() }} @endif
