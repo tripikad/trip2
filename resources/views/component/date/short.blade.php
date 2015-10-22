795bfe485588d805a06b63802a71d510d93ac661
mikkpokk <mikk@nicorex.ee>
2015-10-22 04:15:52 +0300

    Date components temporary validation and faker
    It's easier to remove these temporary validations and fakers from 3
    known files than do it all over again after front-end development.

--- resources/views/component/date/short.blade.php
+++ resources/views/component/date/short.blade.php
@@ -3,8 +3,12 @@
 description: Date.short component. Use date component to print out date or time. Date.short - d. m Y
 
 code: |
-    @include('component.date.short', ['date' => \Carbon\Carbon::now()]);
+    @include('component.date.short', ['date' => \Carbon\Carbon::now()])
 
 --}}
+
+@if((bool)strtotime($date)===false)
+    {{ $date = date("Y-m-d H:i:s", rand( strtotime('2015-01-01 00:00:00'), strtotime(date('Y-m-d H:i:s')) )) }}
+@endif;
 
 @if(isset($date)) {{ $date->format('d. m Y') }}  @endif
