2429c52ca574050136eb41d4ed665fadb7a4f548
Allar Pani <allar@haiku.ee>
2015-10-22 15:35:39 +0300

    Revert "Removed ad component"
    This reverts commit 6b63b331472691d37a98d70dd1d0166840fd2b32.

---
+++ resources/views/component/ad.blade.php
@@ -0,0 +1,23 @@
+<div
+    class="component-ad {{ $options or ''}}"
+    style="
+        background-color: {{ [
+            '#3C1642',
+            '#086788',
+            '#C62E65',
+            '#F28123',
+            '#D34E24',
+            '#1E3231',
+            '#485665',
+            '#32A287',
+            '#D36135',
+            '#A24936',
+        ][rand(0,9)] }};
+">
+    <div class="content">
+
+        <h3 class="title">{{ $title }}</h3>
+
+    </div>
+
+</div>
