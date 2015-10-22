511a838fcf8669de98d05a82474769911c0cbc44
Allar Pani <allar@haiku.ee>
2015-10-22 15:34:22 +0300

    Revert "Removed number component"
    This reverts commit 0a3ab957001ad1d7403707b0f61357c5a047852b.

---
+++ resources/views/component/number.blade.php
@@ -0,0 +1,13 @@
+<div class="component-number {{ $options or ''}}">
+    
+    <div class="content">
+        
+        <div class="number">
+
+            {{ $number or '0'}}
+    
+        </div>
+    
+    </div>
+    
+</div>
