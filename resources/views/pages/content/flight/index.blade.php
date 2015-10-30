054ad10d02388b7623e66532d4e1d275693bd5f3
Allar Pani <allar@haiku.ee>
2015-10-30 15:24:21 +0200

    Update

--- resources/views/pages/content/flight/index.blade.php
+++ resources/views/pages/content/flight/index.blade.php
@@ -25,6 +25,18 @@
 
 <div class="r-flights">
 
+    <div class="r-flights__map">
+
+        <div class="r-flights__map-inner">
+
+            @include('component.map', [
+                'modifiers' => 'm-flights'
+            ])
+
+        </div>
+
+    </div>
+
     <div class="r-flights__masthead">
 
         @include('component.masthead')
