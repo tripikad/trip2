5298f4b97d78c7d2a6ae427706107b0407e4163b
mikkpokk <mikk@nicorex.ee>
2015-11-24 00:53:06 +0200

    StyleCI

--- app/Image.php
+++ app/Image.php
@@ -23,7 +23,7 @@ class Image extends Model
             .'/'
             .$this->filename;
 
-        if (! file_exists(public_path() . $file)) {
+        if (! file_exists(public_path().$file)) {
             $file = '';
         }
 
