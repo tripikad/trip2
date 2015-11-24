f52e637de04d24633717ac6e405343eb0e03718f
mikkpokk <mikk@nicorex.ee>
2015-11-24 23:10:05 +0200

    unset route exception

--- resources/views/component/user/extra.blade.php
+++ resources/views/component/user/extra.blade.php
@@ -29,9 +29,13 @@ code: |
 
     <li class="c-user-extra__item">
 
-        @if(isset($item['route']))
+        @if(isset($item['route']) && $item['route']!='')
 
-        <a href="{{ $item['route'] }}" class="c-user-extra__item-link">
+            <a href="{{ $item['route'] }}" class="c-user-extra__item-link">
+
+        @else
+
+            <span class="c-user-extra__item-link">
 
         @endif
 
@@ -70,9 +74,13 @@ code: |
 
         @endif
 
-        @if(isset($item['route']))
+        @if(isset($item['route']) && $item['route']!='')
 
-        </a>
+            </a>
+
+        @else
+
+            </span>
 
         @endif
 
