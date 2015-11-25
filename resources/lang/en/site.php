65d6fed04effcaec96d60c669e082c8dd12533eb
mikkpokk <mikk@nicorex.ee>
2015-11-25 20:42:26 +0200

    latest posts translation added

--- resources/lang/en/site.php
+++ resources/lang/en/site.php
@@ -4,6 +4,8 @@
 
         'link.read.more' => 'Read more',
 
+        'content.latest.posts' => 'Latest posts',
+
         'dropzone.default' => 'Drop files here to upload',
         'dropzone.fallback.message' => 'Your browser does not support drag and drop file uploads.',
         'dropzone.fallback.text' => 'Please use the fallback form below to upload your files like in the olden days.',
