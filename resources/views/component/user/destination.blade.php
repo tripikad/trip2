eea142c910ba9e067f0024e132e2910ad3fe0e85
mikkpokk <mikk@nicorex.ee>
2015-11-26 20:56:56 +0200

    hack "take" removed

--- resources/views/component/user/destination.blade.php
+++ resources/views/component/user/destination.blade.php
@@ -1,6 +1,6 @@
 @foreach ($destinations as $key => $destination)
 
-    @if (isset($destination->flaggable) && ((isset($take) && --$take && $take>=-1) || ! isset($take)))
+    @if (isset($destination->flaggable))
 
         <span class="c-user-destination__item">
             <a href="{{ route('destination.show', [$destination->flaggable]) }}" class="c-user-destination__item-link {{ $modifiers or '' }}">
