586f9488dbbc9c5a81cdbb048ef6f2538594d853
mikkpokk <mikk@nicorex.ee>
2015-11-28 01:55:37 +0200

    New definations added

--- resources/lang/en/destination.php
+++ resources/lang/en/destination.php
@@ -2,6 +2,11 @@
 
 return [
 
+    'show.good.offer' => 'Good offers',
+    'show.link.view.all' => 'View all offers &rsaquo;',
+    'show.user.havebeen.title' => 'How many Trip.ee users have been here?',
+    'show.user.wantstogo.title' => 'How many Trip.ee users want to go here?',
+
     'index.user.havebeen.title' => ':count have been here',
     'index.user.wantstogo.title' => ':count want to go here',
 
