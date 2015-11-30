4b0c26cfdf002a58364ff6d8f33248ab2ea75160
mikkpokk <mikk@nicorex.ee>
2015-11-30 02:37:31 +0200

    New futures - view & remove destinations

--- resources/views/pages/user/destinations.blade.php
+++ resources/views/pages/user/destinations.blade.php
@@ -6,65 +6,95 @@
 
 @section('content.one')
 
-    {!! Form::model(null, [
-        'url' => route('user.destination.store', [$user]),
-        'method' => 'post'
-    ]) !!}
+    <div id="link-have-been" class="c-user-destination">
 
-    <div class="c-columns m-2-cols m-space">
+        @include('component.row', [
+            'title' => trans('user.destinations.have.been.title'),
+            'text' => view('component.user.destination.destination', [
+                'destinations' => $user->destinationHaveBeen()->sortByDesc('id'),
+                'title' => trans('user.destinations.remove.title'),
+                'modifiers' => 'js-ajax_get'
+            ])
+        ])
 
-        <div class="c-columns__item">
+    </div>
 
-            <div class="c-form__group">
+    <div id="link-want-to-go" class="c-user-destination">
 
-                {!! Form::select(
-                    'have_been[]',
-                    $have_been_destinations,
-                    $have_been_destination,
-                    [
-                        'multiple' => 'true',
-                        'class' => 'js-select',
-                        'placeholder' => trans('user.destinations.have.been.title'),
-                    ]
-                )!!}
+        @include('component.row', [
+            'title' => trans('user.destinations.want.to.go.title'),
+            'text' => view('component.user.destination.destination', [
+                'destinations' => $user->destinationWantsToGo()->sortByDesc('id'),
+                'title' => trans('user.destinations.remove.title'),
+                'modifiers' => 'js-ajax_get'
+            ])
+        ])
+
+    </div>
+
+    <div id="link-add-destinations" class="c-user-destination">
+
+        {!! Form::model(null, [
+            'url' => route('user.destination.store', [$user]),
+            'method' => 'post'
+        ]) !!}
+
+        <div class="c-columns m-2-cols m-space">
+
+            <div class="c-columns__item">
+
+                <div class="c-form__group">
+
+                    {!! Form::select(
+                        'have_been[]',
+                        $have_been_destinations,
+                        $have_been_destination,
+                        [
+                            'multiple' => 'true',
+                            'class' => 'js-select',
+                            'placeholder' => trans('user.destinations.have.been.title'),
+                        ]
+                    )!!}
+
+                </div>
+
+            </div>
+
+            <div class="c-columns__item">
+
+                <div class="c-form__group">
+
+                    {!! Form::select(
+                        'want_to_go[]',
+                        $want_to_go_destinations,
+                        $want_to_go_destination,
+                        [
+                            'multiple' => 'true',
+                            'class' => 'js-select',
+                            'placeholder' => trans('user.destinations.want.to.go.title'),
+                        ]
+                    )!!}
+
+                </div>
 
             </div>
 
         </div>
 
-        <div class="c-columns__item">
+        <div class="c-form__input-wrap">
 
             <div class="c-form__group">
 
-                {!! Form::select(
-                    'want_to_go[]',
-                    $want_to_go_destinations,
-                    $want_to_go_destination,
-                    [
-                        'multiple' => 'true',
-                        'class' => 'js-select',
-                        'placeholder' => trans('user.destinations.want.to.go.title'),
-                    ]
-                )!!}
+                {!! Form::submit(trans('user.destinations.submit.title'), [
+                    'class' => 'c-button m-large m-block'
+                ]) !!}
 
             </div>
 
         </div>
 
-    </div>
-
-    <div class="c-form__input-wrap">
-
-        <div class="c-form__group">
-
-            {!! Form::submit(trans('user.destinations.submit.title'), [
-                'class' => 'c-button m-large m-block'
-            ]) !!}
-
-        </div>
+        {!! Form::close() !!}
 
     </div>
-
-    {!! Form::close() !!}
 
 @stop
