123878a0790b8eb502c49cfde24917681e4caf68
mikkpokk <mikk@nicorex.ee>
2015-12-01 18:20:24 +0200

    Hound camel case

--- resources/assets/js/components/ckeditor.js
+++ resources/assets/js/components/ckeditor.js
@@ -3,8 +3,8 @@
 window.CKEDITOR_BASEPATH = '/plugins/ckeditor/';
 
 var selector = $('.js-ckeditor'),
-    ckeditor_script = CKEDITOR_BASEPATH + 'ckeditor.js',
-    ckeditor_adapter = CKEDITOR_BASEPATH + 'adapters/jquery.js';
+    ckeditor_script = window.CKEDITOR_BASEPATH + 'ckeditor.js',
+    ckeditor_adapter = window.CKEDITOR_BASEPATH + 'adapters/jquery.js';
 
 if (selector.length > 0) {
     $.getScript(ckeditor_script, function() {
