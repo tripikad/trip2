761081ce858836aa4e475b4d5d4ad9850c30c3eb
mikkpokk <mikk@nicorex.ee>
2015-12-01 18:20:48 +0200

    Variables naming

--- resources/assets/js/components/ckeditor.js
+++ resources/assets/js/components/ckeditor.js
@@ -3,13 +3,13 @@
 window.CKEDITOR_BASEPATH = '/plugins/ckeditor/';
 
 var selector = $('.js-ckeditor'),
-    ckeditor_script = window.CKEDITOR_BASEPATH + 'ckeditor.js',
-    ckeditor_adapter = window.CKEDITOR_BASEPATH + 'adapters/jquery.js';
+    script = window.CKEDITOR_BASEPATH + 'ckeditor.js',
+    adapter = window.CKEDITOR_BASEPATH + 'adapters/jquery.js';
 
 if (selector.length > 0) {
-    $.getScript(ckeditor_script, function() {
+    $.getScript(script, function() {
 
-        $.getScript(ckeditor_adapter, function() {
+        $.getScript(adapter, function() {
 
             $.each(selector, function () {
 
