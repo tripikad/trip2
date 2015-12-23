1007066c7dbed35e11695d8c4517e543dfd5b5c2
mikkpokk <mikk@nicorex.ee>
2015-12-23 22:40:39 +0200

    StyleCI

--- resources/lang/et/passwords.php
+++ resources/lang/et/passwords.php
@@ -3,7 +3,7 @@
 return [
 
     'password' => 'Parool peab olema vähemalt 6 töhemärki pikk ja peab klappima parool uuesti väljaga',
-    'user' => "Sellise e-posti aadressiga kasutajat meie andmebaasis pole",
+    'user' => 'Sellise e-posti aadressiga kasutajat meie andmebaasis pole',
     'token' => 'Parooli taastamise link on aegunud või vigane',
     'sent' => 'Me saatsime su e-postile parooli taastamise lingi',
     'reset' => 'Su uus parool on nüüd kasutusel',
