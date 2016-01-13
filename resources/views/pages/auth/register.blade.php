a7b047e1d24454f23f87a35f6654e9c47100e443
Allar Pani <allar@haiku.ee>
2016-01-13 13:47:44 +0200

    Login, register and forgot password pages

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -1,62 +1,123 @@
-@extends('layouts.one_column')
+@extends('layouts.main')
 
 @section('title')
     {{ trans('auth.register.title') }}
 @stop
 
-@section('content.one')
+@section('header')
 
-    {!! Form::open(['route' => 'register.submit']) !!}
+    @include('component.header')
 
-        <div class="c-form__group">
-            {!! Form::label('name', trans('auth.register.field.name.title'), [
-                'class' => 'c-form__label'
-            ]) !!}
-            {!! Form::text('name', null, [
-                'class' => 'c-form__input'
-            ]) !!}
+@stop
+
+@section('content')
+
+<div class="r-auth">
+
+    <div class="r-auth__map">
+
+        <div class="c-auth-map">
+            @include('component.svg.standalone', [
+                'name' => 'map'
+            ])
+        </div>
+    </div>
+
+    <div class="r-auth__header">
+
+        <div class="r-auth__masthead">
+
+            @include('component.auth.masthead', [
+                'subtitle' => 'Liitu Trip.ee reisihuviliste seltskonnaga'
+            ])
+
+        </div>
+    </div>
+
+    <div class="r-auth__content">
+
+        <ul class="c-auth-tabs">
+            <li class="c-auth-tabs__item"><span class="c-auth-tabs__text">Emailiga</span></li>
+            <li class="c-auth-tabs__item"><a href="{{ route('facebook.redirect') }}" class="c-auth-tabs__link m-facebook">Facebook</a></li>
+            <li class="c-auth-tabs__item"><a href="{{ route('google.redirect') }}" class="c-auth-tabs__link m-google">Google</a></li>
+        </ul>
+
+        <div class="r-auth__content-inner">
+
+            {!! Form::open(['route' => 'register.submit']) !!}
+
+                <div class="c-form__group">
+                    {!! Form::label('name', trans('auth.register.field.name.title'), [
+                        'class' => 'c-form__label'
+                    ]) !!}
+                    {!! Form::text('name', null, [
+                        'class' => 'c-form__input'
+                    ]) !!}
+                </div>
+
+                <div class="c-form__group">
+                    {!! Form::label('email', trans('auth.register.field.email.title'), [
+                        'class' => 'c-form__label'
+                    ]) !!}
+                    {!! Form::email('email', null, [
+                        'class' => 'c-form__input'
+                    ]) !!}
+                </div>
+
+                <div class="c-form__group">
+                    {!! Form::label('password', trans('auth.register.field.password.title'), [
+                        'class' => 'c-form__label'
+                    ]) !!}
+                    {!! Form::password('password', [
+                        'class' => 'c-form__input'
+                    ]) !!}
+                </div>
+
+                <div class="c-form__group">
+                    {!! Form::label('password_confirmation', trans('auth.register.field.password_confirmation.title'), [
+                        'class' => 'c-form__label'
+                    ]) !!}
+                    {!! Form::password('password_confirmation', [
+                        'class' => 'c-form__input'
+                    ]) !!}
+                </div>
+
+                {{--
+
+                <div class="c-form__group">
+                    {!! Form::checkbox('eula', 1) !!}
+                    {!! trans('auth.register.field.eula.title', [
+                        'link' => '<a href="' . route('content.show', ['static', 25151]) . '">' . trans('auth.register.field.eula.title.link'). '</a>'
+                    ]) !!}
+                </div>
+
+                --}}
+
+                <div class="c-form__group">
+                    {!! Form::submit(trans('auth.register.submit.title'), [
+                        'class' => 'c-button m-large m-block'
+                    ]) !!}
+                </div>
+
+            {!! Form::close() !!}
         </div>
 
-        <div class="c-form__group">
-            {!! Form::label('email', trans('auth.register.field.email.title'), [
-                'class' => 'c-form__label'
-            ]) !!}
-            {!! Form::email('email', null, [
-                'class' => 'c-form__input'
-            ]) !!}
-        </div>
+        <div class="r-auth__content-footer">
 
-        <div class="c-form__group">
-            {!! Form::label('password', trans('auth.register.field.password.title'), [
-                'class' => 'c-form__label'
-            ]) !!}
-            {!! Form::password('password', [
-                'class' => 'c-form__input'
-            ]) !!}
-        </div>
+            <div class="c-auth-footer">
 
-        <div class="c-form__group">
-            {!! Form::label('password_confirmation', trans('auth.register.field.password_confirmation.title'), [
-                'class' => 'c-form__label'
-            ]) !!}
-            {!! Form::password('password_confirmation', [
-                'class' => 'c-form__input'
-            ]) !!}
-        </div>
+                <p class="c-auth-footer__text">Creating an account means you're okay with Trip.ee's<br><a href="#" class="c-auth-footer__link">Terms of Service</a>, <a href="#" class="c-auth-footer__link">Privacy Policy</a> and <a href="#" class="c-auth-footer__link">Cookie use</a>.</p>
 
-        <div class="c-form__group">
-            {!! Form::checkbox('eula', 1) !!}
-            {!! trans('auth.register.field.eula.title', [
-                'link' => '<a href="' . route('content.show', ['static', 25151]) . '">' . trans('auth.register.field.eula.title.link'). '</a>'
-            ]) !!}
-        </div>
+            </div>
 
-        <div class="c-form__group">
-            {!! Form::submit(trans('auth.register.submit.title'), [
-                'class' => 'c-button m-large m-block'
-            ]) !!}
         </div>
+    </div>
+</div>
 
-    {!! Form::close() !!}
+@stop
+
+@section('footer')
+
+    @include('component.footer')
 
 @stop
--- resources/views/pages/auth/reset/apply.blade.php
+++ resources/views/pages/auth/reset/apply.blade.php
@@ -1,28 +1,83 @@
-@extends('layouts.one_column')
+@extends('layouts.main')
 
 @section('title')
     {{ trans('auth.reset.apply.title') }}
 @stop
 
-@section('content.one')
+@section('header')
 
-    {!! Form::open(['route' => 'reset.apply.submit']) !!}
+    @include('component.header')
 
-        <div class="c-form__group">
-            {!! Form::label('email', trans('auth.reset.apply.field.email.title'), [
-                'class' => 'c-form__label'
-            ]) !!}
-            {!! Form::email('email', null, [
-                'class' => 'c-form__input'
-            ]) !!}
+@stop
+
+@section('content')
+
+<div class="r-auth">
+
+    <div class="r-auth__map">
+
+        <div class="c-auth-map">
+            @include('component.svg.standalone', [
+                'name' => 'map'
+            ])
+        </div>
+    </div>
+
+    <div class="r-auth__header">
+
+        <div class="r-auth__masthead">
+
+            @include('component.auth.masthead', [
+                'subtitle' => 'Sisesta oma e-mail ja me saadame sulle kinnituslingi'
+            ])
+
+        </div>
+    </div>
+
+    <div class="r-auth__content">
+
+        <div class="r-auth__content-inner">
+
+            {!! Form::open(['route' => 'reset.apply.submit']) !!}
+
+                <div class="c-form__group">
+                    {!! Form::label('email', trans('auth.reset.apply.field.email.title'), [
+                        'class' => 'c-form__label'
+                    ]) !!}
+                    {!! Form::email('email', null, [
+                        'class' => 'c-form__input'
+                    ]) !!}
+                </div>
+
+                <div class="c-form__group">
+                    {{-- Form::submit(trans('auth.reset.apply.submit.title'), [
+                        'class' => 'c-button m-large m-block'
+                    ]) --}}
+
+                    {!! Form::submit('Saada', [
+                        'class' => 'c-button m-large m-block'
+                    ]) !!}
+                </div>
+
+            {!! Form::close() !!}
+
         </div>
 
-        <div class="c-form__group">
-            {!! Form::submit(trans('auth.reset.apply.submit.title'), [
-                'class' => 'c-button m-large m-block'
-            ]) !!}
-        </div>
+        <div class="r-auth__content-footer">
 
-    {!! Form::close() !!}
+            <div class="c-auth-footer">
+
+                <p class="c-auth-footer__text">Tuli meelde? <a href="#" class="c-auth-footer__link">Logi sisse siit</a>.</p>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+@stop
+
+@section('footer')
+
+    @include('component.footer')
 
 @stop
