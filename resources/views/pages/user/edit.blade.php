7157c10b98286b50d7e876d7634e0a16baa3d6d3
Allar Pani <allar@haiku.ee>
2016-02-15 15:56:11 +0200

    Dropzone styles + edit profile page

--- resources/views/pages/user/edit.blade.php
+++ resources/views/pages/user/edit.blade.php
@@ -289,7 +289,7 @@
 
      </div>
 
-     <div class="r-auth__content">
+     <div class="r-auth__content m-wide">
 
          <div class="r-auth__content-inner">
 
@@ -482,7 +482,7 @@
 
             <div class="c-form__group m-large-margin">
 
-                <div class="c-columns m-2-cols m-last-smaller m-center">
+                <div class="c-columns m-2-cols m-last-smaller m-space">
 
                     <div class="c-columns__item">
 
@@ -490,25 +490,21 @@
 
                         <p class="c-form__note">Kuvatakse sinu profiili ning postituse juures</p>
 
-                        @include('component.button', [
-                            'modifiers' => 'm-quaternary m-min js-dropzoneClick',
-                            'title' => 'Vali pildifail',
-                            'route' => '#'
+                        {{--
+
+                        @include('component.image.form', [
+                            'form' => [
+                                'url' => $url,
+                                'method' => isset($method) ? $method : 'post',
+                                'model' => isset($user) ? $user : null,
+                                'files' => true,
+                            ],
+                            'name' => 'image',
+                            'maxFileSize' => 5,
+                            'uploadMultiple' => false
                         ])
 
-                        <div style="display:none">
-                            @include('component.image.form', [
-                                'form' => [
-                                    'url' => $url,
-                                    'method' => isset($method) ? $method : 'post',
-                                    'model' => isset($user) ? $user : null,
-                                    'files' => true
-                                ],
-                                'name' => 'image',
-                                'maxFileSize' => 5,
-                                'uploadMultiple' => false
-                            ])
-                        </div>
+                        --}}
 
                     </div>
 
@@ -523,7 +519,7 @@
 
             <div class="c-form__group m-large-margin">
 
-                <div class="c-columns m-2-cols m-last-smaller m-center">
+                <div class="c-columns m-2-cols m-last-smaller m-space">
 
                     <div class="c-columns__item">
 
@@ -531,10 +527,16 @@
 
                         <p class="c-form__note">Kuvatakse sinu profiili päise taustana</p>
 
-                        @include('component.button', [
-                            'modifiers' => 'm-quaternary m-min',
-                            'title' => 'Vali pildifail',
-                            'route' => '#'
+                        @include('component.image.form', [
+                            'form' => [
+                                'url' => $url,
+                                'method' => isset($method) ? $method : 'post',
+                                'model' => isset($user) ? $user : null,
+                                'files' => true
+                            ],
+                            'name' => 'image_bg',
+                            'maxFileSize' => 5,
+                            'uploadMultiple' => false
                         ])
 
                     </div>
