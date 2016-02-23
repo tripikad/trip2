dd6638de0db91da7b1280219148872ab5115d799
Allar Pani <allar@haiku.ee>
2016-02-23 13:29:24 +0200

    Search page mobile tabs + results

--- resources/views/pages/content/news/index.blade.php
+++ resources/views/pages/content/news/index.blade.php
@@ -32,14 +32,18 @@
 
     <div class="r-search__tabs">
 
-        <ul class="c-search-tabs">
-            <li class="c-search-tabs__item"><a href="#" class="c-search-tabs__item-link m-active js-search-tab" data-tab="forum">Foorum <span>7</span></a></li>
-            <li class="c-search-tabs__item"><a href="#" class="c-search-tabs__item-link js-search-tab" data-tab="blogs">Blogid <span>48</span></a></li>
-            <li class="c-search-tabs__item"><a href="#" class="c-search-tabs__item-link js-search-tab" data-tab="news">Uudised <span>39</span></a></li>
-            <li class="c-search-tabs__item"><a href="#" class="c-search-tabs__item-link js-search-tab" data-tab="destinations">Sihtkohad <span>12</span></a></li>
-            <li class="c-search-tabs__item"><a href="#" class="c-search-tabs__item-link js-search-tab" data-tab="flights">Lennupakkumised <span>1</span></a></li>
-            <li class="c-search-tabs__item"><a href="#" class="c-search-tabs__item-link js-search-tab" data-tab="users">Kasutajad <span>14</span></a></li>
-        </ul>
+        <div class="r-search__tabs-wrap">
+
+            <ul class="c-search-tabs">
+                <li class="c-search-tabs__item"><a href="#" class="c-search-tabs__item-link m-active js-search-tab" data-tab="forum">Foorum <span>7</span></a></li>
+                <li class="c-search-tabs__item"><a href="#" class="c-search-tabs__item-link js-search-tab" data-tab="blogs">Blogid <span>48</span></a></li>
+                <li class="c-search-tabs__item"><a href="#" class="c-search-tabs__item-link js-search-tab" data-tab="news">Uudised <span>39</span></a></li>
+                <li class="c-search-tabs__item"><a href="#" class="c-search-tabs__item-link js-search-tab" data-tab="destinations">Sihtkohad <span>12</span></a></li>
+                <li class="c-search-tabs__item"><a href="#" class="c-search-tabs__item-link js-search-tab" data-tab="flights">Lennupakkumised <span>1</span></a></li>
+                <li class="c-search-tabs__item"><a href="#" class="c-search-tabs__item-link js-search-tab" data-tab="users">Kasutajad <span>14</span></a></li>
+            </ul>
+
+        </div>
     </div>
 
     <div class="r-search__content">
@@ -171,11 +175,164 @@
                 </div>
 
                 <div class="c-search-container js-search-container" data-container="news">
-                    <p>news</p>
+
+                    <ul class="c-search-news">
+
+                        <li class="c-search-news__item">
+                            <a href="#" class="c-search-news__item-image" style="background-image: url({{\App\Image::getRandom()}});"></a>
+                            <div class="c-search-news__item-content">
+                                <h3 class="c-search-news__item-title">
+                                    <a href="#" class="c-search-news__item-title-link">Meie loodenaaber Rootsi ja tema käimata rajad</a>
+                                    <span class="c-search-news__item-title-date">02. märts 2016</span>
+                                </h3>
+                                <p>… uudiseid vägivallast ja inimröövidest saabus pidevalt. Kuigi ka praegu leidub Kolumbias piirkondi, kuhu ei soovitata reisida, on üldine olukord turvalisusega <span>Vietnam</span> riigis oluliselt parem, kui paarkümmend aastat tagasi. Kuritegevuse olulise …</p>
+                            </div>
+                        </li>
+
+                        <li class="c-search-news__item">
+                            <a href="#" class="c-search-news__item-image" style="background-image: url({{\App\Image::getRandom()}});"></a>
+                            <div class="c-search-news__item-content">
+                                <h3 class="c-search-news__item-title">
+                                    <a href="#" class="c-search-news__item-title-link">Cinque Terres hakatakse piirama külastajate arvu</a>
+                                    <span class="c-search-news__item-title-date">28. veebruar 2015</span>
+                                </h3>
+                                <p>… uudiseid vägivallast ja inimröövidest saabus pidevalt. Kuigi ka praegu leidub Kolumbias piirkondi, kuhu ei soovitata reisida, on üldine olukord turvalisusega <span>Vietnam</span> riigis oluliselt parem, kui paarkümmend aastat tagasi. Kuritegevuse olulise …</p>
+                            </div>
+                        </li>
+
+                        <li class="c-search-news__item">
+                            <a href="#" class="c-search-news__item-image" style="background-image: url({{\App\Image::getRandom()}});"></a>
+                            <div class="c-search-news__item-content">
+                                <h3 class="c-search-news__item-title">
+                                    <a href="#" class="c-search-news__item-title-link">Ryanair soovib Saksamaal kasvada 4 korda</a>
+                                    <span class="c-search-news__item-title-date">09. juuli 2014</span>
+                                </h3>
+                                <p>… uudiseid vägivallast ja inimröövidest saabus <span>Vietnam</span> riigis pidevalt. Kuigi ka praegu leidub Kolumbias piirkondi, kuhu ei soovitata reisida, on üldine olukord turvalisusega oluliselt parem, kui paarkümmend aastat tagasi. Kuritegevuse olulise …</p>
+                            </div>
+                        </li>
+
+                        <li class="c-search-news__item">
+                            <a href="#" class="c-search-news__item-image" style="background-image: url({{\App\Image::getRandom()}});"></a>
+                            <div class="c-search-news__item-content">
+                                <h3 class="c-search-news__item-title">
+                                    <a href="#" class="c-search-news__item-title-link">Nädalavahetuse filminurk: Namiibia</a>
+                                    <span class="c-search-news__item-title-date">02. detsember 2012</span>
+                                </h3>
+                                <p>… uudiseid vägivallast ja inimröövidest saabus <span>Vietnam</span> riigis pidevalt. Kuigi ka praegu leidub Kolumbias piirkondi, kuhu ei soovitata reisida, on üldine olukord turvalisusega oluliselt parem, kui paarkümmend aastat tagasi. Kuritegevuse olulise …</p>
+                            </div>
+                        </li>
+
+                        <li class="c-search-news__item">
+                            <a href="#" class="c-search-news__item-image" style="background-image: url({{\App\Image::getRandom()}});"></a>
+                            <div class="c-search-news__item-content">
+                                <h3 class="c-search-news__item-title">
+                                    <a href="#" class="c-search-news__item-title-link">Vene firma soovib konkureerida Airbusi ja Boeinguga</a>
+                                    <span class="c-search-news__item-title-date">13. aprill 2012</span>
+                                </h3>
+                                <p>… uudiseid vägivallast ja inimröövidest saabus <span>Vietnam</span> riigis pidevalt. Kuigi ka praegu leidub Kolumbias piirkondi, kuhu ei soovitata reisida, on üldine olukord turvalisusega oluliselt parem, kui paarkümmend aastat tagasi. Kuritegevuse olulise …</p>
+                            </div>
+                        </li>
+
+                        <li class="c-search-news__item">
+                            <a href="#" class="c-search-news__item-image" style="background-image: url({{\App\Image::getRandom()}});"></a>
+                            <div class="c-search-news__item-content">
+                                <h3 class="c-search-news__item-title">
+                                    <a href="#" class="c-search-news__item-title-link">Nädalavahetuse filminurk: mägiratastega Kaukasuses</a>
+                                    <span class="c-search-news__item-title-date">19. mai 2011</span>
+                                </h3>
+                                <p>… uudiseid vägivallast ja inimröövidest saabus <span>Vietnam</span> riigis pidevalt. Kuigi ka praegu leidub Kolumbias piirkondi, kuhu ei soovitata reisida, on üldine olukord turvalisusega oluliselt parem, kui paarkümmend aastat tagasi. Kuritegevuse olulise …</p>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
 
                 <div class="c-search-container js-search-container" data-container="destinations">
-                    <p>destinations</p>
+
+                    <ul class="c-search-destinations">
+
+                        <li class="c-search-destinations__item">
+                            <a href="#" class="c-search-destinations__item-image" style="background-image: url({{\App\Image::getRandom()}});"></a>
+                            <div class="c-search-destinations__item-content">
+                                <h3 class="c-search-destinations__item-title">
+                                    <a href="#" class="c-search-destinations__item-title-link">Vietnam</a>
+                                </h3>
+                                <ul class="c-search-destinations__item-info">
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Asia</a></li>
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Vietnam</a></li>
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li class="c-search-destinations__item">
+                            <a href="#" class="c-search-destinations__item-image" style="background-image: url({{\App\Image::getRandom()}});"></a>
+                            <div class="c-search-destinations__item-content">
+                                <h3 class="c-search-destinations__item-title">
+                                    <a href="#" class="c-search-destinations__item-title-link">Hanoi</a>
+                                </h3>
+                                <ul class="c-search-destinations__item-info">
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Asia</a></li>
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Vietnam</a></li>
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Hanoi</a></li>
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li class="c-search-destinations__item">
+                            <a href="#" class="c-search-destinations__item-image" style="background-image: url({{\App\Image::getRandom()}});"></a>
+                            <div class="c-search-destinations__item-content">
+                                <h3 class="c-search-destinations__item-title">
+                                    <a href="#" class="c-search-destinations__item-title-link">Ho Chi Minh City</a>
+                                </h3>
+                                <ul class="c-search-destinations__item-info">
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Asia</a></li>
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Vietnam</a></li>
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Ho Chi Minh City</a></li>
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li class="c-search-destinations__item">
+                            <a href="#" class="c-search-destinations__item-image" style="background-image: url({{\App\Image::getRandom()}});"></a>
+                            <div class="c-search-destinations__item-content">
+                                <h3 class="c-search-destinations__item-title">
+                                    <a href="#" class="c-search-destinations__item-title-link">Nha Trang</a>
+                                </h3>
+                                <ul class="c-search-destinations__item-info">
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Asia</a></li>
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Vietnam</a></li>
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Nha Trang</a></li>
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li class="c-search-destinations__item">
+                            <a href="#" class="c-search-destinations__item-image" style="background-image: url({{\App\Image::getRandom()}});"></a>
+                            <div class="c-search-destinations__item-content">
+                                <h3 class="c-search-destinations__item-title">
+                                    <a href="#" class="c-search-destinations__item-title-link">Da Nang</a>
+                                </h3>
+                                <ul class="c-search-destinations__item-info">
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Asia</a></li>
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Vietnam</a></li>
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Da Nang</a></li>
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li class="c-search-destinations__item">
+                            <a href="#" class="c-search-destinations__item-image" style="background-image: url({{\App\Image::getRandom()}});"></a>
+                            <div class="c-search-destinations__item-content">
+                                <h3 class="c-search-destinations__item-title">
+                                    <a href="#" class="c-search-destinations__item-title-link">Hoi An</a>
+                                </h3>
+                                <ul class="c-search-destinations__item-info">
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Asia</a></li>
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Vietnam</a></li>
+                                    <li><a href="#" class="c-search-destinations__item-info-link">Hoi An</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
 
                 <div class="c-search-container js-search-container" data-container="flights">
