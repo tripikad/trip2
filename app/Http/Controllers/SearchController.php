67de039c58409ac4fbc21d58a323e7f3a88d84a8
mikkpokk <mikk@nicorex.ee>
2016-02-23 17:38:27 +0200

    SearchController added

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use View;
+use Cache;
+use App\Content;
+use App\Destination;
+use App\Main;
+use DB;
+
+class SearchController extends Controller
+{
+    public function show()
+    {
+        $viewVariables = [];
+
+        return response()
+            ->view('pages.search.show', $viewVariables)
+            ->header('Cache-Control', 'public, s-maxage='.config('cache.search.header'));
+    }
+}
