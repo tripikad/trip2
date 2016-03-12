2af780ce27c7855876c9a35a970b011ff88e464b
mikkpokk <mikk@internationaleservices.eu>
2016-03-12 03:20:09 +0200

    latedef added

--- resources/assets/js/components/svg4everybody.js
+++ resources/assets/js/components/svg4everybody.js
@@ -1,8 +1,10 @@
+/* jslint latedef: svg4everybody */
+
 var svg4everybody_path = '/plugins/svg4everybody/',
     script = svg4everybody_path + 'svg4everybody.min.js';
 
 $.getScript(script, function() {
     if (typeof svg4everybody == 'function') {
-        svg4everybody();
+        $('test').svg4everybody();
     }
 });
