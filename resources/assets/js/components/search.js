48298417169f35638117b5b71a1d43bafad77bf0
kkallasm <kaspar68@gmail.com>
2016-03-28 01:09:58 +0300

    hound

--- resources/assets/js/components/search.js
+++ resources/assets/js/components/search.js
@@ -1,13 +1,14 @@
 /* Header search */
+/*
+$('.js-search__form-input').on('focus', function(){
+    $(this).closest('.js-search').addClass('m-active');
+});
 
-// $('.js-search__form-input').on('focus', function(){
-//     $(this).closest('.js-search').addClass('m-active');
-// });
-
-// $('.js-search__form-icon').on('click', function(){
-//     $(this).next($('.js-search__form-input')).focus();
-//     $(this).closest('.js-search').addClass('m-active');
-// });
+$('.js-search__form-icon').on('click', function(){
+    $(this).next($('.js-search__form-input')).focus();
+    $(this).closest('.js-search').addClass('m-active');
+});
+*/
 
 $(document).on('click', function (e) {
     if (!$('.js-search').is(e.target) &&
@@ -17,16 +18,16 @@ $(document).on('click', function (e) {
 });
 
 /* Search tabs */
-
-// $('.js-search-tab').on('click', function(){
-//     var data = $(this).data('tab');
-//     $('.js-search-tab').removeClass('m-active');
-//     $('.js-search-container').removeClass('m-active');
-//     $(this).addClass('m-active');
-//     $('.js-search-container[data-container='+ data +']').addClass('m-active');
-//     return false;
-// });
-
+/*
+$('.js-search-tab').on('click', function(){
+    var data = $(this).data('tab');
+    $('.js-search-tab').removeClass('m-active');
+    $('.js-search-container').removeClass('m-active');
+    $(this).addClass('m-active');
+    $('.js-search-container[data-container='+ data +']').addClass('m-active');
+    return false;
+});
+*/
 
 $('.c-search__form-input').on('keyup', function() {
     if($('.c-search__form-input').val()) {
