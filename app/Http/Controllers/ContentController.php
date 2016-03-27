6945f3469a7f13a25c3eaa3da9640aeeba815450
mikkpokk <mikk@internationaleservices.eu>
2016-03-28 00:19:38 +0300

    Use traits

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -12,8 +12,15 @@ use App\Image;
 use App\Main;
 use Illuminate\Pagination\LengthAwarePaginator;
 
+use App\Http\Controllers\ContentTraits\Blog;
+use App\Http\Controllers\ContentTraits\Flight;
+use App\Http\Controllers\ContentTraits\Forum;
+use App\Http\Controllers\ContentTraits\Travelmate;
+
 class ContentController extends Controller
 {
+    use Blog, Flight, Forum, Travelmate;
+
     public function index(Request $request, $type)
     {
         if ($type == 'internal'
@@ -102,47 +109,6 @@ class ContentController extends Controller
             ->header('Cache-Control', 'public, s-maxage='.config('cache.content.index.header'));
     }
 
-    public function blog_profile()
-    {
-        $viewVariables = [];
-
-        return response()
-            ->view('pages.content.blog.profile', $viewVariables)
-            ->header('Cache-Control', 'public, s-maxage='.config('cache.content.blog.profile.header'));
-    }
-
-    public function getTravelMateIndex()
-    {
-        $content = Content::whereIn('id', [1534, 25151])
-            ->whereStatus(1)
-            ->get();
-
-        $viewVariables['about'] = $content->where('id', 1534);
-
-        $viewVariables['rules'] = $content->where('id', 25151);
-
-        $viewVariables['activity'] = Content::whereType('travelmate')
-            ->whereStatus(1)
-            ->whereBetween('created_at', [
-                Carbon::now(),
-                Carbon::now()->addDays(14),
-            ])
-            ->count();
-
-        return $viewVariables;
-    }
-
-    public function getForumIndex()
-    {
-        $viewVariables['flights'] = Content::whereType('flight')
-            ->whereStatus(1)
-            ->orderBy('created_at', 'desc')
-            ->take(3)
-            ->get();
-
-        return $viewVariables;
-    }
-
     public function show($type, $id)
     {
         if ($type == 'internal'
@@ -187,157 +153,6 @@ class ContentController extends Controller
         return response()
             ->view($view, $viewVariables)
             ->header('Cache-Control', 'public, s-maxage='.config('cache.content.show.header'));
-    }
-
-    public function getTravelMateShow($content)
-    {
-        $viewVariables['travel_mates'] = Content::where('id', '!=', $content->id)
-            ->whereStatus(1)
-            ->whereType('travelmate')
-            ->orderBy('created_at', 'desc')
-            ->take(3)
-            ->get();
-
-        $destination_ids = $content->destinations->lists('id')->toArray();
-        $topic_ids = $content->topics->lists('id')->toArray();
-
-        $viewVariables['destination'] = null;
-        $viewVariables['parent_destination'] = null;
-        $destinationNotIn = [];
-
-        $sidebar_flights = Content::
-            with('destinations')
-            ->whereHas('destinations', function ($query) use ($destination_ids) {
-                $query->whereIn('content_destination.destination_id', $destination_ids);
-            })
-            ->where('type', 'flight')
-            ->whereStatus(1)
-            ->orderBy('created_at', 'desc')
-            ->get();
-
-        if (count($sidebar_flights)) {
-            $sidebar_flights = $sidebar_flights->groupBy('destination_id')->max()->take(2);
-
-            $viewVariables['destination'] = $sidebar_flights->first()->destinations->first();
-            if ($viewVariables['destination']) {
-                $viewVariables['parent_destination'] = $viewVariables['destination']->parent()->first();
-            }
-
-            $destinationNotIn = $sidebar_flights->first()->destinations->lists('id')->toArray();
-        }
-
-        $types = [
-            'forums' => ['forum', 'expat', 'buysell'],
-            'flights' => ['flight'],
-        ];
-
-        $viewVariables['sidebar_flights'] = $sidebar_flights;
-
-        foreach ($types as $key => $type) {
-            $viewVariables[$key] = Content::
-            join('content_destination', 'content_destination.content_id', '=', 'contents.id')
-                ->leftJoin('content_topic', 'content_topic.content_id', '=', 'contents.id')
-                ->whereIn('contents.type', $type)
-                ->where('contents.status', 1)
-                ->whereNotIn('content_destination.destination_id', $destinationNotIn)
-                ->whereNested(function ($query) use ($destination_ids, $topic_ids) {
-                    $query->whereIn(
-                        'content_destination.destination_id',
-                        $destination_ids
-                    )
-                    ->orWhereIn(
-                        'content_topic.topic_id',
-                        $topic_ids
-                    );
-                })
-                ->orderBy('contents.created_at', 'desc')
-                ->take(3)
-                ->get();
-        }
-
-        return $viewVariables;
-    }
-
-    public function getForumShow($content)
-    {
-        $viewVariables['travel_mates'] = Content::whereType('travelmate')
-            ->whereStatus(1)
-            ->orderBy('created_at', 'desc')
-            ->take(3)
-            ->get();
-
-        $viewVariables['flights'] = Content::whereType('flight')
-            ->whereStatus(1)
-            ->orderBy('created_at', 'desc')
-            ->take(3)
-            ->get();
-
-        $viewVariables['forums'] = Content::whereIn('type', ['forum', 'expat', 'buysell'])
-            ->whereStatus(1)
-            ->orderBy('created_at', 'desc')
-            ->take(4)
-            ->get();
-
-        $destination_ids = $content->destinations->lists('id')->toArray();
-        $topic_ids = $content->topics->lists('id')->toArray();
-
-        $relation_posts = Content::
-            with('destinations')
-            ->whereHas('destinations', function ($query) use ($destination_ids) {
-                $query->whereIn('destination_id', $destination_ids);
-            })
-            ->whereIn('type', ['forum', 'expat', 'buysell'])
-            ->where('id', '!=', $content->id)
-            ->orderBy('created_at', 'desc')
-            ->get();
-
-        $first_relative_posts = null;
-        $second_relative_posts = null;
-        $viewVariables['first_destination'] = null;
-        $viewVariables['first_destination_parent'] = null;
-        $viewVariables['second_destination'] = null;
-        $viewVariables['second_destination_parent'] = null;
-        if (count($relation_posts)) {
-            $relation_posts = $relation_posts->groupBy(function ($item) {
-                return $item->destinations->first()->id;
-            })->take(2);
-
-            if (count($relation_posts)) {
-                $first_relative_posts = $relation_posts->first();
-                $viewVariables['first_destination'] = $first_relative_posts->first()->destinations->first();
-                $viewVariables['first_destination_parent'] = $first_relative_posts->first()->destinations->first()->parent()->first();
-            }
-
-            if (count($relation_posts) > 1) {
-                $second_relative_posts = $relation_posts->last();
-                $viewVariables['second_destination'] = $second_relative_posts->first()->destinations->first();
-                $viewVariables['second_destination_parent'] = $second_relative_posts->first()->destinations->first()->parent()->first();
-            }
-        }
-
-        $viewVariables['first_relative_posts'] = $first_relative_posts;
-        $viewVariables['second_relative_posts'] = $second_relative_posts;
-
-        $viewVariables['relative_flights'] = Content::
-            join('content_destination', 'content_destination.content_id', '=', 'contents.id')
-            ->leftJoin('content_topic', 'content_topic.content_id', '=', 'contents.id')
-            ->where('contents.type', 'flight')
-            ->where('contents.status', 1)
-            ->whereNested(function ($query) use ($destination_ids, $topic_ids) {
-                $query->whereIn(
-                    'content_destination.destination_id',
-                    $destination_ids
-                )
-                ->orWhereIn(
-                    'content_topic.topic_id',
-                    $topic_ids
-                );
-            })
-            ->orderBy('contents.created_at', 'desc')
-            ->take(2)
-            ->get();
-
-        return $viewVariables;
     }
 
     public function create($type)
