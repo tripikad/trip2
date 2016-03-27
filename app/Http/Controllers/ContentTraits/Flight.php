893792ff6da6db934f583e1740ede8d56c973bd1
mikkpokk <mikk@internationaleservices.eu>
2016-03-28 02:07:36 +0300

    Get unique topics

--- app/Http/Controllers/ContentTraits/Flight.php
+++ app/Http/Controllers/ContentTraits/Flight.php
@@ -2,6 +2,45 @@
 
 namespace App\Http\Controllers\ContentTraits;
 
+use App\Content;
+
 trait Flight
 {
+    public function getFlightIndex($contents, $topics)
+    {
+        $viewVariables = [];
+
+        $uniqueTopics = [];
+        foreach ($contents as $content) {
+            if (count($content->topics)) {
+                foreach ($content->topics as $topic) {
+                    if (! in_array($topic->name, $uniqueTopics)) {
+                        $uniqueTopics[] = [
+                            'name' => $topic->name,
+                            'id' => $topic->id,
+                        ];
+                    }
+                }
+            }
+        }
+
+        if (! count($uniqueTopics)) {
+            $topics = $topics->take(12);
+
+            if (count($topics)) {
+                foreach ($topics as $id => $name) {
+                    $uniqueTopics[] = [
+                        'name' => $name,
+                        'id' => $id,
+                    ];
+                }
+            }
+        }
+
+        $uniqueTopics = collect($uniqueTopics);
+
+        $viewVariables['uniqueTopics'] = $uniqueTopics;
+
+        return $viewVariables;
+    }
 }
