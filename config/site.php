9b708b05e503538d7bbe0ac79fa3395c9cd2f549
mikkpokk <mikk@internationaleservices.eu>
2016-04-01 06:07:04 +0300

    allowAllTags option added

--- config/site.php
+++ config/site.php
@@ -4,7 +4,11 @@ return [
 
     'name' => 'Trip2',
 
-    'allowedtags' => '<b><i><strong><em><a><br><ul><ol><li><img><iframe><h4>',
+    'allowedtags' => '<b><i><strong><em><a><br><ul><ol><li><img><iframe><h4><h5><h6><p>',
+
+    'allowAllTags' => [
+        'content/flight'
+    ],
 
     'currency' => [
 
