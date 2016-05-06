cc43c741a3420eb37031cab64ef7d193e559ad95
mikkpokk <mikk@internationaleservices.eu>
2016-05-07 00:11:49 +0300

    Functions from blade component + responsive

--- resources/assets/js/components/promo.js
+++ resources/assets/js/components/promo.js
@@ -21,4 +21,30 @@ googletag.cmd.push(function() {
     googletag.pubads().enableSingleRequest();
     googletag.pubads().collapseEmptyDivs();
     googletag.enableServices();
+    googletag.pubads().addEventListener('slotRenderEnded', function(e) {
+        if ($('#'+e.slot.m.o).length) {
+            return renderEnded(e.slot.m.o, e.size[0], e.size[1]);
+        }
+    });
 });
+
+window.onload = function(){
+    for (var promo in promos) {
+        if(promos[promo].id1 && promos[promo].id2) {
+            if ($('#' + promos[promo].id2).length) {
+                googletag.display(promos[promo].id2);
+            }
+        }
+    }
+}
+
+function renderEnded (adunitId, width, height) {
+    $('#' + adunitId).find('iframe').on('load', function(){
+        var iFrame = this,
+            newHeight = (($(iFrame).width() * height) / width);
+        $(iFrame).contents().find('#google_image_div').css({'max-width': '100%', 'height': 'auto', 'display': 'block'});
+        $(iFrame).contents().find('img').attr('onload', 'alert(\'test\');').css({'max-width': '100%', 'height': 'auto', 'display': 'block'});
+        $(iFrame).css({'height': newHeight+'px'});
+        $(iFrame).parents("#" + adunitId).show();
+    });
+}
