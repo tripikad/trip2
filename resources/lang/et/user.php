c8c8dd0dd31d45cbf3d27049115779a1ec10b4fd
mikkpokk <mikk@internationaleservices.eu>
2016-05-09 02:40:42 +0300

    New definitions added

--- resources/lang/et/user.php
+++ resources/lang/et/user.php
@@ -18,14 +18,16 @@ return [
 
     'image.create.title' => 'Lisa profiilipilt',
     'image.edit.title' => 'Muuda profiilipilti',
+    'image.title' => 'Profiilipilt',
+    'image.description' => 'Kuvatakse sinu profiili ning postituse juures',
 
     'edit.title' => 'Muuda profiili',
     'edit.contact.title' => 'Kontaktandmed',
     'edit.account.title' => 'Kasutaja andmed',
-    'edit.general.title' => 'Üldine informatsioon',
+    'edit.general.title' => 'Üldinfo',
     'edit.notify.title' => 'E-posti teavitused',
 
-    'edit.choose.gender' => 'Vali sugu',
+    'edit.choose.gender' => 'Sugu',
 
     'edit.field.name.title' => 'Kasutajanimi',
     'edit.field.email.title' => 'E-post',
@@ -34,15 +36,19 @@ return [
 
     'edit.field.real.name.title' => 'Sinu nimi',
     'edit.field.birthyear.title' => 'Sünniaasta',
-    'edit.field.description.title' => 'Kirjuta siia lühitutvustus endast',
+    'edit.field.description.label' => 'Lühikirjeldus',
+    'edit.field.description.title' => 'Kirjelda tripikatele ennast ja oma reisikogemusi…',
 
     'edit.field.contact_facebook.title' => 'Facebook\'i URL',
     'edit.field.contact_twitter.title' => 'Twitter\'i URL',
     'edit.field.contact_instagram.title' => 'Instagram\'i URL',
-    'edit.field.contact_homepage.title' => 'Kodulehekülje URL',
+    'edit.field.contact_homepage.title' => 'Kodulehekülg',
 
     'edit.field.notify_message.title' => 'Teavita mind, kui keegi on saatnud mulle sõnumi',
     'edit.field.notify_follow.title' => 'Teavita mind, kui keegi on kirjutanud kommentaari postitusele, mida ma jälgin',
+
+    'edit.profile_color.title' => 'Profiili värv',
+    'edit.profile_color.description' => 'Kuvatakse sinu profiili taustana',
 
     'edit.submit' => 'Uuenda profiili',
 
