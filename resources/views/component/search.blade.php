34e2ba0c7f375408a4903f5175d773ff659bd2c2
mikkpokk <mikk@internationaleservices.eu>
2016-05-09 21:31:01 +0300

    Span to label

--- resources/views/component/search.blade.php
+++ resources/views/component/search.blade.php
@@ -1,21 +1,15 @@
 @inject('request', 'Illuminate\Http\Request')
 
 <div class="c-search js-search {{ $modifiers or '' }}">
-
-<form action="/search" method="get" id="global_search_form">
-
-    <div class="c-search__form">
-
-        <span class="c-search__form-icon">
-            @include('component.svg.sprite', [
-                'name' => 'icon-search'
-            ])
-        </span>
-
-        <input type="text" name="q" class="c-search__form-input js-search__form-input" placeholder="{{ $placeholder }}" autocomplete="off" value="{{$request->get('q')}}">
-        
-    </div>
-
-</form>
-
-<div class="c-search__results" id="search_results_div"></div>
+    <form action="/search" method="get" id="global_search_form">
+        <div class="c-search__form">
+            <input type="text" name="q" id="search-field" class="c-search__form-input js-search__form-input" placeholder="{{ $placeholder }}" autocomplete="off" value="{{$request->get('q')}}">
+            <label for="search-field" class="c-search__form-icon">
+                @include('component.svg.sprite', [
+                    'name' => 'icon-search'
+                ])
+            </label>
+        </div>
+    </form>
+    <div class="c-search__results" id="search_results_div"></div>
+</div>
