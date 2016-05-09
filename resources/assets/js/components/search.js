1122e385a53eb418accb71f756c69b0aa4566c45
mikkpokk <mikk@internationaleservices.eu>
2016-05-10 01:38:31 +0300

    Ajax request optimized

--- resources/assets/js/components/search.js
+++ resources/assets/js/components/search.js
@@ -5,21 +5,25 @@ $(document).on('click', function (e) {
    }
 });
 
-$('.c-search__form-input').on('keyup', function() {
-    if($('.c-search__form-input').val()) {
+var searchInput = '.c-search__form-input',
+    searchRequest = null;
+$(searchInput).on('keypress keyup keydown click change', function() {
+    if($(searchInput).val()) {
+        if (searchRequest) {
+            searchRequest.abort();
+        }
+
         $(this).closest('.js-search').addClass('m-active');
         var headerSearch = ($(this).attr('id') && $(this).attr('id') == 'search_small')?1:0;
-        $.ajax({
+        searchRequest = $.ajax({
             url: '/search/ajaxsearch',
             type: 'get',
             data: {q: $('.c-search__form-input').val(), 'header_search': headerSearch},
             success: function(response) {
-                if(response) {
+                if (response) {
                     $('#search_results_div').html(response);
-                }
-                else {
-                    $('.js-search').removeClass('m-active');
-                    $('#search_results_div').html('');
+                } else {
+                    $('#search_results_div').hide();
                 }
             }
         });
