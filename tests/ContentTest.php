effb3575614dc33d7a5df8e035b000f8443e2858
mikkpokk <mikk@internationaleservices.eu>
2016-05-09 05:38:27 +0300

    401 is expected exception

--- tests/ContentTest.php
+++ tests/ContentTest.php
@@ -74,6 +74,10 @@ class ContentTest extends TestCase
         }
     }
 
+    /**
+     * @expectedException PHPUnit_Framework_ExpectationFailedException
+     * @expectedExceptionMessage Received status code [401]
+     */
     public function test_regular_user_can_not_create_private_content()
     {
         $regular_user = factory(App\User::class)->create();
@@ -81,19 +85,15 @@ class ContentTest extends TestCase
         foreach ($this->privateContentTypes as $type) {
 
             // Acting as unlogged user
-
             $this->visit("content/$type")
-                ->dontSee(trans("content.$type.create.title"));
-            $response = $this->call('GET', "content/$type/create");
-            $this->assertEquals(401, $response->status());
+                ->dontSee(trans("content.$type.create.title"))
+                ->visit("content/$type/create"); // 401
 
             // Acting as registered user
-
             $this->actingAs($regular_user)
                 ->visit("content/$type")
-                ->dontSee(trans("content.$type.create.title"));
-            $response = $this->call('GET', "content/$type/create");
-            $this->assertEquals(401, $response->status());
+                ->dontSee(trans("content.$type.create.title"))
+                ->visit("content/$type/create"); // 401
         }
     }
 }
