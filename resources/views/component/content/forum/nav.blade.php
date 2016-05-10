83b580586ceb35bb8ec4ee81665896a81819ffec
mikkpokk <mikk@internationaleservices.eu>
2016-05-10 03:27:18 +0300

    Add element only if array is not empty

--- resources/views/component/content/forum/nav.blade.php
+++ resources/views/component/content/forum/nav.blade.php
@@ -14,47 +14,32 @@ code: |
 <ul class="c-forum-list-nav">
 
     @if(isset($items))
-
         @foreach ($items as $item)
-
-            @if (isset($item['type']) && $item['type'] === 'button')
-
-                <li class="c-forum-list-nav__item m-button">
-
-                    @include('component.button', [
-                        'modifiers' => $item['modifiers'],
-                        'title' => (trans($item['title']) ? trans($item['title']) : $item['title']),
-                        'route' => $item['route']
-                    ])
-
-                </li>
-
-            @else
-
-                <li class="c-forum-list-nav__item">
-
-                    @if(isset($item['icon']))
-
-                        <a href="{{ $item['route'] }}" class="c-forum-list-nav__item-link">{{ (trans($item['title']) ? trans($item['title']) : $item['title']) }}
-                            <span class="c-forum-list-nav__item-icon">
-                                @include('component.svg.sprite', [
-                                    'name' => $item['icon']
-                                ])
-                            </span>
-                        </a>
-
-                    @else
-
-                        <a href="{{ $item['route'] }}" class="c-forum-list-nav__item-link">{{ (trans($item['title']) ? trans($item['title']) : $item['title']) }}</a>
-
-                    @endif
-
-                </li>
-
+            @if (count($item))
+                @if (isset($item['type']) && $item['type'] === 'button')
+                    <li class="c-forum-list-nav__item m-button">
+                        @include('component.button', [
+                            'modifiers' => $item['modifiers'],
+                            'title' => (trans($item['title']) ? trans($item['title']) : $item['title']),
+                            'route' => $item['route']
+                        ])
+                    </li>
+                @else
+                    <li class="c-forum-list-nav__item">
+                        @if(isset($item['icon']))
+                            <a href="{{ $item['route'] }}" class="c-forum-list-nav__item-link">{{ (trans($item['title']) ? trans($item['title']) : $item['title']) }}
+                                <span class="c-forum-list-nav__item-icon">
+                                    @include('component.svg.sprite', [
+                                        'name' => $item['icon']
+                                    ])
+                                </span>
+                            </a>
+                        @else
+                            <a href="{{ $item['route'] }}" class="c-forum-list-nav__item-link">{{ (trans($item['title']) ? trans($item['title']) : $item['title']) }}</a>
+                        @endif
+                    </li>
+                @endif
             @endif
-
         @endforeach
-
     @endif
-
 </ul>
