d8f27e680e98168f7785cf033d84b18f670c7016
mikkpokk <mikk@internationaleservices.eu>
2016-05-12 00:34:45 +0300

    Helper format_smtp_header created

---
+++ app/Utils/utils.php
@@ -0,0 +1,11 @@
+<?php
+
+function format_smtp_header(array $data)
+{
+    $json = json_encode($data);
+    $json = preg_replace('/(["\]}])([,:])(["\[{])/', '$1$2 $3', $json);
+
+    $str  = wordwrap($json, 76, "\n   ");
+
+    return $str;
+}
