76073b3f01c45bb4131bead0ddb7e34438442f66
kristjanjansen <kristjan.jansen@gmail.com>
2016-05-12 22:31:34 +0300

    convert fix

--- app/Console/Commands/ConvertBase.php
+++ app/Console/Commands/ConvertBase.php
@@ -414,6 +414,8 @@ class ConvertBase extends Command
     {
         $user = $this->getUser($uid);
 
+        if (!$user) return false;
+
         $blockedSender = false;
 
         // We only consider user being blocked when it is not admininstrator, editor, senior editor or superuser
