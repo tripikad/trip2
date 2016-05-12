b184af4d53e1fda18a836ba003b8d710a4039bcf
mikkpokk <mikk@internationaleservices.eu>
2016-05-12 18:13:51 +0300

    401 error definitions added

--- resources/lang/et/error.php
+++ resources/lang/et/error.php
@@ -9,4 +9,12 @@ return [
 
     '401.ajax.title' => 'Pole piisavalt õiguseid',
 
+    '401.title.logged' => 'Õigused puuduvad',
+    '401.body.logged' => 'Sinu kasutajal pole õiguseid soovitud leheküljele minna.',
+
+    '401.title.unlogged' => 'Pead esmalt sisse logima',
+    '401.body.unlogged' => 'Palun logi oma kasutajakontosse sisse, et soovitud leheküljele ligipääsu saada.
+
+     Sisse logimiseks / registreerimiseks vajaminevad väljad leiad menüüst "Minu Trip.ee"',
+
 ];
