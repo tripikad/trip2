0c6b2514824f8f03478c2d504538ded36065b7fd
mikkpokk <mikk@internationaleservices.eu>
2016-05-12 20:15:46 +0300

    Visual fixes, clean-up
    Closes issue #675

--- resources/views/pages/content/travelmate/show.blade.php
+++ resources/views/pages/content/travelmate/show.blade.php
@@ -3,19 +3,15 @@
 @section('title', trans("content.$type.index.title"))
 
 @section('header')
-
     @include('component.header',[
         'modifiers' => 'm-alternative'
     ])
-
 @stop
 
 @section('content')
 
 <div class="r-travelmates m-single">
-
     <div class="r-travelmates__masthead">
-
         @include('component.masthead', [
             'modifiers' => 'm-alternative',
             'image' => \App\Image::getRandom(),
@@ -28,8 +24,6 @@
         <div class="r-travelmates__content m-first">
             <h1 class="r-travelmates__title">{{ $content->title }}</h1>
 
-            @include('component.actions', ['actions' => $content->getActions()])
-
             <div class="r-travelmates__meta">
                 <p class="r-travelmates__meta-date">
                     {{ trans('content.travelmate.post.added', [
@@ -38,39 +32,33 @@
                         ])
                     ]) }}
                 </p>
-
-                @if (count($content->destinations))
-
-                    @include('component.tags', [
-                        'modifiers' => 'm-small',
-                        'items' => $content->destinations->transform(function ($content_destination) {
-                            return [
-                                'modifiers' => ['m-purple', 'm-yellow', 'm-red', 'm-green'][rand(0,3)],
-                                'route' => route('destination.show', [$content_destination->id]),
-                                'title' => $content_destination->name
-                            ];
-                        })
-                    ])
-
-                @endif
-
+                <div class="m-pull-right">
+                    @include('component.actions', ['actions' => $content->getActions()])
+                </div>
             </div>
 
-            @if (count($content->topics))
-
+            @if (count($content->topics) || count($content->destinations))
+                @php
+                (count($content->topics) ? $content_topics = $content->topics->transform(function ($content_topic) {
+                    return [
+                            'modifiers' => 'm-gray',
+                            'title' => $content_topic->name
+                    ];
+                }) : $content_topics = collect([]));
+                (count($content->destinations) ? $content_destinations = $content->destinations->transform(function ($content_destination) {
+                    return [
+                            'modifiers' => ['m-purple', 'm-yellow', 'm-red', 'm-green'][rand(0,3)],
+                            'route' => route('destination.show', [$content_destination->id]),
+                            'title' => $content_destination->name
+                    ];
+                }) : $content_destinations = collect([]));
+                @endphp
                 <div class="r-travelmates__meta">
-
                     @include('component.tags', [
                         'modifiers' => 'm-small',
-                        'items' => $content->topics->transform(function ($content_topic) {
-                            return [
-                                'modifiers' => 'm-gray',
-                                'title' => $content_topic->name
-                            ];
-                        })
+                        'items' => array_merge($content_topics->toArray(), $content_destinations->toArray()),
                     ])
                 </div>
-
             @endif
 
             <div class="r-travelmates__content-body">
@@ -90,7 +78,7 @@
                     'image' => $content->user->imagePreset('small_square'),
                     'letter' => [
                         'modifiers' => 'm-purple',
-                        'text' => 'J'
+                        'text' => (strlen($content->user->name) ? $content->user->name[0] : '')
                     ],
                     'user' => $content->user,
                     'name' => $content->user->name,
@@ -136,54 +124,39 @@
 
     <div class="r-travelmates__wrap">
         <div class="r-travelmates__content">
-
             <div class="r-block">
-
                 <div class="r-block__inner">
-
                     <div class="r-block__header">
-
                         <div class="r-block__header-title">
-
                             @include('component.title', [
-                                'title' => 'Soovita pakkumist sõpradele',
+                                'title' => trans('content.share.post.title'),
                                 'modifiers' => 'm-large m-green'
                             ])
                         </div>
                     </div>
-
                     <div class="r-block__body">
-
                         @include('component.facebook')
                     </div>
-
                 </div>
             </div>
 
             @if(count($comments) > 0)
-
                 <div class="r-block">
                     <div class="r-block__header">
-
                         @include('component.title', [
                             'title' => trans('content.comments.title'),
                             'modifiers' => 'm-purple'
                         ])
-
                     </div>
-
                     <div class="r-block__body">
-
                         @include('component.comment.index', [
                             'comments' => $comments
                         ])
-
                     </div>
                 </div>
             @endif
 
             @if (\Auth::check())
-
                 <div class="r-block">
                     <div class="r-block__inner">
                         <div class="r-block__header">
@@ -201,16 +174,11 @@
                         </div>
                     </div>
                 </div>
-
             @endif
-
         </div>
-
         <div class="r-travelmates__sidebar">
             <div class="r-block m-small">
-
                 @if ($destination && $parent_destination)
-
                     @include('component.destination', [
                         'modifiers' => 'm-purple',
                         'title' => $destination ? $destination->name : null,
@@ -218,24 +186,20 @@
                         'subtitle' => $parent_destination ? $parent_destination->name : null,
                         'subtitle_route' => $parent_destination ? route('destination.show', $parent_destination) : null,
                     ])
-
                 @endif
 
                 @if (count($sidebar_flights))
                     @foreach ($sidebar_flights as $sidebar_flight)
-
                         @include('component.card', [
                             'modifiers' => 'm-purple',
                             'route' => route('content.show', [$sidebar_flight->type, $sidebar_flight]),
                             'title' => $sidebar_flight->title.' '.$sidebar_flight->price.config('site.currency.symbol'),
                             'image' => $sidebar_flight->imagePreset()
                         ])
-
                     @endforeach
                 @endif
 
                 @if (isset($forums) && count($forums))
-
                     <div class="r-block__inner">
                         <div class="r-block__header">
                             <div class="r-block__header-title m-flex">
@@ -245,46 +209,24 @@
                                 ])
                             </div>
                         </div>
-
-                        @include('component.content.forum.list', [
-                            'modifiers' => 'm-compact',
-                            'items' => $forums->transform(function ($forum) {
-                                return [
-                                    'topic' => str_limit($forum->title, 30),
-                                    'route' => route('content.show', [$forum->type, $forum]),
-                                    'profile' => [
-                                        'modifiers' => 'm-mini',
-                                        'image' => $forum->user->imagePreset('small_square'),
-                                        'letter' => [
-                                            'modifiers' => 'm-red',
-                                            'text' => 'J'
-                                        ],
-                                    ],
-                                    'badge' => [
-                                        'modifiers' => 'm-inverted m-purple',
-                                        'count' => $forum->comments->count()
-                                    ]
-                                ];
-                            })
+                        @include('region.content.forum.list', [
+                            'modifiers' => [
+                                'main' => 'm-compact',
+                                'letter' => 'm-red',
+                                'badge' => 'm-inverted m-purple',
+                            ],
+                            'items' => $forums,
                         ])
-
                     </div>
-
                 @endif
-
             </div>
-
             <div class="r-block m-small m-mobile-hide">
-
                 @include('component.promo', ['promo' => 'sidebar_small'])
-
             </div>
         </div>
     </div>
 
-
     @if (count($flights))
-
         <div class="r-travelmates__offers">
             <div class="r-travelmates__offers-wrap">
                 @include('region.content.flight.card', [
@@ -292,11 +234,9 @@
                 ])
             </div>
         </div>
-
     @endif
 
     @if (isset($travel_mates) && count($travel_mates))
-
         @if (isset($flights) && count($flights))
         <div class="r-travelmates__additional m-padding">
         @else
@@ -309,26 +249,19 @@
                 ])
             </div>
         </div>
-
     @endif
 
     <div class="r-travelmates__footer-promo">
         <div class="r-travelmates__footer-promo-wrap">
-
            @include('component.promo', ['promo' => 'footer'])
-
         </div>
     </div>
-
 </div>
-
 @stop
 
 @section('footer')
-
     @include('component.footer', [
         'modifiers' => 'm-alternative',
         'image' => \App\Image::getRandom()
     ])
-
 @stop
