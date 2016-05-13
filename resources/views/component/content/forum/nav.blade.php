32247f86c644ddd7523e0fcde11438f185e7c9a1
mikkpokk <mikk@internationaleservices.eu>
2016-05-13 05:08:23 +0300

    Check if item is active

--- resources/views/component/content/forum/nav.blade.php
+++ resources/views/component/content/forum/nav.blade.php
@@ -17,7 +17,7 @@ code: |
         @foreach ($items as $item)
             @if (count($item))
                 @if (isset($item['type']) && $item['type'] === 'button')
-                    <li class="c-forum-list-nav__item m-button">
+                    <li class="c-forum-list-nav__item m-button {{ Ekko::isActiveURL($item['route']) }}">
                         @include('component.button', [
                             'modifiers' => $item['modifiers'],
                             'title' => (trans($item['title']) ? trans($item['title']) : $item['title']),
@@ -25,7 +25,7 @@ code: |
                         ])
                     </li>
                 @else
-                    <li class="c-forum-list-nav__item">
+                    <li class="c-forum-list-nav__item {{ Ekko::isActiveURL($item['route']) }}">
                         @if(isset($item['icon']))
                             <a href="{{ $item['route'] }}" class="c-forum-list-nav__item-link">{{ (trans($item['title']) ? trans($item['title']) : $item['title']) }}
                                 <span class="c-forum-list-nav__item-icon">
