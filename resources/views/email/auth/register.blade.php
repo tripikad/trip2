c03f61fde39ebe6aa3d035247624e56fa0143414
kristjanjansen <kristjan.jansen@gmail.com>
2016-05-24 22:40:38 +0300

    mail fixes 2

--- resources/views/email/auth/register.blade.php
+++ resources/views/email/auth/register.blade.php
@@ -1,8 +1,9 @@
 {{ trans('auth.register.email.body.line1') }}
-
+<br /><br />
 {{ trans('auth.register.email.body.line2') }}
-
+<br /><br />
 {{ route('register.confirm', [$user->registration_token], true) }}
-
+<br /><br />
 --
+<br />
 {{ config('site.name')}}
