377a8c54127b6b6316b7721afc71873e2156f990
kkallasm <kaspar68@gmail.com>
2016-05-25 23:08:23 +0300

    improve pergormance

--- app/User.php
+++ app/User.php
@@ -185,12 +185,8 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
         $age = $this->created_at;
 
         $level = config('user.ranking.starting_level');
-        $end = false;
 
         foreach (config('user.ranking.levels') as $rank => $condition) {
-            if ($end) {
-                break;
-            }
 
             foreach ($condition as $condition => $value) {
                 if ($condition === 'countries') {
@@ -217,8 +213,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
                 }
 
                 if ($level === config('user.ranking.starting_level')) {
-                    $end = true;
-                    break;
+                    break 2;
                 }
             }
         }
