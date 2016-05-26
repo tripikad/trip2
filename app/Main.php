95ea3cf96e82a22980a38b03e2e977d1329fc7a5
kristjanjansen <kristjan.jansen@gmail.com>
2016-05-26 14:18:51 +0300

    fix

--- app/Main.php
+++ app/Main.php
@@ -14,7 +14,9 @@ class Main
 
         $urlPattern = "/(?i)\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))(?![^<>]*>)/i";
 
-        $filteredBody = preg_replace($urlPattern, '<a href="$1">$1</a>', $filteredBody);
+        if ($processedBody = preg_replace($urlPattern, '<a href="$1">$1</a>', $filteredBody)) {
+             $filteredBody = $processedBody;
+        }
 
         $imagePattern = '/\[\[([0-9]+)\]\]/';
 
