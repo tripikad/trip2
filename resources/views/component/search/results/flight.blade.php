06a565cf21ca122e7962f4fda932b94a907d52bc
Mikk Pokk <mikk@internationaleservices.eu>
2016-06-14 18:57:52 +0300

    Hide hardcoded tags at the moment

--- resources/views/component/search/results/flight.blade.php
+++ resources/views/component/search/results/flight.blade.php
@@ -9,10 +9,10 @@
                 <a href="{{$row->route}}" class="c-search-flights__item-title-link">{{$row->title}}</a>
                 <span class="c-search-flights__item-title-date">{{\Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $row->created_at)->format('d.m.Y H:i')}}</span>
             </h3>
-            <ul class="c-search-flights__item-info">
+            <!--<ul class="c-search-flights__item-info">
                 <li>Hanoi, Vietnam</li>
                 <li>Jaanuar – Veebruar 2017</li>
-            </ul>
+            </ul>-->
         </div>
     </li>
 
