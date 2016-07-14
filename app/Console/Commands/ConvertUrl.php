eda39a709f44f1b0da5d2f7b6032284afae37bc4
kristjanjansen <kristjan.jansen@gmail.com>
2016-07-14 10:17:52 -0400

    Applied fixes from StyleCI

--- app/Console/Commands/ConvertUrl.php
+++ app/Console/Commands/ConvertUrl.php
@@ -2,38 +2,40 @@
 
 namespace App\Console\Commands;
 
-use DB;
-
 class ConvertUrl extends ConvertBase
 {
     protected $signature = 'convert:url';
 
     public function generateMovedTermAliases()
     {
-    
+
         // dump($this->topicMap);
 
         collect($this->topicMap)
-            ->filter(function($value) {
+            ->filter(function ($value) {
                 return collect($value)->has('move');
             })
-            ->map(function($value, $key) {
+            ->map(function ($value, $key) {
                 return (object) [
                     'from' => $this->getTermByName($key)->tid,
-                    'to' => $this->getTermByName(collect($value)->first())->tid
+                    'to' => $this->getTermByName(collect($value)->first())->tid,
                 ];
             })
-            ->map(function($moveMap) {
+            ->map(function ($moveMap) {
                 $alias = $this->getTermAlias($moveMap->from);
+
                 return (object) [
                     'path1' => 'taxonomy/term/'.$moveMap->from,
                     'path2' => $alias ? $alias->dst : null,
                     'aliasable_id' => $moveMap->to,
                 ];
             })
-            ->map(function($moveMap) { dump($moveMap); return $moveMap; })
-            ->each(function($moveMap) {
-                
+            ->map(function ($moveMap) {
+                dump($moveMap);
+
+                return $moveMap;
+            })
+            ->each(function ($moveMap) {
                 \DB::table('aliases')->insert([
                     'aliasable_id' => $moveMap->aliasable_id,
                     'aliasable_type' => 'topic',
@@ -41,51 +43,47 @@ class ConvertUrl extends ConvertBase
                 ]);
 
                 if ($moveMap->path2) {
-                    
                     \DB::table('aliases')->insert([
                         'aliasable_id' => $moveMap->aliasable_id,
                         'aliasable_type' => 'topic',
                         'path' =>  $moveMap->path2,
                     ]);
-
                 }
-
             });
-
-        }
+    }
 
     public function generateDeletedTermAliases()
     {
         collect($this->topicMap)
-            ->filter(function($value) {
+            ->filter(function ($value) {
                 return collect($value)->has('delete');
             })
-            ->map(function($value, $key) {
+            ->map(function ($value, $key) {
                 return $this->getTermByName($key)->tid;
             })
-            ->map(function($tid) {
+            ->map(function ($tid) {
                 $alias = $this->getTermAlias($tid);
+
                 return (object) [
                     'path' => $alias ? $alias->dst : null,
                     'aliasable_id' => null,
                 ];
             })
-            ->filter(function($deleteMap) {
+            ->filter(function ($deleteMap) {
                 return $deleteMap->path;
             })
-            ->map(function($deleteMap) { dump($deleteMap); return $deleteMap; })
-            ->each(function($deleteMap) {
+            ->map(function ($deleteMap) {
+                dump($deleteMap);
 
+                return $deleteMap;
+            })
+            ->each(function ($deleteMap) {
                 \DB::table('aliases')->insert([
                     'aliasable_id' => 0,
                     'aliasable_type' => 'topic',
                     'path' =>  $deleteMap->path,
                 ]);
-
-            });     
-
-
-
+            });
     }
 
     public function handle()
@@ -93,5 +91,4 @@ class ConvertUrl extends ConvertBase
         $this->generateMovedTermAliases();
         $this->generateDeletedTermAliases();
     }
-
-};
+}
--- app/Console/Commands/ConvertUrlTest.php
+++ app/Console/Commands/ConvertUrlTest.php
@@ -2,31 +2,27 @@
 
 namespace App\Console\Commands;
 
-use DB;
-
 class ConvertUrlTest extends ConvertBase
 {
     protected $signature = 'convert:urltest';
 
     public function handle()
     {
-
         $csv = array_map('str_getcsv', file(base_path().'/app/Console/Commands/ConvertUrlTest.csv'));
 
-        array_walk($csv, function(&$a) use ($csv) {
-          $a = array_combine($csv[0], $a);
+        array_walk($csv, function (&$a) use ($csv) {
+            $a = array_combine($csv[0], $a);
         });
         array_shift($csv);
 
         $this->line(count($csv).' items');
 
-        collect($csv)->slice(0, 6000)->each(function($row, $key) {
-
+        collect($csv)->slice(0, 6000)->each(function ($row, $key) {
             $url = str_replace('http://trip.ee/', '', $row['URL']);
 
             $response = $this->client->head('http://localhost:8000/'.$url, [
                 'exceptions' => false,
-                'allow_redirects' => false
+                'allow_redirects' => false,
             ]);
 
             $code = $response->getStatusCode();
@@ -34,15 +30,9 @@ class ConvertUrlTest extends ConvertBase
             if (in_array($code, [200, 301])) {
 
                 // $this->info($row['Response Code'].' -> '.$code.' '.$url);
-        
             } else {
-
                 $this->error($key.'  '.$row['Response Code'].' -> '.$code.' '.$url);
-
             }
-
         });
-
     }
-
-};
+}
