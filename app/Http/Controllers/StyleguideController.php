c848b235ec7c9d6b7c1494c71a66893f348a9633
kristjanjansen <kristjan.jansen@gmail.com>
2016-08-03 07:54:34 -0400

    Applied fixes from StyleCI

--- app/Http/Controllers/StyleguideController.php
+++ app/Http/Controllers/StyleguideController.php
@@ -2,20 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App;
-use Request;
-use Storage;
-
-class StyleguideController extends Controller {
-
-    public function index() {
-
+class StyleguideController extends Controller
+{
+    public function index()
+    {
         return view('v2.layouts.1col')
             ->with('content', collect()
                 ->push(component('Arc'))
                 ->push(component('Button')->with('title', 'Button'))
             );
-
     }
-
 }
