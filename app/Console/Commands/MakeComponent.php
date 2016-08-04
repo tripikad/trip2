c33039d991602e2ce6dbaa3694b428f2bbb657d0
kristjanjansen <kristjan.jansen@gmail.com>
2016-08-04 08:30:21 -0400

    Applied fixes from StyleCI

--- app/Console/Commands/MakeComponent.php
+++ app/Console/Commands/MakeComponent.php
@@ -7,98 +7,89 @@ use Storage;
 
 class MakeComponent extends Command
 {
-
     protected $signature = 'make:component { name } {--vue}';
 
     public function handle()
     {
-
         $name = $this->argument('name');
-        $dir = "resources/views/v2/components/$name"; 
-        
+        $dir = "resources/views/v2/components/$name";
+
         Storage::disk('root')->makeDirectory($dir);
 
         $element = 'title';
 
         $css = [
-            "@import \"variables\";",
+            '@import "variables";',
             ".$name {",
-            "}",
-            "    .$name"."__"."$element {",
-            "    }\n"
+            '}',
+            "    .$name".'__'."$element {",
+            "    }\n",
         ];
 
         Storage::disk('root')->put("$dir/$name.css", implode("\n\n", $css));
 
         $vue = [
-            "<template>",
-            "",
+            '<template>',
+            '',
             "    <div class=\"$name\" :class=\"isclasses\">",
-            "",
-            "        <div class=\"$name"."__"."$element\">",
-            "",
-            "            {{ title }} {{ message }}",
-            "",
-            "        </div>",
-            "",
-            "    </div>",
-            "",
-            "</template>",
-            "",
-            "<script>",
-            "",
-            "    export default {",
-            "",
-            "        props: {",
-            "            isclasses: { default: '' },", 
-            "            title: { default: '$name' }", 
-            "        },",
-            "",
-            "        data() {",
-            "            return {", 
+            '',
+            "        <div class=\"$name".'__'."$element\">",
+            '',
+            '            {{ title }} {{ message }}',
+            '',
+            '        </div>',
+            '',
+            '    </div>',
+            '',
+            '</template>',
+            '',
+            '<script>',
+            '',
+            '    export default {',
+            '',
+            '        props: {',
+            "            isclasses: { default: '' },",
+            "            title: { default: '$name' }",
+            '        },',
+            '',
+            '        data() {',
+            '            return {',
             "                message: 'from Vue'",
-            "            }",
-            "        }",
-            "",
-            "    }",
-            "",
-            "</script>\n"
+            '            }',
+            '        }',
+            '',
+            '    }',
+            '',
+            "</script>\n",
         ];
 
         $blade = [
-            "@php",
+            '@php',
             "\$title = \$title ?? '';",
-            "@endphp",
+            '@endphp',
             "<div class=\"$name {{ \$isclasses }}\">",
-            "    <div class=\"$name"."__"."$element\">",
-            "        {{ \$title }}",
-            "    </div>",
-            "</div>\n"
+            "    <div class=\"$name".'__'."$element\">",
+            '        {{ $title }}',
+            '    </div>',
+            "</div>\n",
         ];
 
         if ($this->option('vue')) {
-
             Storage::disk('root')->put("$dir/$name.vue", implode("\n", $vue));
-            
+
             $this->info("\nVue component $dir created\n");
             $this->line("Your next steps:\n");
             $this->line("    1. Edit resources/views/v2/main.js to import your new component\n");
             $this->line("    2. Run gulp v2\n");
             $this->line("    3. Add a following line to app/Http/Controllers/StyleguideController.php\n");
             $this->comment("       ->push(component('$name')->with('title', 'I am $name'))\n");
-            
         } else {
-
             Storage::disk('root')->put("$dir/$name.blade.php", implode("\n\n", $blade));
             $this->info("\nBlade component $dir created\n");
             $this->line("Your next steps:\n");
             $this->line("    1. Run gulp v2\n");
             $this->line("    2. Add a following line to app/Http/Controllers/StyleguideController.php\n");
             $this->comment("       ->push(component('$name')->with('title', 'I am $name'))\n");
-            
         }
-
-
     }
-
 }
--- app/Console/Commands/MakeRegion.php
+++ app/Console/Commands/MakeRegion.php
@@ -7,24 +7,22 @@ use Storage;
 
 class MakeRegion extends Command
 {
-
     protected $signature = 'make:region { name }';
 
     public function handle()
     {
-
         $name = $this->argument('name');
-        $dir = "app/Http/Regions"; 
-        
+        $dir = 'app/Http/Regions';
+
         $php = [
-            "<?php",
+            '<?php',
             "namespace App\Http\Regions;",
             "use Illuminate\Http\Request;",
             "class $name {",
             "    public function render(Request \$request, \$content)\n    {",
             "        return component('Body')\n            ->with('body', \$content->body);",
-            "    }",
-            "}\n"
+            '    }',
+            "}\n",
         ];
 
         Storage::disk('root')->put("$dir/$name.php", implode("\n\n", $php));
@@ -33,14 +31,5 @@ class MakeRegion extends Command
         $this->line("Your next steps:\n");
         $this->line("    1. Add a following line to app/Http/Controllers/StyleguideController.php\n");
         $this->comment("       ->push(region('$name', (object) ['body' => 'I am $name region']))\n");
-        
     }
-
 }
-
-
-
-
-
-
-
