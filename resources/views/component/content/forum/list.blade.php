24f6a0654d5980914361737caa7fb27cb4f3ca65
fredpaist <fred023p@gmail.com>
2016-08-05 17:19:32 +0300

    badge

--- resources/views/component/content/forum/list.blade.php
+++ resources/views/component/content/forum/list.blade.php
@@ -101,12 +101,16 @@ modifiers:
 
                     <h3 class="c-forum-list__item-topic">{{ $item['topic'] }}</h3>
 
-                    @if($item['new'])
-                    <span>Unread</span>
-                    @endif
-                    
+
                     @if(isset($item['date']))
                         <p class="c-forum-list__date">{{ $item['date'] }}</p>
+                    @endif
+
+                    @if(isset($item['new']) && $item['new'])
+                        @include('component.badge', [
+                            'modifiers' => 'm-inverted m-red',
+                            'title' => 'unread'
+                        ])
                     @endif
 
                 </div>
