9ee6a612e2257c0145ae7b9310d1dd91e9824af8
kristjanjansen <kristjan.jansen@gmail.com>
2016-08-08 13:31:43 +0300

    Alert component

---
+++ resources/views/v2/components/Alert/Alert.vue
@@ -0,0 +1,55 @@
+<template>
+        
+    <div
+        class="Alert"
+        :class="isclasses"
+        v-show="show"
+        transition="fadeCollapse"
+    >
+
+        <div class="Alert__title">
+            {{ alert }}
+        </div>
+
+        <div class="Alert__close" @click="show = false">
+            <component is="Icon" name="icon-close"></component>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+import Icon from '../Icon/Icon.vue'
+
+export default {
+
+    components: {
+        Icon
+    },
+
+    props: {
+        isclasses: { default: '' },
+        alert: { default: '' }
+    },
+
+    data() {
+        return {
+            show: false
+        }
+    },
+
+    events: {
+        'showAlert': function(alert) {
+            this.alert = alert
+            this.show = true
+            setTimeout(function() {
+                this.show = false
+            }.bind(this), 2000)
+        }
+    }
+
+}
+
+</script>
--- resources/views/v2/main.js
+++ resources/views/v2/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 
+import Alert from './components/Alert/Alert.vue'
 import Arc from './components/Arc/Arc.vue'
 import Icon from './components/Icon/Icon.vue'
 import IconLoader from './components/IconLoader/IconLoader.vue'
@@ -16,15 +17,24 @@ new Vue({
     el: 'body',
 
     components: {
+        Alert,
         Arc,
         Icon,
         IconLoader
     },
 
     events: {
+        showAlert: function(alert) {
+            this.$broadcast('showAlert', alert)
+        }
     },
 
     ready() {
+        this.$http.get('/alert').then(function(res) {
+            if (res.data.info) {
+                this.$emit('showAlert', res.data.info)
+            }
+        })
     }
 
 })
