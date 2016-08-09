570d8d6370500fb4dbbe4a2e0efd10fcb16b2198
fredpaist <fred023p@gmail.com>
2016-08-09 19:51:35 +0300

    searchsmall

---
+++ resources/views/v2/components/SearchSmall/SearchSmall.vue
@@ -0,0 +1,50 @@
+<template>
+
+    <div class="SearchSmall" :class="isclasses">
+
+        <div class="SearchSmall__icon">
+
+            <component
+                is="Icon"
+                icon="icon-search"
+                size="md"
+                @click="ShowField = true"
+            ></component>
+
+        </div>
+
+        <div class="SearchSmall__field" v-if="ShowField" @keyup.enter="Search()">
+
+            <input type="text">{{ search }}</input
+
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+import Icon from '../Icon/Icon.vue'
+
+    export default {
+
+        components: {
+            Icon
+        },
+
+        props: {
+            isclasses: { default: '' },
+            ShowField: false,
+            search: { default: '' }
+        },
+
+        methods: {
+            Search: function (search) {
+                window.location('./search/q='+search);
+            }
+        }
+
+    }
+
+</script>
