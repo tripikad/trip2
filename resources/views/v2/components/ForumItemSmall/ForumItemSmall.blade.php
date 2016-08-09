0ed30c56a82dcdac750f10dc441f3399a8430df9
fredpaist <fred023p@gmail.com>
2016-08-09 15:38:13 +0300

    component

--- resources/views/v2/components/ForumItemSmall/ForumItemSmall.blade.php
+++ resources/views/v2/components/ForumItemSmall/ForumItemSmall.blade.php
@@ -4,12 +4,17 @@ $profile = $profile ?? '';
 $route = $route ?? '';
 $title = $title ?? '';
 $time = $time ?? '';
+$badge = $badge ?? '';
 
 @endphp
 
 
 
     <div class="ForumItemSmall {{ $isclasses }}">
+
+    	<div class="ForumItemSmall__Profile">
+
+        	{!! $profile !!}
 
         </div>
 
@@ -23,11 +28,20 @@ $time = $time ?? '';
 
         	    </div>
 
+                <div class="ForumItemSmall__time">
 
+                    {{ $time }}
+
+                </div>
 
             </div>
 
         </a>
 
+        <div class="ForumItemSmall__badge">
+
+        {!! $badge !!}
+
+        </div>
     </div>
 
