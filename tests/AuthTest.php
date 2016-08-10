01548b513158b9bac4dd29ea2369f86a06cc748c
Mikk Pokk <mikk@internationaleservices.eu>
2016-08-10 03:54:44 +0300

    Fixing the test
    #754

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -9,7 +9,7 @@ class AuthTest extends TestCase
 
     public function test_user_can_register_confirm_and_login()
     {
-        $this->markTestSkipped();
+        //$this->markTestSkipped();
 
         // User can register
 
