aba0c54e743f49d49f45af6ad2ad3e5d336825b0
kristjanjansen <kristjan.jansen@gmail.com>
2016-08-14 20:55:29 +0300

    fixes

--- app/Http/Controllers/V2StyleguideController.php
+++ app/Http/Controllers/V2StyleguideController.php
@@ -91,7 +91,7 @@ class V2StyleguideController extends Controller
                         ->push(component('Body')
                             ->with('body', 'Siit leiad omale sobiva reisikaaslase. Kasuta ka allpool olevat filtrit soovitud tulemuste saamiseks.')
                         )
-                        ->push(component('Link')
+                        ->push(component('LinkBar')
                             ->with('title', 'Kasutustingimused')
                         )
                         ->push(component('Button')
@@ -102,9 +102,7 @@ class V2StyleguideController extends Controller
 
                 ->push(component('Body')
                     ->is('responsive')
-                    ->with('body', '
-                        <p>Ennui flannel offal next level bitters four loko listicle synth church-key you probably havent heard of them keffiyeh sriracha.</p><img src="photos/social.jpg" /><p>Meditation retro shabby chic food truck, master cleanse offal tofu. Taxidermy skateboard post-ironic, freegan helvetica pickled art party tilde kinfolk stumptown.</p><table><tr><th>Synth</th><th>Skateboard</th><th>Freegan</th></tr><tr><td>Skateboard</td><td>Freegan</td><td>Beard</td></tr></table><p>Meditation retro shabby chic food truck, master cleanse offal tofu. Taxidermy skateboard post-ironic, freegan helvetica pickled art party tilde kinfolk stumptown.</p><div class="iframeWrapper"><iframe width="560" height="315" src="https://www.youtube.com/embed/ATr37Yl17jg" frameborder="0" allowfullscreen></iframe></div><p>Trade 90 cold-pressed beard photo booth selvage craft.</p><h3>H3 Gentrify etsy chartreuse</h3><p>Trade 90 cold-pressed beard photo booth selvage craft. <a href="">Ennui flannel offal</a> next level bitters four loko listicle synth church-key.<ul><li>You probably havent</li><li>Heard</li><li>of them</li></ul>keffiyeh sriracha.</p><h4>H4 Gentrify etsy chartreuse</h4><p> trade 90 cold-pressed beard photo booth selvage craft</p>
-                    ')
+                    ->with('body', format_body('Ennui flannel offal next level bitters four loko listicle synth church-key you probably havent heard of them keffiyeh sriracha.\n\nMeditation retro shabby chic food truck, master cleanse offal tofu. Taxidermy skateboard post-ironic, freegan helvetica pickled art party tilde kinfolk stumptown.</p><table><tr><th>Synth</th><th>Skateboard</th><th>Freegan</th></tr><tr><td>Skateboard</td><td>Freegan</td><td>Beard</td></tr></table><p>Meditation retro shabby chic food truck, master cleanse offal tofu. Taxidermy skateboard post-ironic, freegan helvetica pickled art party tilde kinfolk stumptown.'))
                 )
 
                 ->push(component('Button')
@@ -113,14 +111,7 @@ class V2StyleguideController extends Controller
                     ->with('route', route('styleguide.index'))
                 )
 
-                ->push(component('Arc'))
-
-                ->push(region('FooterLight'))
-
-                ->push(region('Footer'))
-
-            )
-            ->with('footer', '');
+            );
     }
 
     public function form()
