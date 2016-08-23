51af9131712c789b75341c772d833f47e19960a9
kristjanjansen <kristjan.jansen@gmail.com>
2016-08-23 22:03:02 +0300

    frontpage

---
+++ app/Http/Controllers/V2FrontpageController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Content;
+
+class V2FrontpageController extends Controller
+{
+    public function index()
+    {
+        $type = 'news';
+
+        $posts = Content::whereType($type)
+            ->whereStatus(1)
+            ->take(6)
+            ->latest()
+            ->get();
+
+        return view('v2.layouts.frontpage')
+
+            ->with('header', region('Masthead', 'Search'))
+
+            ->with('content_first', collect()
+                ->push(component('Block')->with('content', collect(['FrontpageFlightCards'])))
+            )
+
+            ->with('footer', region('Footer'))
+        ;
+    
+    }
+}
