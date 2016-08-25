884d66bac59a13a3b2f9c1e213754daaf7bba4cb
kristjanjansen <kristjan.jansen@gmail.com>
2016-08-25 11:58:24 +0300

    travelmate.show

--- app/Http/Controllers/V2TravelmateController.php
+++ app/Http/Controllers/V2TravelmateController.php
@@ -47,5 +47,102 @@ class V2TravelmateController extends Controller
 
     public function show($id)
     {
+        $type = 'travelmate';
+        $user = auth()->user();
+
+        $post = Content::
+            with(
+                'images',
+                'user',
+                'user.images',
+                'comments',
+                'comments.user',
+                'destinations',
+                'topics'
+            )
+            ->whereStatus(1)
+            ->findOrFail($id);
+
+        $posts = Content::whereType($type)
+            ->whereStatus(1)
+            ->take(3)
+            ->latest()
+            ->get();
+
+        $flights = Content::whereType('flight')
+            ->whereStatus(1)
+            ->latest()
+            ->take(3)
+            ->get();
+
+        return view('v2.layouts.2col')
+
+            ->with('header', region('Masthead', trans("content.$type.index.title")))
+
+            ->with('content', collect()
+                ->push(component('FlightTitle')->with('title', $post->vars()->title))
+                ->push(component('Meta')
+                    ->with('items', collect()
+                        ->push(component('MetaLink')
+                            ->with('title', $post->vars()->created_at)
+                        )
+                        ->pushWhen($user && $user->hasRole('admin'), component('MetaLink')
+                            ->with('title', trans('content.action.edit.title'))
+                            ->with('route', route('flight.edit', [$post]))
+                        )
+                        ->merge($post->destinations->map(function ($tag) {
+                            return component('Tag')->is('orange')->with('title', $tag->name);
+                        }))
+                        ->merge($post->topics->map(function ($tag) {
+                            return component('Tag')->with('title', $tag->name);
+                        }))
+                    )
+                )
+                ->push(component('Body')->is('responsive')->with('body', $post->vars()->body))
+                ->push(component('Block')->with('content', collect()
+                    ->push(component('Button')
+                            ->is('facebook')
+                            ->with('external', true)
+                            ->with('icon', 'icon-facebook')
+                            ->with('title', trans('utils.share.facebook'))
+                            ->with('route', route('utils.share.facebook')))
+                        ->push(component('Button')
+                            ->is('twitter')
+                            ->with('external', true)
+                            ->with('icon', 'icon-twitter')
+                            ->with('title', trans('utils.share.twitter'))
+                            ->with('route', route('utils.share.twitter'))
+                        )
+                    )
+                )
+                ->merge($post->comments->map(function ($comment) {
+                    return region('Comment', $comment);
+                }))
+                //->pushWhen(region('CommentCreateForm', $post))
+
+            )
+
+            ->with('sidebar', collect()
+                ->push(component('Block')->with('content', collect(['ProfileCard'])))
+                ->push(component('Block')->with('content', collect(['DestinationBar'])))
+                ->merge($flights->map(function ($flight) {
+                    return region('FlightCard', $flight);
+                }))
+                ->push(component('Block')->with('content', collect(['5 x ForumRowSmall'])))
+                ->push(component('Promo')->with('promo', 'sidebar_small'))
+            )
+
+            ->with('bottom', collect()
+                ->push(component('Grid3')
+                    ->with('gutter', true)
+                    ->with('items', $posts->map(function ($post) {
+                        return region('TravelmateCard', $post);
+                    })
+                ))
+                ->push(component('Promo')->with('promo', 'footer'))
+            )
+
+            ->with('footer', region('Footer'));
     }
+
 }
