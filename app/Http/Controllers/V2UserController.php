906ae96927cf367f3444c736b5cbdc5413daafbe
fredpaist <fred023p@gmail.com>
2016-08-27 14:38:52 +0300

    gallery on userprofile

--- app/Http/Controllers/V2UserController.php
+++ app/Http/Controllers/V2UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Content;
 
 class V2UserController extends Controller
 {
@@ -21,12 +22,38 @@ class V2UserController extends Controller
             ->take(10)
             ->get();
 
+        $userimages = $user
+            ->contents()
+            ->whereStatus(1)
+            ->where('type', 'photo')
+            ->latest('created_at')
+            ->get()
+                ->map(function ($image) {
+                    return [
+                        'id' => $image->id,
+                        'small' => $image->imagePreset('small_square'),
+                        'large' => $image->imagePreset('large'),
+                        'meta' => component('Meta')->with('items', collect()
+                            ->push(component('MetaLink')
+                                ->with('title', $image->title)
+                            )
+                            ->push(component('MetaLink')
+                                ->with('title', $image->created_at)
+                            )
+                        )->render(),
+                    ];
+                });
+
         return view('v2.layouts.2col')
 
             ->with('header', region('UserHeader', $user))
 
             ->with('content', collect()
-                ->push(component('Block')->with('content', collect(['UserPhoto'])))
+                ->push(component('Block')->with('content', collect()
+                        ->push(component('Gallery')
+                            ->with('images', $userimages)
+                        )
+                    ))
                 ->merge($comments->map(function ($comment) {
                     return region('Comment', $comment);
                 }))
