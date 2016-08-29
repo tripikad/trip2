fbce350963e447093408b7583622238b0123eb9c
fredpaist <fred023p@gmail.com>
2016-08-29 13:55:52 +0300

    Share buttons region

---
+++ app/Http/Regions/Share.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Regions;
+
+class Share
+{
+    public function render()
+    {
+        return component('Grid2')
+            ->with('gutter', true)
+            ->with('items', collect()
+                 ->push(component('Button')
+                    ->is('facebook')
+                    ->with('external', true)
+                    ->with('icon', 'icon-facebook')
+                    ->with('title', trans('utils.share.facebook'))
+                    ->with('route', route('utils.share.facebook'))
+                )
+                ->push(component('Button')
+                    ->is('twitter')
+                    ->with('external', true)
+                    ->with('icon', 'icon-twitter')
+                    ->with('title', trans('utils.share.twitter'))
+                    ->with('route', route('utils.share.twitter'))
+                ));
+    }
+}
