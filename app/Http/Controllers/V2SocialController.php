56974822d3177eee2a48334b173c67c45cef48d2
kristjanjansen <kristjan.jansen@gmail.com>
2016-09-01 13:38:41 +0300

    fix

--- app/Http/Controllers/V2SocialController.php
+++ app/Http/Controllers/V2SocialController.php
@@ -9,10 +9,6 @@ class V2SocialController extends Controller
 {
     public function share($social)
     {
-        if (array_key_exists($social, config('utils.share'))) {
-            return Redirect::away(config("utils.share.$social").URL::previous());
-        }
-
-        return back();
+        return Redirect::away(config("utils.share.$social").URL::previous());
     }
 }
