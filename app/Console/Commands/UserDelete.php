4e12b0aaea58d656fcc6870d3a6b51f338645b40
fredpaist <fred023p@gmail.com>
2016-09-03 09:47:55 +0000

    Applied fixes from StyleCI

--- app/Console/Commands/UserDelete.php
+++ app/Console/Commands/UserDelete.php
@@ -13,36 +13,26 @@ class UserDelete extends Command
 
     public function handle()
     {
-
         $id = $this->argument('id');
 
         $user = User::findorfail($id);
 
-        if ($this->confirm("Do you wish to delete user: $user->name? [yes|no]"))
-        {
+        if ($this->confirm("Do you wish to delete user: $user->name? [yes|no]")) {
+            $user->comments->each(function ($comment) {
+                $comment->delete();
+            });
 
-                $user->comments->each(function ($comment) {
-
+            $user->contents->each(function ($post) {
+                $post->comments->each(function ($comment) {
                     $comment->delete();
-
                 });
 
-                $user->contents->each(function ($post) {
+                $post->delete();
+            });
 
-                    $post->comments->each(function ($comment){
-
-                        $comment->delete();
-
-                    });
-
-                    $post->delete();
-                });
-
-                $user->images->each(function ($image) {
-
-                    $image->delete();
-
-                });
+            $user->images->each(function ($image) {
+                $image->delete();
+            });
 
             $user->delete();
 
