f42282e6b0edf72678bcd1e6668a9758a04a043f
fredpaist <fred023p@gmail.com>
2016-09-03 16:17:21 +0300

    relations remove

--- app/Console/Commands/UserDelete.php
+++ app/Console/Commands/UserDelete.php
@@ -16,21 +16,64 @@ class UserDelete extends Command
         $id = $this->argument('id');
 
         $user = User::findorfail($id);
-
+ 
         if ($this->confirm("Do you wish to delete user: $user->name? [yes|no]")) {
-            $user->comments->each(function ($comment) {
-                $comment->delete();
+
+            $user->flags->each(function ($flag) {
+
+                $flag->delete();
+                
             });
 
+            $user->comments->each(function ($comment) {
+
+                $comment->delete();
+
+            });
+
+            //remove user contents
             $user->contents->each(function ($post) {
+
+                //get destinations->id as array and then remove relationship
+
+                $destinations = $post->destinations()->select('destinations.id')->lists('id')->toArray();
+
+                $post->destinations()->detach($destinations);
+
+                //get topics->id as array and then remove relationship
+
+                $topics = $post->topics()->select('topics.id')->lists('id')->toArray();
+
+                $post->topics()->detach($topics);
+
+                //get carriers->id as array and then remove relationship
+
+                $carriers = $post->carriers()->select('carriers.id')->lists('id')->toArray();
+
+                $post->carriers()->detach($carriers);
+
+                //remove content comments
+
                 $post->comments->each(function ($comment) {
+                    
                     $comment->delete();
+
                 });
 
                 $post->delete();
             });
 
             $user->images->each(function ($image) {
+
+                if($image->imagePresets('original')){
+                    File::delete($image->imagePresets('small'));
+                    File::delete($image->imagePresets('small_square'));
+                    File::delete($image->imagePresets('xsmall_square'));
+                    File::delete($image->imagePresets('original'));
+                    File::delete($image->imagePresets('medium'));
+                    File::delete($image->imagePresets('large'));
+                }
+
                 $image->delete();
             });
 
