6f5b591f12c88e43fe21ae5b5197865e8ea9094c
Mikk Pokk <mikk@internationaleservices.eu>
2016-10-06 01:52:30 +0300

    Use direct item calls

--- resources/views/component/search/results/forum.blade.php
+++ resources/views/component/search/results/forum.blade.php
@@ -1,18 +1,18 @@
 <ul class="c-search-forum">
-@foreach ($content as $row)
+@if (isset($content) && count($content))
+    @foreach ($content as $row)
+    <li class="c-search-forum__item">
+        <h3 class="c-search-forum__item-title">
+            <a href="{{ route($row->type.'.show', [$row->slug]) }}" class="c-search-forum__item-title-link"><span>{{$row->title}}</span> <div class="c-badge m-inverted m-red">{{ $row->comments->count() }}</div></a>
+            <span class="c-search-forum__item-title-date">{{\Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $row->updated_at)->format('d.m.Y H:i')}}</span>
+        </h3>
+        <div class="c-search-forum__item-content">
+        <div class="c-search-forum__item-image"@if ($row->user) style="background-image: url({{ $row->user->imagePreset() }});"@endif></div>
+           <p>{{ (strlen(strip_tags($row->body)) > 300) ? substr(strip_tags($row->body), 0, 300).'...' : strip_tags($row->body) }}</p>
+        </div>
+    </li>
 
-<li class="c-search-forum__item">
-    <h3 class="c-search-forum__item-title">
-        <a href="{{$row->route}}" class="c-search-forum__item-title-link"><span>{{$row->title}}</span> <div class="c-badge m-inverted m-red">{{$row->comments_count}}</div></a>
-        <span class="c-search-forum__item-title-date">{{\Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $row->updated_at)->format('d.m.Y H:i')}}</span>
-    </h3>
-    <div class="c-search-forum__item-content">
-    <div class="c-search-forum__item-image" style="background-image: url({{$row->user_img}});"></div>
-       <!--  <p>… uudiseid vägivallast ja inimröövidest saabus pidevalt. Kuigi ka praegu leidub Kolumbias piirkondi, kuhu ei soovitata reisida, on üldine olukord turvalisusega <span>Vietnam</span> riigis oluliselt parem, kui paarkümmend aastat tagasi. Kuritegevuse olulise …</p> -->
-       <p>{{$row->short_body_text}}</p>
-    </div>
-</li>
-
-@endforeach
+    @endforeach
+@endif
 </ul>
                     
