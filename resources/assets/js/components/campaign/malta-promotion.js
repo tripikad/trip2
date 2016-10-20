f922e2efe402ddd90571d6350f773c6566342a22
kkallasm <kaspar68@gmail.com>
2016-10-20 19:32:36 +0300

    header and cache

---
+++ resources/assets/js/components/campaign/malta-promotion.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+    $(".malta-promotion__svg-wrap svg").on("click", function(e) {
+        $(".malta-promotion").slideUp();
+        setCookie( "malta-stip-closed", true, 100 );
+    });
+
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length,c.length);
+            }
+        }
+        return "";
+    }
+});
