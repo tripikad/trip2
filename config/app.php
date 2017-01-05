0888264fe5557ecf0af164ae954f54e0b7d49ad0
kristjanjansen <kristjan.jansen@gmail.com>
2017-01-05 21:51:50 +0200

    Cusco markdown v2 (#1360)

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         Jenssegers\Date\DateServiceProvider::class,
         GrahamCampbell\HTMLMin\HTMLMinServiceProvider::class,
         Cviebrock\EloquentSluggable\ServiceProvider::class,
-        GrahamCampbell\Markdown\MarkdownServiceProvider::class,
+        Indal\Markdown\MarkdownServiceProvider::class,
 
         Msurguy\Honeypot\HoneypotServiceProvider::class,
         Roumen\Sitemap\SitemapServiceProvider::class,
@@ -249,7 +249,7 @@ return [
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
         'Ekko'      => Laravelista\Ekko\Facades\Ekko::class,
         'Honeypot'  => Msurguy\Honeypot\HoneypotFacade::class,
-        'Markdown' => GrahamCampbell\Markdown\Facades\Markdown::class,
+        'Markdown' => Indal\Markdown\Facade::class,
 
     ],
 
