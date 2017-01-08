e126565076d26df96c17603cd80a8a8bff99e2bc
kristjanjansen <kristjan.jansen@gmail.com>
2017-01-08 21:15:10 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/V2BlogController.php
+++ app/Http/Controllers/V2BlogController.php
@@ -2,17 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class V2BlogController extends Controller
 {
     public function index()
     {
-
     }
 
     public function show($id)
     {
-        
     }
 }
