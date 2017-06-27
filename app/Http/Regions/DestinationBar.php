42e9415aa285d79cfc44ca7c1e3d9f9659832396
Mikk Pokk <mikk@internationaleservices.eu>
2017-06-27 19:37:00 +0300

    Duplicate destination queries fix

--- app/Http/Regions/DestinationBar.php
+++ app/Http/Regions/DestinationBar.php
@@ -2,18 +2,41 @@
 
 namespace App\Http\Regions;
 
+use App\Destination;
+
 class DestinationBar
 {
     public function render($destination, $is = '')
     {
-        $parentLength = mb_strlen($destination
-            ->getAncestors()
-            ->onlyLast(2)
-            ->map(function ($parent) {
-                return $parent->vars()->name;
-            })
-            ->implode('')
-        );
+        //dd($destination->getAncestors()->onlyLast(2));
+        $parent_destinations = app('db')->select('select
+        `d1`.`id` as `d1_id`, `d1`.`parent_id` as `d1_parent_id`, `d1`.`name` as `d1_name`, `d1`.`slug` as `d1_slug`,
+        `d2`.`id` as `d2_id`, `d2`.`parent_id` as `d2_parent_id`, `d2`.`name` as `d2_name`, `d2`.`slug` as `d2_slug` from `destinations` `d1` LEFT JOIN `destinations` `d2` ON `d2`.`id` = `d1`.`parent_id` where `d1`.`id` = ' . (int) $destination->parent_id);
+
+        if ($parent_destinations && count($parent_destinations)) {
+            $parent_destinations = $parent_destinations[0];
+        }
+
+        $parentLength = mb_strlen($parent_destinations->d1_name . ' ' . ($parent_destinations->d2_name ?? ''));
+        $parentDestinations = collect([]);
+
+        for ($i = 2; $i >= 1; --$i) {
+            if ($i == 1 && $parentLength > 25) {
+                break;
+            } else {
+                if ($parent_destinations->{'d'.$i.'_id'}) {
+                    $new_destination = new Destination;
+                    $new_destination->id = $parent_destinations->{'d'.$i.'_id'};
+                    $new_destination->parent_id = $parent_destinations->{'d'.$i.'_parent_id'};
+                    $new_destination->name = $parent_destinations->{'d'.$i.'_name'};
+                    $new_destination->slug = $parent_destinations->{'d'.$i.'_slug'};
+
+                    $parentDestinations = $parentDestinations->merge([$new_destination]);
+                }
+            }
+        }
+
+        //dd($parentDestinations);
 
         return component('DestinationBar')
             ->is($is)
@@ -21,7 +44,7 @@ class DestinationBar
             ->with('route', route('destination.show', [$destination]))
             ->with('parents', region(
                 'DestinationParents',
-                $destination->getAncestors()->onlyLast($parentLength > 25 ? 1 : 2),
+                $parentDestinations,
                 $short = true
             )
         );
--- app/Http/Regions/DestinationParents.php
+++ app/Http/Regions/DestinationParents.php
@@ -6,14 +6,18 @@ class DestinationParents
 {
     public function render($parents, $short = false)
     {
-        return component('Meta')
-            ->with('items', $parents->map(function ($parent) use ($short) {
-                $title = $short ? $parent->vars()->shortName : $parent->vars()->name;
+        $loopCount = 0;
+        $parentsCount = $parents->count();
 
+        return component('Meta')
+            ->with('items', $parents->map(function ($parent) use ($short, &$loopCount, $parentsCount) {
+                ++$loopCount;
+                $title = $short ? $parent->vars()->shortName : $parent->vars()->name;
+                
                 return component('MetaLink')
                     ->is('large')
                     ->is('white')
-                    ->with('title', $title.' › ')
+                    ->with('title', $title . ($loopCount != $parentsCount ? ' › ' : ''))
                     ->with('route', route('destination.show', [$parent]));
             }));
     }
