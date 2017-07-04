3444bd1a424362b77797f99b66444f9deb4e4f0a
fredpaist <fred.paist@gmail.com>
2017-07-04 17:39:24 +0300

    do not convert all to misc

--- app/Console/Commands/ForumMiscTopic.php
+++ app/Console/Commands/ForumMiscTopic.php
@@ -31,13 +31,31 @@ class ForumMiscTopic extends Command
     {
         $this->info('finding all content where topic has id of 5000');
 
+        $urls =  collect([
+            'https://trip.ee/foorum/uldfoorum/filmid-dokumentaalfilmid-ja-tv-seriaalid',
+            'https://trip.ee/foorum/uldfoorum/eesti-mainimine-filmides',
+            'https://trip.ee/foorum/uldfoorum/oleks-vaja-eestis-aatikat',
+            'https://trip.ee/foorum/uldfoorum/uurimistoeoe-lugemiseks',
+            'https://trip.ee/foorum/uldfoorum/filipiinide-naised',
+            'https://trip.ee/foorum/uldfoorum/kiire-id-kaardi-lugeja-tai-krabi',
+            'https://trip.ee/foorum/uldfoorum/huvitavad-templid-passis',
+            'https://trip.ee/foorum/uldfoorum/asjade-toimetamine-kanaaridelt-eestisse',
+            ]);
+
+        $content = $urls->map(function($url) {
+            return collect(explode('/',$url))->last();
+        })->map(function($slug) {
+            return \App\Content::whereSlug($slug)->first();
+        });
+
+        /*
         $items = Content::whereType('forum')->join('content_topic', 'content_topic.content_id', '=', 'contents.id')
             ->select('contents.*')
-            ->where('content_topic.topic_id', '=', 5000)->get();
+            ->where('content_topic.topic_id', '=', 5000)->get();*/
 
         $this->info('Converting content');
 
-        $items->each(function ($item) {
+        $content->each(function ($item) {
             $item->type = 'misc';
             $item->timestamps = false;
             $item->save();
@@ -47,7 +65,7 @@ class ForumMiscTopic extends Command
 
         $this->info('delete the topic misc');
 
-        $topic = Topic::find(5000);
+        $topic = Topic::findOrFail(5000);
         $topic->delete();
     }
 }
