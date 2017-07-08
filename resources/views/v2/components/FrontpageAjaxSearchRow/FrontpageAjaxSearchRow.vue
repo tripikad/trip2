4746010832f53280d94e747e7bd517fb9f3b702b
Mikk Pokk <mikk@internationaleservices.eu>
2017-07-08 15:43:27 +0300

    FrontpageAjaxSearchRow component created

---
+++ resources/views/v2/components/FrontpageAjaxSearchRow/FrontpageAjaxSearchRow.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <div class="FrontpageAjaxSearchRow" :class="isclasses">
+
+        <div v-if="index != 'attributes'" class="FrontpageAjaxSearchRow__item">
+            <div class="FrontpageAjaxSearchRow__title">
+
+                {{ result.title }}
+
+            </div>
+        </div>
+        <a v-else class="FrontpageAjaxSearchRow__footer" :href="result.route">
+            {{ result.total }} Tulemust kokku
+        </a>
+
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+
+        props: {
+            isclasses: { default: '' },
+            result: { default: [] },
+            index: { default: '' }
+        }
+
+    }
+
+</script>
