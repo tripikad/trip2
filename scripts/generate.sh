757a11990c9248ac316a831a313b1caeb793567e
kristjanjansen <kristjan.jansen@gmail.com>
2017-07-09 17:03:12 +0300

    cleanup

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,7 +2,8 @@
 
 if [ $# -eq 0 ]; then
     printf "\nUsage: ./generate.sh http://your-local-trip.dev"
-    printf "\n\nYou need to have csvkit installed\n\e[4;37mhttp://csvkit.readthedocs.io/en/1.0.2/tutorial/1_getting_started.html#installing-csvkit\e[0;37m\n\n"
+    printf "\n\nThe script will take around 10 minutes to run"
+    printf "\n\nYou will need to have csvkit installed\n\e[4;37mhttp://csvkit.readthedocs.io/en/1.0.2/tutorial/1_getting_started.html#installing-csvkit\e[0;37m\n\n"
 else
     
 printf "\n\e[0;32mGetting the data...\e[0;37m\n\n"
@@ -13,7 +14,7 @@ curl https://raw.githubusercontent.com/datasets/country-codes/master/data/countr
 
 curl $1/api/destinations/data > data/destinations_data.json
 
-printf "\n\e[0;32mGenerating the dots for the map (this will take around 10min) ...\e[0;37m\n\n"
+printf "\n\e[0;32mGenerating the dots for the Dotmap component...\e[0;37m\n\n"
 
 node generate_dots.js > ../config/dots.php
 
--- scripts/generate_dots.js
+++ scripts/generate_dots.js
@@ -3,26 +3,40 @@ var countries = require('./data/countries.json')
 var codes = require('./data/codes.json')
 var destinations = require('./data/destinations_data.json')
 
+// Utilities
+
+// Mapping 3-letter ISO country codes to 2-letter ISO codes
+
 function iso3to2(iso3) {
     var code = codes.find(code => code['ISO3166-1-Alpha-3'] === iso3)
     return code ? code['ISO3166-1-Alpha-2'] : iso3
 }
+
+// Mapping the 3-letter ISO code to trip.ee's Destination model id
 
 function iso3toId(iso3) {
     var destination = destinations.find(destination => destination.code === iso3to2(iso3))
     return destination ? destination.id : 0;
 }
 
+// Starting dot generation
+
 var lat = 0
 var lon = 0
 var step = 2.5
 var halfStep = step / 2
 
-var circles = []
+var dots = []
+
+// Loop over world Y-axis
 
 for (var lat = 80; lat > -80; lat -= step) {
 
+    // Loop over world X-axis
+
     for (var lon = -180; lon < 180; lon += step) {
+
+        // Set up a square polygon with dot coordinates in the center
 
         var box = turf.polygon([[
             [lon - halfStep, lat + halfStep],
@@ -31,10 +45,17 @@ for (var lat = 80; lat > -80; lat -= step) {
             [lon - halfStep, lat - halfStep],
             [lon - halfStep, lat + halfStep]
         ]])
-        var circle = turf.circle(turf.point([lon, lat]), halfStep, 8, 'degrees')
-        var circle = turf.point([lon, lat]);
 
-        circle.properties.countries = []
+        // Set up the dot to be generated as a GeoJSON object
+        // for easier debuggability with tools like geojson.io
+
+        var dot = turf.point([lon, lat]);
+
+        dot.properties.countries = []
+
+        // Loop over countries to find the intersection with our
+        // square. If there is an intersection, add the country to
+        // the dot.properties.countries array
 
         countries.features
             .slice(0, 2)
@@ -47,7 +68,7 @@ for (var lat = 80; lat > -80; lat -= step) {
                     turf.polygon(country.geometry.coordinates)
                 )
                 if (intersection !== undefined) {
-                    circle.properties.countries.push(iso3toId(country.id))
+                    dot.properties.countries.push(iso3toId(country.id))
                 }
             }
             if (country.geometry.type === 'MultiPolygon') {
@@ -57,28 +78,29 @@ for (var lat = 80; lat > -80; lat -= step) {
                         turf.polygon(polygon)
                     )
                     if (intersection !== undefined) {
-                        circle.properties.countries.push(iso3toId(country.id))
+                        dot.properties.countries.push(iso3toId(country.id))
                     }
                 })
             }
         })
 
-        if (circle.properties.countries.length > 0) {
-            circles.push(circle)
+        if (dot.properties.countries.length > 0) {
+            dots.push(dot)
         }
 
     }
 }
 
+// Generate the PHP array output
+
 console.log('<?php\n\nreturn [\n')
 
-circles.forEach(circle => {
+dots.forEach(dot => {
     console.log(`    [
-        'destination_ids' => [${circle.properties.countries}],
-        'lat' => ${circle.geometry.coordinates[1]},
-        'lon' => ${circle.geometry.coordinates[0]}
+        'destination_ids' => [${dot.properties.countries}],
+        'lat' => ${dot.geometry.coordinates[1]},
+        'lon' => ${dot.geometry.coordinates[0]}
     ],`);
 })
 
 console.log('\n];\n')
-
