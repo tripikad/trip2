c25521d25bc2f0f12b56a86b40e7ee7e7ddea1d7
Mikk Pokk <mikk@internationaleservices.eu>
2017-07-13 22:59:02 +0300

    UnreadContent model created

---
+++ app/UnreadContent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UnreadContent extends Model
+{
+
+    protected $appends = [
+        'unread',
+    ];
+
+    public function content()
+    {
+        return $this->hasOne('App\Content', 'id', 'content_id');
+    }
+
+    public function getUnreadAttribute()
+    {
+        $unread_timestamp = strtotime($this->read_at);
+        $unread_count = 0;
+
+        if ($this->content) {
+            if ($this->content->comments) {
+                foreach ($this->content->comments as $comment)
+                {
+                    if (strtotime($comment->created_at) > $unread_timestamp) {
+                        ++$unread_count;
+                    }
+                }
+            }
+        }
+
+        return (int) $unread_count;
+    }
+
+}
