a7f1347662ff2c32da8abc182d6b09a7e79fc292
Mikk Pokk <mikk@internationaleservices.eu>
2017-07-13 23:15:32 +0300

    Except timestamp

--- app/UnreadContent.php
+++ app/UnreadContent.php
@@ -10,6 +10,8 @@ class UnreadContent extends Model
         'unread',
     ];
 
+    protected $except_timestamp_to = '2017-07-13 23:10:00'; // todo
+
     public function content()
     {
         return $this->hasOne('App\Content', 'id', 'content_id');
@@ -18,16 +20,18 @@ class UnreadContent extends Model
     public function getUnreadAttribute()
     {
         $unread_timestamp = strtotime($this->read_at);
+        $except_timestamp = strtotime($this->except_timestamp_to);
         $unread_count = 0;
 
         if ($this->content) {
-            if (strtotime($this->content->created_at) > $unread_timestamp) {
+            $content_created_at_timestamp = strtotime($this->content->created_at);
+            if ($content_created_at_timestamp > $unread_timestamp && $except_timestamp < $content_created_at_timestamp) {
                 $unread_count += 1;
             }
 
             if ($this->content->comments) {
                 foreach ($this->content->comments as $comment) {
-                    if (strtotime($comment->created_at) > $unread_timestamp) {
+                    if (strtotime($comment->created_at) > $unread_timestamp && $except_timestamp < $unread_timestamp) {
                         ++$unread_count;
                     }
                 }
