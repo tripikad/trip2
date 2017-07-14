8e6a5f0305693c67dd4b67a92d68b330559b2628
Mikk Pokk <mikk@internationaleservices.eu>
2017-07-14 13:52:20 +0300

    UnreadContent methods

--- app/UnreadContent.php
+++ app/UnreadContent.php
@@ -3,41 +3,81 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class UnreadContent extends Model
 {
-    protected $appends = [
-        'unread',
-    ];
-
-    protected $except_timestamp_to = '2017-07-13 23:10:00'; // todo
+    public $timestamps = false;
+    protected static $except_timestamp_to = '2017-07-14 14:00:00'; // todo
 
     public function content()
     {
         return $this->hasOne('App\Content', 'id', 'content_id');
     }
 
-    public function getUnreadAttribute()
+    public function getUnread(Content $content)
     {
         $unread_timestamp = strtotime($this->read_at);
-        $except_timestamp = strtotime($this->except_timestamp_to);
-        $unread_count = 0;
+        $except_timestamp = strtotime(UnreadContent::$except_timestamp_to);
+        $unread_data = [
+            'count' => 0,
+            'first_comment_id' => null,
+        ];
 
         if ($this->content) {
-            $content_created_at_timestamp = strtotime($this->content->created_at);
+            $content_created_at_timestamp = strtotime($content->created_at);
             if ($content_created_at_timestamp > $unread_timestamp && $except_timestamp < $content_created_at_timestamp) {
-                $unread_count += 1;
+                $unread_data['count'] += 1;
             }
 
-            if ($this->content->comments) {
-                foreach ($this->content->comments as $comment) {
-                    if (strtotime($comment->created_at) > $unread_timestamp && $except_timestamp < $unread_timestamp) {
-                        ++$unread_count;
+            if ($content->comments) {
+                foreach ($content->comments as &$comment) {
+                    if (strtotime($comment->created_at) > $unread_timestamp && $except_timestamp < strtotime($comment->created_at)) {
+                        if (! $unread_data['first_comment_id']) {
+                            $unread_data['first_comment_id'] = $comment->id;
+                        }
+
+                        ++$unread_data['count'];
                     }
                 }
             }
         }
 
-        return (int) $unread_count;
+        return (int) $unread_data;
+    }
+
+    public static function getUnreadContent(Content $content)
+    {
+        $unread_content = $content->unread_content;
+
+        $unread_data = [
+            'count' => 0,
+            'first_comment_id' => null,
+        ];
+
+        if ($unread_content && auth()->check()) {
+            return (int) $unread_content->getUnread($content);
+        } elseif (auth()->check()) {
+            $except_timestamp = strtotime(UnreadContent::$except_timestamp_to);
+            $content_created_at_timestamp = strtotime($content->created_at);
+
+            if ($except_timestamp < $content_created_at_timestamp) {
+                $unread_data['count'] += 1;
+            }
+
+            if ($content->comments) {
+                foreach ($content->comments as &$comment) {
+                    if ($except_timestamp < strtotime($comment->created_at)) {
+                        if (! $unread_data['first_comment_id']) {
+                            $unread_data['first_comment_id'] = $comment->id;
+                        }
+
+                        ++$unread_data['count'];
+                    }
+                }
+            }
+        }
+
+        return $unread_data;
     }
 }
