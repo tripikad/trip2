de3ab78151e1ff87fa5d11e2b950f515b8014fe2
Mikk Pokk <mikk@internationaleservices.eu>
2017-07-17 19:46:15 +0300

    New definition added

--- resources/lang/et/search.php
+++ resources/lang/et/search.php
@@ -10,6 +10,7 @@ return [
     'tab.flight' => 'Lennupakkumised',
 
     'results.noresults' => 'Tulemusi ei leitud',
+    'results.noresults_frontpage' => 'Detailsesse otsingusse liikumiseks vajuta luubile või klahvile "Enter".',
     'results.all' => 'Kõik tulemused',
     'results.newest_first' => 'Järjesta: uuemad tulemused esimesena',
     'results.relevance_first' => 'Järjesta: täpsemad vasted esimesena',
