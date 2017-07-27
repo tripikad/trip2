d777493e7086b26e09451258aed3854749147136
fredpaist <fred.paist@gmail.com>
2017-07-27 12:24:21 +0300

    travelmateStart component (#1624)

---
+++ resources/views/v2/components/TravelmateStart/TravelmateStart.vue
@@ -0,0 +1,54 @@
+<template>
+
+    <div class="TravelmateStart" :class="isclasses">
+        <div class="TravelmateStart__fields">
+
+            <div class="TravelmateStart__field"
+                    v-for="(date, index) in dates"
+                    @click="atClick(index)"
+                    :class="{ 'TravelmateStart--active': activeIndex === index }">
+                {{ date.title }}
+            </div>
+
+        </div>
+
+        <input type="hidden" :value="localValue" :name="name">
+
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+
+        props: {
+            isclasses: { default: '' },
+            title: { default: '' },
+            dates: { default: []},
+            value: { default: ''},
+            name: { default: ''}
+        },
+
+        data: () => ({
+            activeIndex: 0,
+            localValue: false,
+        }),
+
+        methods: {
+            atClick: function(id) {
+                this.activeIndex = id;
+                return this.localValue = this.dates[id].datetime.date;
+            }
+        },
+
+        mounted() {
+            this.activeIndex = this.value;
+            return this.localValue = this.value
+                ?  this.dates[this.value].datetime.date
+                : this.dates[0].datetime.date;
+        }
+
+    }
+
+</script>
