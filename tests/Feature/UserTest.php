ec3c374676af97662bb1e25ec3571a72872a304e
kristjanjansen <kristjan.jansen@gmail.com>
2017-07-30 21:50:31 +0300

    user_can_edit_profile test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Hash;
 use App\User;
 use Tests\BrowserKitTestCase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -9,6 +10,43 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class UserTest extends BrowserKitTestCase
 {
     use DatabaseTransactions;
+
+    public function test_user_can_edit_its_profile()
+    {
+        $user_editing_profile = factory(User::class)->create();
+
+        $this->actingAs($user_editing_profile)
+            ->visit("user/$user_editing_profile->id")
+            ->click(trans('menu.user.edit.profile'))
+            ->seePageIs("user/$user_editing_profile->id/edit") 
+            ->type('manny', 'name')
+            ->type('manny@calavera.com', 'email')
+            ->type('calavera', 'password')
+            ->type('calavera', 'password_confirmation')
+            ->type('Manny Calavera', 'real_name')
+            ->check('real_name_show')
+            ->type('A travel agent at afterworld', 'description')
+            ->check('notify_message')
+            ->type('http://facebook.com', 'contact_facebook')
+            ->type('http://instagram.com', 'contact_instagram')
+            ->type('http://twitter.com', 'contact_twitter')
+            ->type('http://calavera.com', 'contact_homepage')
+            ->press(trans('user.edit.submit'))
+            ->seePageIs("user/$user_editing_profile->id") 
+            ->see('manny')
+            ->seeInDatabase('users', [
+                'id' => $user_editing_profile->id,
+                'name' => 'manny',
+                'email' => 'manny@calavera.com',
+                //'password' => Hash::make('calavera'), // FIXME
+                'real_name' => 'Manny Calavera',
+                'contact_facebook' => 'http://facebook.com',
+                'contact_instagram' => 'http://instagram.com',
+                'contact_twitter' => 'http://twitter.com',
+                'contact_homepage' => 'http://calavera.com'
+            ]);
+
+    }
 
     public function test_user_can_upload_profile_image()
     {
