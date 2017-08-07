b4fe98ca57d6adf9fcf05e81ca2cde5ef151acf9
kristjanjansen <kristjan.jansen@gmail.com>
2017-08-07 09:40:16 +0300

    removing app/Main.php and related leftovers

--- app/Destination.php
+++ app/Destination.php
@@ -23,63 +23,9 @@ class Destination extends Baum\Node
         return $this->belongsTo('App\User');
     }
 
-    public function vars()
-    {
-        return new V2DestinationVars($this);
-    }
-
     public function flags()
     {
         return $this->morphMany('App\Flag', 'flaggable');
-    }
-
-    // Also present in V2DestinationVars
-
-    public function usersHaveBeen()
-    {
-        return $this->flags->where('flag_type', 'havebeen');
-    }
-
-    public function usersWantsToGo()
-    {
-        return $this->flags->where('flag_type', 'wantstogo');
-    }
-
-    // ...
-
-    public function getPopular()
-    {
-        return Cache::remember('popular.destinations.root'.$this->id, config('cache.destination.getPopular'), function () {
-            return Destination::find($this->id)
-                ->descendantsAndSelf()
-                ->with('flags')
-                ->get()
-                ->transform(function ($item) {
-                    $item->attributes['usersHaveBeen'] = $item->usersHaveBeen()->count();
-                    $item->attributes['usersWantsToGo'] = $item->usersWantsToGo()->count();
-                    $item->attributes['interestTotal'] = $item->attributes['usersHaveBeen'] + $item->attributes['usersWantsToGo'];
-
-                    return $item;
-                });
-        });
-    }
-
-    public static function getNames()
-    {
-        return Cache::rememberForever('destination.names', function () {
-            return collect(Main::collectionAsSelect(
-                [],
-                ' › ',
-                Destination::select('name', 'id', 'parent_id')->get()
-            ))->sort();
-        });
-    }
-
-    public function getDestinationParent()
-    {
-        if ($this) {
-            return $this->parent()->first();
-        }
     }
 
     public function sluggable()
@@ -89,5 +35,12 @@ class Destination extends Baum\Node
                 'source' => 'name',
             ],
         ];
+    }
+
+    // V2
+    
+    public function vars()
+    {
+        return new V2DestinationVars($this);
     }
 }
