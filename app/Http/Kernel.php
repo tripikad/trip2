79a7face826559959a2ebc3be5890e5b7b48a42e
Mikk Pokk <mikk@internationaleservices.eu>
2017-08-12 14:53:45 +0300

    UserActivity class added inside web middleware groups

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,6 +34,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\UserActivity::class,
             \App\Http\Middleware\XssProtection::class,
             \App\Http\Middleware\LoggedCookie::class,
         ],
