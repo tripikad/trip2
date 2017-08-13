e6a5c38a7aa8b59e8b9a8e69a66d6369bcd7e80e
Mikk Pokk <mikk@internationaleservices.eu>
2017-08-13 19:39:57 +0300

    newsletter_subscriptions table

---
+++ database/migrations/2017_08_13_165209_create_newsletter_subscriptions_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsletterSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('newsletter_subscriptions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('newsletter_type_id')->unsigned()->index();
+            $table->integer('user_id')->nullable()->unsigned()->index();
+            $table->integer('destination_id')->nullable()->unsigned()->index();
+            $table->string('email');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+
+            $table->foreign('newsletter_type_id')->references('id')->on('newsletter_types')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->foreign('destination_id')->references('id')->on('destinations')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('newsletter_subscriptions');
+    }
+}
