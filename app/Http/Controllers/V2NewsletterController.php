2728183ce6ea1bbe8381a981069c48be153a080e
Mikk Pokk <mikk@internationaleservices.eu>
2017-08-13 20:37:49 +0300

    newsletter list layout set-up

--- app/Http/Controllers/V2NewsletterController.php
+++ app/Http/Controllers/V2NewsletterController.php
@@ -8,6 +8,22 @@ class V2NewsletterController extends Controller
 {
     public function index()
     {
-
+        return layout('2col')
+            ->with('background', component('BackgroundMap'))
+            ->with('color', 'gray')
+            ->with('header', region('ForumHeader', collect()
+                ->push(component('Title')
+                    ->with('title', trans('menu.admin.newsletter'))
+                )
+                ->push(component('BlockHorizontal')
+                    ->with('content', region('ForumLinks'))
+                )
+            ))
+            ->with('content', collect()
+                ->push('Sisu')
+            )
+            ->with('sidebar', collect())
+            ->with('footer', region('FooterLight'))
+            ->render();
     }
 }
