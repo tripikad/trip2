0e8d6eaac4495edecb356205a27880c8ca71aaa5
Mikk Pokk <mikk@internationaleservices.eu>
2017-08-15 17:47:54 +0300

    newsletter definitions for subscription form

--- resources/lang/et/newsletter.php
+++ resources/lang/et/newsletter.php
@@ -6,4 +6,19 @@ return [
     'button.preview' => 'Vaata eelvaadet',
     'button.edit' => 'Muuda kirja',
 
+    'subscribe.info.general' => 'Liitumisel saadetakse sulle kuni 3 korda nädalas uusi lennupakkumisi. Täpsemate pakkumiste reeglite määramiseks <a href="'.route('login.form').'">logi sisse</a> ja tule lennupakkumiste leheküljele tagasi. Kui sul pole veel kasutajat siis esmalt <a href="'.route('register.form').'">registreeru kasutajaks</a>.',
+
+    'subscribe.flight.heading' => 'Lennupakkumised e-posti',
+    'subscribe.field.email' => 'Sinu e-post',
+    'subscribe.field.destinations' => 'Vali kuni :max sihtkohta',
+    'subscribe.field.price_error' => 'Soovin kõiki veahinna pakkumisi',
+    'button.subscribe_flight' => 'Kinnitan',
+
+    'button.subscribe_processing' => 'Palun oota...',
+
+    'error.already_subscribed' => 'Oled juba liitunud lennupakkumiste uudiskirjaga!',
+
+    'subscribed.flight.successfully' => 'Oled lennupakkumiste teavituste saajate nimekirja lisatud!',
+    'subscribed.flight.detailed.successfully' => 'Sinu lennupakkumiste teavituste muudatused on salvestatud!',
+
 ];
