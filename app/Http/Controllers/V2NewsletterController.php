87034dd86635db05c3987732255e0cfca5336dc7
Mikk Pokk <mikk@internationaleservices.eu>
2017-08-15 18:27:11 +0300

    Security fix

--- app/Http/Controllers/V2NewsletterController.php
+++ app/Http/Controllers/V2NewsletterController.php
@@ -7,6 +7,7 @@ use App\NewsletterType;
 use Illuminate\Http\Request;
 use Illuminate\Mail\Markdown;
 use App\NewsletterSubscription;
+use App\Http\Regions\FlightNewsletterSubscribe;
 
 class V2NewsletterController extends Controller
 {
@@ -106,79 +107,85 @@ class V2NewsletterController extends Controller
 
             $destination_ids = $request->input('destinations') ?? [];
             $price_error = $request->input('price_error') ? 1 : 0;
+            
+            if (count($destination_ids) > FlightNewsletterSubscribe::$max) {
+                $errors[] = trans('newsletter.max_limit_exceeded', ['max' => FlightNewsletterSubscribe::$max]);
+            }
 
-            $insert_destinations = $destination_ids;
-            $insert_price_error = $price_error;
+            if (! count($errors)) {
+                $insert_destinations = $destination_ids;
+                $insert_price_error = $price_error;
 
-            foreach ($previous_subscriptions as &$previous_subscription) {
-                if ($previous_subscription->price_error) {
-                    $insert_price_error = 0;
-                } elseif ($previous_subscription->destination_id && in_array($previous_subscription->destination_id, $insert_destinations)) {
-                    unset($insert_destinations[array_search($previous_subscription->destination_id, $insert_destinations)]);
+                foreach ($previous_subscriptions as &$previous_subscription) {
+                    if ($previous_subscription->price_error) {
+                        $insert_price_error = 0;
+                    } elseif ($previous_subscription->destination_id && in_array($previous_subscription->destination_id, $insert_destinations)) {
+                        unset($insert_destinations[array_search($previous_subscription->destination_id, $insert_destinations)]);
+                    }
+
+                    if ($previous_subscription->active) {
+                        if ($previous_subscription->destination_id && ! in_array($previous_subscription->destination_id, $destination_ids)) {
+                            $deactivate_subscription_ids[] = $previous_subscription->id;
+                        } elseif ($previous_subscription->price_error && ! $price_error) {
+                            $deactivate_subscription_ids[] = $previous_subscription->id;
+                        }
+                    } else {
+                        if ($previous_subscription->destination_id && in_array($previous_subscription->destination_id, $destination_ids)) {
+                            $activate_subscription_ids[] = $previous_subscription->id;
+                        } elseif ($previous_subscription->price_error && $price_error) {
+                            $activate_subscription_ids[] = $previous_subscription->id;
+                        }
+                    }
                 }
 
-                if ($previous_subscription->active) {
-                    if ($previous_subscription->destination_id && ! in_array($previous_subscription->destination_id, $destination_ids)) {
-                        $deactivate_subscription_ids[] = $previous_subscription->id;
-                    } elseif ($previous_subscription->price_error && ! $price_error) {
-                        $deactivate_subscription_ids[] = $previous_subscription->id;
-                    }
-                } else {
-                    if ($previous_subscription->destination_id && in_array($previous_subscription->destination_id, $destination_ids)) {
-                        $activate_subscription_ids[] = $previous_subscription->id;
-                    } elseif ($previous_subscription->price_error && $price_error) {
-                        $activate_subscription_ids[] = $previous_subscription->id;
+                // Deactivate
+                if (count($deactivate_subscription_ids)) {
+                    NewsletterSubscription::whereIn('id', $deactivate_subscription_ids)->update([
+                        'active' => 0,
+                    ]);
+                }
+
+                // Activate
+                if (count($activate_subscription_ids)) {
+                    NewsletterSubscription::whereIn('id', $activate_subscription_ids)->update([
+                        'active' => 1,
+                    ]);
+                }
+
+                // Insert new record
+                $insert_records = [];
+                if (count($insert_destinations)) {
+                    foreach ($insert_destinations as &$destination_id) {
+                        $insert_records[] = [
+                            'newsletter_type_id' => $id,
+                            'user_id' => $user->id,
+                            'price_error' => 0,
+                            'destination_id' => $destination_id,
+                            'active' => 1,
+                            'created_at' => Carbon::now(),
+                            'updated_at' => Carbon::now(),
+                        ];
                     }
                 }
-            }
 
-            // Deactivate
-            if (count($deactivate_subscription_ids)) {
-                NewsletterSubscription::whereIn('id', $deactivate_subscription_ids)->update([
-                    'active' => 0,
-                ]);
-            }
-
-            // Activate
-            if (count($activate_subscription_ids)) {
-                NewsletterSubscription::whereIn('id', $activate_subscription_ids)->update([
-                    'active' => 1,
-                ]);
-            }
-
-            // Insert new record
-            $insert_records = [];
-            if (count($insert_destinations)) {
-                foreach ($insert_destinations as &$destination_id) {
+                if ($insert_price_error) {
                     $insert_records[] = [
                         'newsletter_type_id' => $id,
                         'user_id' => $user->id,
-                        'price_error' => 0,
-                        'destination_id' => $destination_id,
+                        'price_error' => 1,
+                        'destination_id' => null,
                         'active' => 1,
                         'created_at' => Carbon::now(),
                         'updated_at' => Carbon::now(),
                     ];
                 }
-            }
 
-            if ($insert_price_error) {
-                $insert_records[] = [
-                    'newsletter_type_id' => $id,
-                    'user_id' => $user->id,
-                    'price_error' => 1,
-                    'destination_id' => null,
-                    'active' => 1,
-                    'created_at' => Carbon::now(),
-                    'updated_at' => Carbon::now(),
-                ];
-            }
+                if (count($insert_records)) {
+                    NewsletterSubscription::insert($insert_records);
+                }
 
-            if (count($insert_records)) {
-                NewsletterSubscription::insert($insert_records);
+                $info = trans('newsletter.subscribed.flight.detailed.successfully');
             }
-
-            $info = trans('newsletter.subscribed.flight.detailed.successfully');
         } elseif ($newsletter_type->type == 'flight_general' && ! $user) {
             $this->validate($request, [
                 'e-post' => 'required|email',
