63d2209ced456ab71f1b1ce111754011aabd62b3
Mikk Pokk <mikk@internationaleservices.eu>
2017-08-17 02:57:30 +0300

    NewsletterSentSubscriber model created

---
+++ app/NewsletterSentSubscriber.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NewsletterSentSubscriber extends Model
+{
+    // Setup
+
+    protected $dates = ['started_at', 'ended_at'];
+
+    // Relations
+}
