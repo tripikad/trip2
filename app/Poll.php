ddefd7deab86231a607054aab24343c010735290
kristjanjansen <kristjan.jansen@gmail.com>
2017-08-16 22:46:38 +0000

    Apply fixes from StyleCI

--- app/Poll.php
+++ app/Poll.php
@@ -7,21 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Poll extends Model
 {
     protected $table = 'poll';
-    
+
     protected $fillable = ['name', 'start_date', 'end_date', 'type'];
-    
+
     public $timestamps = false;
-    
+
     public function content()
     {
         return $this->belongsTo('App\Content', 'id', 'id');
     }
-    
+
     public function poll_fields()
     {
         return $this->hasMany('App\PollField', 'poll_id', 'id');
     }
-    
+
     public function poll_results()
     {
         return $this->hasMany('App\PollResult', 'poll_id', 'id');
