2ee98a372379687fbe939f1ec21f48e84639221a
kristjanjansen <kristjan.jansen@gmail.com>
2017-08-17 01:52:02 +0300

    Apply fixes from StyleCI (#1708)

--- app/PollResult.php
+++ app/PollResult.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class PollResult extends Model
 {
     protected $table = 'poll_results';
-    
+
     public function poll()
     {
         return $this->belongsTo('App\Poll', 'poll_id', 'id');
     }
-    
+
     public function poll_field()
     {
         return $this->belongsTo('App\PollField', 'field_id', 'field_id');
