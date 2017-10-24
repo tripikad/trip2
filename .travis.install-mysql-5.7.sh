90b0b54d42f498d7277ff97659bad27c57753237
kkallasm <kaspar68@gmail.com>
2017-10-25 00:32:42 +0300

    travis <3

--- .travis.install-mysql-5.7.sh
+++ .travis.install-mysql-5.7.sh
@@ -1,6 +1,1 @@
-echo "TEST123"
-wget http://dev.mysql.com/get/mysql-apt-config_0.3.2-1ubuntu14.04_all.deb
-sudo dpkg -i mysql-apt-config_0.3.2-1ubuntu14.04_all.deb
-sudo apt-get update -q
-sudo apt-get install mysql-server-5.7
-sudo mysql_upgrade
+
