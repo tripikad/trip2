999cf6652afec90023f192be37568fd30a28c46c
kristjanjansen <kristjan.jansen@gmail.com>
2017-12-02 22:36:27 +0200

    test2

--- app/Console/Commands/GenerateSimilars.php
+++ app/Console/Commands/GenerateSimilars.php
@@ -1,166 +1,166 @@
 <?php
 
-namespace App\Console\Commands;
+// namespace App\Console\Commands;
 
-use App\Content;
-use Illuminate\Console\Command;
+// use App\Content;
+// use Illuminate\Console\Command;
 
-class GenerateSimilars extends Command
-{
-    protected $signature = 'generate:similars';
+// class GenerateSimilars extends Command
+// {
+//     protected $signature = 'generate:similars';
 
-    protected $totalSize;
-    protected $chunkSize;
+//     protected $totalSize;
+//     protected $chunkSize;
 
-    public function __construct()
-    {
-        parent::__construct();
+//     public function __construct()
+//     {
+//         parent::__construct();
 
-        $this->totalSize = config('similars.totalsize');
-        $this->chunkSize = config('similars.chunksize');
-    }
+//         $this->totalSize = config('similars.totalsize');
+//         $this->chunkSize = config('similars.chunksize');
+//     }
 
-    public function handle()
-    {
-        $totalSize = $this->totalSize;
-        $chunkSize = $this->chunkSize;
-        $chunkCount = $totalSize / $chunkSize;
+//     public function handle()
+//     {
+//         $totalSize = $this->totalSize;
+//         $chunkSize = $this->chunkSize;
+//         $chunkCount = $totalSize / $chunkSize;
 
-        $count = 0;
+//         $count = 0;
 
-        $progress = $this->output->createProgressBar($totalSize);
+//         $progress = $this->output->createProgressBar($totalSize);
 
-        $this->info("\nGenerating similar content\n");
+//         $this->info("\nGenerating similar content\n");
 
-        Content::orderBy('updated_at', 'desc')->chunk($chunkSize, function ($contentChunk) use (&$count, $chunkCount, $progress) {
-            $contentChunk->each(function ($content) use ($progress) {
-                $this->generateSimilars($content);
-                $progress->advance();
-            });
-            $count++;
-            if ($count >= $chunkCount) {
-                return false;
-            }
-        });
+//         Content::orderBy('updated_at', 'desc')->chunk($chunkSize, function ($contentChunk) use (&$count, $chunkCount, $progress) {
+//             $contentChunk->each(function ($content) use ($progress) {
+//                 $this->generateSimilars($content);
+//                 $progress->advance();
+//             });
+//             $count++;
+//             if ($count >= $chunkCount) {
+//                 return false;
+//             }
+//         });
 
-        $this->info("\n\nDone\n");
-    }
+//         $this->info("\n\nDone\n");
+//     }
 
-    protected function generateSimilars($content)
-    {
-        $similars = collect();
+//     protected function generateSimilars($content)
+//     {
+//         $similars = collect();
 
-        collect(['forum', 'news', 'flight'])
-            ->each(function ($type) use (&$similars, $content) {
-                $similars->put($type, $this->generateSimilarsOfType($content, $type));
-            });
+//         collect(['forum', 'news', 'flight'])
+//             ->each(function ($type) use (&$similars, $content) {
+//                 $similars->put($type, $this->generateSimilarsOfType($content, $type));
+//             });
 
-        $content['meta->similars'] = $similars;
-        $content->timestamps = false;
-        $content->save();
-    }
+//         $content['meta->similars'] = $similars;
+//         $content->timestamps = false;
+//         $content->save();
+//     }
 
-    protected function generateSimilarsOfType($sourceContent, $type)
-    {
-        $similars = collect();
+//     protected function generateSimilarsOfType($sourceContent, $type)
+//     {
+//         $similars = collect();
 
-        $totalSize = $this->totalSize;
-        $chunkSize = $this->chunkSize;
-        $chunkCount = $totalSize / $chunkSize;
+//         $totalSize = $this->totalSize;
+//         $chunkSize = $this->chunkSize;
+//         $chunkCount = $totalSize / $chunkSize;
 
-        $count = 0;
+//         $count = 0;
 
-        Content::orderBy('updated_at', 'desc')->whereNotIn('id', [$sourceContent->id])
-            ->whereType($type)
-            ->chunk($chunkSize, function ($targetContentChunk) use ($sourceContent, &$similars, &$count, $chunkCount) {
-                $targetContentChunk->each(
-                    function ($targetContent) use ($sourceContent, &$similars) {
-                        $similar = $this->getSimilar($sourceContent, $targetContent);
-                        if ($similar) {
-                            $similars->push(collect()
-                                ->put('items', $similar)
-                                ->put('score', $this->getSimilarScore($similar))
-                            );
-                        }
-                    }
-                );
+//         Content::orderBy('updated_at', 'desc')->whereNotIn('id', [$sourceContent->id])
+//             ->whereType($type)
+//             ->chunk($chunkSize, function ($targetContentChunk) use ($sourceContent, &$similars, &$count, $chunkCount) {
+//                 $targetContentChunk->each(
+//                     function ($targetContent) use ($sourceContent, &$similars) {
+//                         $similar = $this->getSimilar($sourceContent, $targetContent);
+//                         if ($similar) {
+//                             $similars->push(collect()
+//                                 ->put('items', $similar)
+//                                 ->put('score', $this->getSimilarScore($similar))
+//                             );
+//                         }
+//                     }
+//                 );
 
-                $similars = $similars->filter(function ($s) {
-                    return $s['score'] >= 0.3;
-                })
-                ->take(3);
+//                 $similars = $similars->filter(function ($s) {
+//                     return $s['score'] >= 0.3;
+//                 })
+//                 ->take(3);
 
-                $count++;
+//                 $count++;
 
-                if ($similars->count() >= 3 || $count >= $chunkCount) {
-                    return false;
-                }
-            });
+//                 if ($similars->count() >= 3 || $count >= $chunkCount) {
+//                     return false;
+//                 }
+//             });
 
-        return $similars->values();
-    }
+//         return $similars->values();
+//     }
 
-    // protected function getSimilar($sourceContent, $targetContent)
-    // {
-    //     $sourceMeta = $sourceContent->meta['keywords'];
-    //     $targetMeta = $targetContent->meta['keywords'];
+//     protected function getSimilar($sourceContent, $targetContent)
+//     {
+//         $sourceMeta = $sourceContent->meta['keywords'];
+//         $targetMeta = $targetContent->meta['keywords'];
 
-    //     if (! $sourceMeta || ! $targetMeta) {
-    //         return false;
-    //     }
+//         if (! $sourceMeta || ! $targetMeta) {
+//             return false;
+//         }
 
-    //     $sourceKeywords = collect($sourceMeta)
-    //         ->filter(function ($keyword) {
-    //             return $keyword['score'] >= 0.35;
-    //         })
-    //         ->keyBy('name');
+//         $sourceKeywords = collect($sourceMeta)
+//             ->filter(function ($keyword) {
+//                 return $keyword['score'] >= 0.35;
+//             })
+//             ->keyBy('name');
 
-    //     $targetKeywords = collect($targetMeta)
-    //         ->filter(function ($keyword) {
-    //             return $keyword['score'] >= 0.35;
-    //         })
-    //         ->keyBy('name');
+//         $targetKeywords = collect($targetMeta)
+//             ->filter(function ($keyword) {
+//                 return $keyword['score'] >= 0.35;
+//             })
+//             ->keyBy('name');
 
-    //     $similar = $sourceKeywords->keys()
-    //         ->intersect($targetKeywords->keys())
-    //         ->map(function ($key) use ($sourceKeywords, $targetKeywords, $targetContent) {
-    //             return [
-    //                 'title' => $targetContent->title,
-    //                 'id' => $targetContent->id,
-    //                 'source' => $sourceKeywords[$key],
-    //                 'target' => $targetKeywords[$key],
-    //                 'created' => $targetContent->created_at->diffForHumans(null, true),
-    //                 'updated' => $targetContent->updated_at->diffForHumans(null, true),
-    //             ];
-    //         })
-    //         ->values()
-    //         ->unique('id');
+//         $similar = $sourceKeywords->keys()
+//             ->intersect($targetKeywords->keys())
+//             ->map(function ($key) use ($sourceKeywords, $targetKeywords, $targetContent) {
+//                 return [
+//                     'title' => $targetContent->title,
+//                     'id' => $targetContent->id,
+//                     'source' => $sourceKeywords[$key],
+//                     'target' => $targetKeywords[$key],
+//                     'created' => $targetContent->created_at->diffForHumans(null, true),
+//                     'updated' => $targetContent->updated_at->diffForHumans(null, true),
+//                 ];
+//             })
+//             ->values()
+//             ->unique('id');
 
-    //     return $similar->isNotEmpty() ? $similar : false;
-    // }
+//         return $similar->isNotEmpty() ? $similar : false;
+//     }
 
-    protected function getSimilarScore($similar)
-    {
-        $destinationCount = $similar->filter(function ($similarItem) {
-            return $similarItem['source']['type'] == 'destination';
-        })
-        ->count();
+//     protected function getSimilarScore($similar)
+//     {
+//         $destinationCount = $similar->filter(function ($similarItem) {
+//             return $similarItem['source']['type'] == 'destination';
+//         })
+//         ->count();
 
-        $topicCount = $similar->filter(function ($similarItem) {
-            return $similarItem['source']['type'] == 'topic';
-        })
-        ->count();
+//         $topicCount = $similar->filter(function ($similarItem) {
+//             return $similarItem['source']['type'] == 'topic';
+//         })
+//         ->count();
 
-        $scoreMap = [
-        //   destinations
-        //   0   1   2+
-            [0.0, 0.3, 0.5],  // 0  topics
-            [0.2, 0.5, 0.7],  // 1  topics
-            [0.2, 0.7, 0.9],  // 2+ topics
-        ];
+//         $scoreMap = [
+//         //   destinations
+//         //   0   1   2+
+//             [0.0, 0.3, 0.5],  // 0  topics
+//             [0.2, 0.5, 0.7],  // 1  topics
+//             [0.2, 0.7, 0.9],  // 2+ topics
+//         ];
 
-        return $scoreMap
-            [min([$topicCount, 2])]
-            [min([$destinationCount, 2])];
-    }
-}
+//         return $scoreMap
+//             [min([$topicCount, 2])]
+//             [min([$destinationCount, 2])];
+//     }
+// }
