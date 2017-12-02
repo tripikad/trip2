d6c68ed547dd1eb487c5c717f461bf62d536aba1
kristjanjansen <kristjan.jansen@gmail.com>
2017-12-02 22:39:05 +0200

    hm

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\ForumMiscTopic::class,
         \App\Console\Commands\Newsletter::class,
         \App\Console\Commands\GenerateKeywords::class,
-        \App\Console\Commands\GenerateSimilars::class,
+        //\App\Console\Commands\GenerateSimilars::class,
     ];
 
     protected function schedule(Schedule $schedule)
