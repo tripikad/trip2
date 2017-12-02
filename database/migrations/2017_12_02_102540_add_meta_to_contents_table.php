ecb32cfc72b58e3a190f181fde804349e0351f9c
kristjanjansen <kristjan.jansen@gmail.com>
2017-12-02 14:14:38 +0200

    Apply fixes from StyleCI (#1754)

--- database/migrations/2017_12_02_102540_add_meta_to_contents_table.php
+++ database/migrations/2017_12_02_102540_add_meta_to_contents_table.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\User;
+use App\Content;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
-use App\User;
-use App\Content;
 
 class AddMetaToContentsTable extends Migration
 {
