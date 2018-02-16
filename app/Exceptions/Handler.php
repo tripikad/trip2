87c7cae58ca544fd96603a0c6cd603460a67d644
Mikk Pokk <mikk@internationaleservices.eu>
2018-02-16 17:09:05 +0200

    Show debug information by IP

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,7 @@ use Illuminate\Database\QueryException;
 use Illuminate\Auth\AuthenticationException;
 use Symfony\Component\Debug\Exception\FatalErrorException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Config;
 
 class Handler extends ExceptionHandler
 {
@@ -58,6 +59,16 @@ class Handler extends ExceptionHandler
     {
         if ($e instanceof \Illuminate\Database\Eloquent\ModelNotFoundException) {
             abort(404);
+        }
+
+        if (strpos(config('app.debug_ips'), ',') !== false) {
+            $ips = explode(',', config('app.debug_ips'));
+        } else {
+            $ips = [];
+        }
+
+        if (in_array(request()->ip(), $ips)) {
+            Config::set('app.debug', true);
         }
 
         if (! config('app.debug') && ($e instanceof \ErrorException || $e instanceof FatalErrorException || $e instanceof QueryException)) {
