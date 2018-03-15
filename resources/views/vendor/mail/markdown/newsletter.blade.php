fb090b086fc694141f8a25241f3262d0d79ef2a9
Mikk Pokk <mikk@internationaleservices.eu>
2018-03-15 02:28:01 +0200

    newsletter markdown fix

--- resources/views/vendor/mail/markdown/newsletter.blade.php
+++ resources/views/vendor/mail/markdown/newsletter.blade.php
@@ -1,33 +1,33 @@
 @component('mail::newsletter_layout')
-    {{-- Header --}}
-    @slot('header')
-        @component('mail::header', ['url' => config('app.url')])
-            {{ config('app.name') }}
-        @endcomponent
-    @endslot
+{{-- Header --}}
+@slot('header')
+@component('mail::header', ['url' => config('app.url')])
+{{ config('app.name') }}
+@endcomponent
+@endslot
 
-    {{-- Body --}}
-    {{ $slot }}
+{{-- Body --}}
+{{ $slot }}
 
-    {{-- Subcopy --}}
-    @isset($subcopy)
-        @slot('subcopy')
-            @component('mail::subcopy')
-                {{ $subcopy }}
-            @endcomponent
-        @endslot
-    @endisset
+{{-- Subcopy --}}
+@isset($subcopy)
+@slot('subcopy')
+@component('mail::subcopy')
+{{ $subcopy }}
+@endcomponent
+@endslot
+@endisset
 
-    {{-- Footer --}}
-    @slot('footer')
-        @component('mail::footer')
-            @if (isset($unsubscribe_route) && $unsubscribe_route)
-                [Loobun uudiskirjast]({{ $unsubscribe_route }})
+{{-- Footer --}}
+@slot('footer')
+@component('mail::footer')
+@if (isset($unsubscribe_route) && $unsubscribe_route)
+[Loobun uudiskirjast]({{ $unsubscribe_route }})
 
 
-            @endif
-            {{ trans('site.footer.copyright', ['current_year' =>  \Carbon\Carbon::now()->year]) }}
-        @endcomponent
-    @endslot
+@endif
+{{ trans('site.footer.copyright', ['current_year' =>\Carbon\Carbon::now()->year]) }}
+@endcomponent
+@endslot
 @endcomponent
 
