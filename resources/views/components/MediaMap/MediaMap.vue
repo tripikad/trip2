cbb95f5df79850d20a6e13d497af5b51594187a0
kkallasm <kaspar68@gmail.com>
2018-03-21 00:30:15 +0200

    mediamap

---
+++ resources/views/components/MediaMap/MediaMap.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div class="MediaMap" :class="isclasses">
+
+        <h1>Map on siin</h1>
+        <svg
+                class="MediaMap_svg"
+                :class="isclasses"
+        >
+
+            <use :xlink:href="'#tripee_logo'"></use>
+
+        </svg>
+
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+
+        props: {
+            isclasses: { default: '' },
+            width: 100,
+            height: 100
+        },
+
+        methods: {
+
+        },
+
+        mounted() {
+
+        }
+
+    }
+
+</script>
