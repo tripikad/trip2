27f9b2e73beb7fd7f46af115ed31e61de4441033
kristjanjansen <kristjan.jansen@gmail.com>
2018-04-23 17:26:11 +0300

    init

---
+++ tests/Browser/FrontpageTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class FrontpageTest extends DuskTestCase
+{
+    public function testFrontpage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->whenAvailable('.FrontpageSearch__input', function ($search) {
+                    $search->assertSourceHas('Kuhu sa soovid minna?');
+                });
+        });
+    }
+}
