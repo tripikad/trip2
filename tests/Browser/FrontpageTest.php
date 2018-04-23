994866464b2df6df96eb9394117b7d651f4a8774
kristjanjansen <kristjan.jansen@gmail.com>
2018-04-23 21:45:47 +0300

    fix

--- tests/Browser/FrontpageTest.php
+++ tests/Browser/FrontpageTest.php
@@ -12,17 +12,16 @@ class FrontpageTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
                 ->assertSourceHas('Autoriõigused')
-                ->dump();
         });
     }
 
-    // public function testFrontpageJS()
-    // {
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->visit('/')
-    //             ->whenAvailable('.FrontpageSearch__input', function ($search) {
-    //                 $search->assertSourceHas('Kuhu sa soovid minna?');
-    //             }, 10);
-    //     });
-    // }
+    public function testFrontpageJS()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->whenAvailable('.FrontpageSearch__input', function ($search) {
+                    $search->assertSourceHas('Kuhu sa soovid minna?');
+                }, 10);
+        });
+    }
 }
