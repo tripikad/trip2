16b51594b81044166f419c4e87daad54edeee9d0
kristjanjansen <kristjan.jansen@gmail.com>
2018-04-23 14:02:17 +0300

    fix

--- app/Console/Commands/MakeComponent.php
+++ app/Console/Commands/MakeComponent.php
@@ -83,8 +83,7 @@ class MakeComponent extends Command
             Storage::disk('root')->put("$dir/$name.blade.php", implode("\n\n", $blade));
             $this->info("\nBlade component $dir created\n");
             $this->line("Your next steps:\n");
-            $this->line("    1. Run npm run build\n");
-            $this->line("    2. Add a following line to app/Http/Controllers/V2ExperimentsController.php\n");
+            $this->line("    1. Add a following line to app/Http/Controllers/V2ExperimentsController.php\n");
             $this->comment("       ->push(component('$name')->with('title', 'I am $name'))\n");
         }
     }
