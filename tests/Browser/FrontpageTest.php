b4af34778a70691b921633856473607c5cdb613c
kristjanjansen <kristjan.jansen@gmail.com>
2018-04-24 13:35:10 +0300

    Dusk tests starter (#1830)
    Initial browser testing setup and simple frontpage test for php-rendered and js-rendered content

---
+++ tests/Browser/FrontpageTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+
+class FrontpageTest extends DuskTestCase
+{
+    public function testFrontpage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->assertSourceHas('Autoriõigused');
+        });
+    }
+
+    public function testFrontpageJS()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->whenAvailable('.FrontpageSearch__input', function ($search) {
+                    $search->assertSourceHas('Kuhu sa soovid minna?');
+                });
+        });
+    }
+}
