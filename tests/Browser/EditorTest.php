d65d3cdf5b6727657aaebaa7c45380a0bdea09ed
kristjanjansen <kristjan.jansen@gmail.com>
2018-04-25 11:43:01 +0300

    init

---
+++ tests/Browser/EditorTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use App\User;
+use App\Content;
+
+class EditorTest extends DuskTestCase
+{
+    public function test_superuser_can_create_content_with_editor()
+    {
+        $super_user = factory(User::class)->create(['role' => 'superuser']);
+
+        foreach (['flight', 'news',] as $type) {
+            $this->browse(function ($browser) use ($super_user, $type) {
+                $browser
+                //->resize(1200, 10)
+                ->loginAs($super_user)
+                ->visit("$type/create")
+                ->type('title', "Hola editores de titulo de $type")
+                ->click('textarea[readonly=readonly]')
+                ->pause(1000) // Loading the editor
+                ->keys('.Editor__source textarea', "Hola editores de cuerpo de $type")
+                ->pause(2000) // Waiting for ajax-based preview
+                ->assertSeeIn('.Editor__target', "Hola editores de cuerpo de $type")
+                ->screenshot($type)
+                ->click('.Editor__toolbarRight > .Editor__tool')
+                ->press('Lisa')
+                ->assertSee("Hola editores de titulo de $type")
+            ;
+            });
+
+            // Cleanup
+
+            $content = Content::whereTitle("Hola editores de titulo de $type")
+                ->whereBody("Hola editores de cuerpo de $type")
+                ->whereType($type)
+                ->whereUserId($super_user->id)
+                ->first()
+                ->delete();
+        }
+
+        $super_user->delete();
+    }
+}
