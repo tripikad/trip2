790f5ae8ca1e5e42b299fe03380281620f119e51
kristjanjansen <kristjan.jansen@gmail.com>
2018-04-28 15:42:23 +0300

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,90 +1,97 @@
 var fs = require("fs");
 var path = require("path");
 var webpack = require("webpack");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 var SpriteLoaderPlugin = require("svg-sprite-loader/plugin");
 var CleanWebpackPlugin = require("clean-webpack-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
-    entry: {
-        main: "./resources/views/main.js",
-    },
-    output: {
-        path: path.resolve(__dirname, "./public/dist"),
-        publicPath: "/dist/",
-        filename: "[name].[chunkhash:6].js",
-        chunkFilename: "main.[name].[chunkhash:6].js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: "vue-loader"
-            },
-            {
-                test: /\.js$/,
-                loader: "babel-loader",
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    use: [
-                        { loader: "css-loader", options: { importLoaders: 1 } },
-                        "postcss-loader"
-                    ]
-                })
-            },
-            {
-                test: /\.svg$/,
-                use: [
-                    {
-                        loader: "svg-sprite-loader",
-                        options: {
-                            extract: true,
-                            spriteFilename: "[chunkname].svg"
-                        }
-                    },
-                    "svgo-loader"
-                ]
-            },
-            {
-                test: /\.(ttf|woff|woff2|eot)$/,
-                loader: "file-loader",
-                options: {
-                    name: "[name].[ext]"
-                }
-            }
+  entry: {
+    main: "./resources/views/main.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "./public/dist"),
+    publicPath: "/dist/",
+    filename: "[name].[chunkhash:6].js",
+    chunkFilename: "main.[name].[chunkhash:6].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 }
+          },
+          "postcss-loader"
         ]
-    },
-    plugins: [
-        new ExtractTextPlugin('[name].[chunkhash:6].css'),
-        new SpriteLoaderPlugin(),
-        new CleanWebpackPlugin('./public/dist'),
-        function() {
-            this.plugin("done", stats => {
-                var assets = stats.toJson().assetsByChunkName;
-                var manifest = {
-                    js: assets.main.find(asset => path.extname(asset) === ".js"),
-                    css: assets.main.find(asset => path.extname(asset) === ".css"),
-                    svg: "main.svg"
-                };
-                fs.writeFileSync(
-                    path.join(__dirname, 'public/dist/manifest.json'),
-                    JSON.stringify(manifest)
-                );
-            });
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "svg-sprite-loader",
+            options: {
+              extract: true,
+              spriteFilename: "[chunkname].svg"
+            }
+          },
+          "svgo-loader"
+        ]
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]"
         }
-    ],
-    resolve: {
-        alias: {
-            vue$: "vue/dist/vue.esm.js"
-        }
-    },
-    performance: {
-        hints: false
-    },
-    devtool: "#eval-source-map"
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash:6].css"
+    }),
+    new SpriteLoaderPlugin(),
+    new CleanWebpackPlugin("./public/dist")
+    // function() {
+    //     this.plugin("done", stats => {
+    //         var assets = stats.toJson().assetsByChunkName;
+    //         var manifest = {
+    //             js: assets.main.find(asset => path.extname(asset) === ".js"),
+    //             css: assets.main.find(asset => path.extname(asset) === ".css"),
+    //             svg: "main.svg"
+    //         };
+    //         fs.writeFileSync(
+    //             path.join(__dirname, 'public/dist/manifest.json'),
+    //             JSON.stringify(manifest)
+    //         );
+    //     });
+    // }
+  ],
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.esm.js"
+    }
+  },
+  performance: {
+    hints: false
+  },
+  devtool: "#eval-source-map",
+  stats: { entrypoints: false }
 };
 
 if (process.env.NODE_ENV === "production") {
