8b7c050a70727f0154cd79b1efaab4faabc8f385
kristjanjansen <kristjan.jansen@gmail.com>
2018-04-30 09:41:56 +0300

    Components F + FrontpageSearchTest

--- resources/views/components/FormButtonProcess/FormButtonProcess.vue
+++ resources/views/components/FormButtonProcess/FormButtonProcess.vue
@@ -12,27 +12,23 @@
 </template>
 
 <script>
+export default {
+    props: {
+        isclasses: { default: '' },
+        title: { default: '' },
+        processingtitle: { default: '' },
+        id: { default: '' }
+    },
 
-    export default {
+    data: () => ({
+        processing: false
+    }),
 
-        props: {
-            isclasses: { default: '' },
-            title: { default: '' },
-            processingtitle: { default: '' },
-            id: { default: '' }
-        },
-
-        data: () => ({
-            processing: false,
-        }),
-
-        methods: {
-            onClick() {
-                this.processing = true
-                document.getElementById(this.id).submit()
-            }
+    methods: {
+        onClick() {
+            this.processing = true
+            document.getElementById(this.id).submit()
         }
-
     }
-
+}
 </script>
---
+++ tests/Browser/FrontpageSearchTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Content;
+use App\Searchable;
+use App\User;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class FrontpageSearchTest extends DuskTestCase
+{
+    public function test_anybody_can_search_in_frontpage()
+    {
+        $regular_user = factory(User::class)->create();
+
+        $content = factory(Content::class)->create([
+            'user_id' => factory(User::class)->create()->id,
+            'type' => 'forum',
+            'title' => 'Donde esta nuesta playa',
+        ]);
+
+        Searchable::unguard();
+
+        $searchable = Searchable::create([
+            'user_id' => $content->user->id,
+            'content_id' => $content->id,
+            'content_type' => $content->type,
+            'comment_id' => null,
+            'destination_id' => null,
+            'title' => $content->title,
+            'body' => $content->body,
+            'created_at' => $content->created_at,
+            'updated_at' => $content->updated_at,
+        ]);
+
+        Searchable::reguard();
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->pause(100)
+                ->assertSourceHas('Kuhu sa soovid minna?')
+                ->type('.FrontpageSearch__input', 'Donde esta nuesta playa')
+                ->pause(500)
+                ->assertSeeIn('.FrontpageSearch__results', 'Donde esta nuesta playa')
+                ->click('.FrontpageSearchItem')
+                ->assertPathBeginsWith('/foorum/uldfoorum/');
+        });
+
+        $searchable->delete();
+        $content->delete();
+        $regular_user->delete();
+    }
+}
