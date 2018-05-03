09fc4721a8a93905c07873c62cd936cd08fadf4b
kristjanjansen <kristjan.jansen@gmail.com>
2018-05-03 20:56:02 +0300

    Apply fixes from StyleCI (#1884)

--- app/Hashers/Md5Hasher.php
+++ app/Hashers/Md5Hasher.php
@@ -2,22 +2,17 @@
 
 namespace App\Hashers;
 
-use RuntimeException;
 use Illuminate\Hashing\HashManager;
 
 class Md5Hasher extends HashManager
 {
     public function make($value, array $options = [])
     {
-
         return $this->driver()->make(md5($value), $options);
-
     }
 
     public function check($value, $hashedValue, array $options = [])
     {
-        
         return $this->driver()->check(md5($value), $hashedValue, $options);
-    
     }
 }
