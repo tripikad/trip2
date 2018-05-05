695e562d43d70d7ae952ef984a9afe757afb0233
kristjanjansen <kristjan.jansen@gmail.com>
2018-05-05 14:05:52 +0000

    Apply fixes from StyleCI

--- routes/console.php
+++ routes/console.php
@@ -3,32 +3,34 @@
 function formatBytes($size, $precision = 3)
 {
     $base = log($size, 1024);
-    $suffixes = array('', 'K', 'M', 'G', 'T');
+    $suffixes = ['', 'K', 'M', 'G', 'T'];
 
-    return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
+    return round(pow(1024, $base - floor($base)), $precision).' '.$suffixes[floor($base)];
 }
 
-
-function filepath($name, $width, $quality = 75, $step1 = null, $step2 = null) {
-    $filename = collect([$name,'w'.($width < 100 ? '0'.$width : $width),'q'.$quality,$step1,$step2])
+function filepath($name, $width, $quality = 75, $step1 = null, $step2 = null)
+{
+    $filename = collect([$name, 'w'.($width < 100 ? '0'.$width : $width), 'q'.$quality, $step1, $step2])
         ->reject(function ($name) {
             return empty($name);
         })
         ->implode('_');
+
     return storage_path('tmp/'.$filename.'.jpeg');
 }
 
 Artisan::command('image:list {name}', function ($name) {
     $files = collect(Storage::disk('root')->files('storage/tmp/'))
-        ->filter(function($filepath) use ($name) {
+        ->filter(function ($filepath) use ($name) {
             return starts_with(basename($filepath), $name);
         })
         ->splice(1)
         ->reverse()
         ->values()
-        ->map(function($filepath) {
+        ->map(function ($filepath) {
             $filename = basename($filepath);
             $size = Storage::disk('root')->size($filepath);
+
             return [
                 'name' => basename($filepath),
                 'formattedSize' => formatBytes($size),
@@ -37,28 +39,27 @@ Artisan::command('image:list {name}', function ($name) {
         })
         ->sortByDesc('ratio');
 
-    
     $baseSize = $files->first()['ratio'];
 
-    $files = $files->map(function($fileData) use ($baseSize) {
-            $fileData['ratio'] =  round($fileData['ratio'] / $baseSize * 100,1);
-            return $fileData;
-        })
-        ->map(function($fileData) {
-            if (!strstr($fileData['name'], 'w900') && !strstr($fileData['name'], 'w700') && !strstr($fileData['name'], 'w300') && !strstr($fileData['name'], 'w180') && !strstr($fileData['name'], 'w075')) {
+    $files = $files->map(function ($fileData) use ($baseSize) {
+        $fileData['ratio'] = round($fileData['ratio'] / $baseSize * 100, 1);
+
+        return $fileData;
+    })
+        ->map(function ($fileData) {
+            if (! strstr($fileData['name'], 'w900') && ! strstr($fileData['name'], 'w700') && ! strstr($fileData['name'], 'w300') && ! strstr($fileData['name'], 'w180') && ! strstr($fileData['name'], 'w075')) {
                 return collect($fileData)->map(function ($data) {
                     return '<comment>'.$data.'</comment>';
                 });
             }
+
             return $fileData;
         });
 
-    $this->table(['name', 'formattedSize','ratio %'], $files);
-
+    $this->table(['name', 'formattedSize', 'ratio %'], $files);
 });
 
 Artisan::command('image:resize {name}', function ($name) {
-    
     $presets = [
         ['w' => 1200, 'h' => null, 'op' => 'resize'],
         ['w' => 900, 'h' => null, 'op' => 'resize'],
@@ -70,19 +71,18 @@ Artisan::command('image:resize {name}', function ($name) {
         ['w' => 180, 'h' => null, 'op' => 'fit'],
         ['w' => 160, 'h' => null, 'op' => 'fit'], // 180
         ['w' => 80, 'h' => null, 'op' => 'fit'],  // 90
-        ['w' => 75, 'h' => null, 'op' => 'fit'], // 
+        ['w' => 75, 'h' => null, 'op' => 'fit'], //
         ['w' => 40, 'h' => null, 'op' => 'fit'], // 45
       //['w' => 420, 'h' => 260, 'op' => 'fit'],
     ];
 
-    collect($presets)->each(function($preset) use ($name) {
-        
+    collect($presets)->each(function ($preset) use ($name) {
         $image = Imageconv::make(storage_path('tmp/'.$name.'.jpeg'));
 
         $image->{$preset['op']}($preset['w'], $preset['h'], function ($constraint) {
             $constraint->aspectRatio();
         });
-        
+
         $image->save(filepath($name, $preset['w'], 75), 75);
 
         ImageOptimizer::optimize(
@@ -93,13 +93,11 @@ Artisan::command('image:resize {name}', function ($name) {
         // Imageconv::make(filepath($name, $preset['w'], 75, 'opt'))
         //     ->sharpen(3)
         //     ->save(filepath($name, $preset['w'], 75, 'opt','sharp'));
-
     });
-
 });
 /*
 Artisan::command('image:resize {from} {to=""} {width=""} {quality=100}', function ($from, $to, $width, $quality) {
-    
+
     $fromImage = Imageconv::make(storage_path('tmp/'.$from.'.jpeg'));
 
     $rows = [];
