53526d31dfb508a3b6e4e344652ea0549c89b261
kristjanjansen <kristjan.jansen@gmail.com>
2019-10-21 17:54:07 +0300

    save

---
+++ resources/views/components/OfferRow/OfferRow.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="OfferRow" :class="isclasses">{{ offer }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    isclasses: { default: "" },
+    offer: { default: {} }
+  },
+};
+</script>
