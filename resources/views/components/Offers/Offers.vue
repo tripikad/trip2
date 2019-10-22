4dfc7807b28dbcbe4202e4b2ba3091d44a81259a
kristjanjansen <kristjan.jansen@gmail.com>
2019-10-22 18:18:51 +0300

    adding destination filter

--- resources/views/components/Offers/Offers.vue
+++ resources/views/components/Offers/Offers.vue
@@ -2,6 +2,11 @@
     <div class="Offers" :class="isclasses">
         <div class="Offers__filters">
             <form-select placeholder="Company" :options="companies" v-model="activeCompany" />
+            <form-select
+                placeholder="Destination"
+                :options="destinations"
+                v-model="activeDestination"
+            />
         </div>
         <!--
         <form-select-multiple :options="options" v-model="activeOptions" />
@@ -23,34 +28,54 @@ export default {
     data: () => ({
         offers: [],
         id: '1TLEDlvDC_06gy75IhNAyXaUjt-9oOT2XOqW2LEpycHE',
-        options: [
-            { id: 1, name: 'hello' },
-            { id: 2, name: 'wold' }
-        ],
-        activeOptions: [],
-        activeCompany: -1
+        activeCompany: -1,
+        activeDestination: -1
     }),
     computed: {
         companies() {
             return unique(
                 this.offers.map(o => o.company)
-            ).map((name, id) => ({ id, name }))
+            ).map((name, id) => ({
+                id,
+                name
+            }))
+        },
+        destinations() {
+            return unique(
+                this.offers.map(o => o.destination)
+            ).map((name, id) => ({
+                id,
+                name
+            }))
         },
         filteredOffers() {
-            return this.offers.filter(o => {
-                //console.log(o.company, this.activeCompany)
-                if (this.activeCompany > -1) {
-                    return (
-                        o.company ==
-                        this.getById(
-                            this.companies,
-                            this.activeCompany,
-                            'name'
+            return this.offers
+                .filter(o => {
+                    if (this.activeCompany > -1) {
+                        return (
+                            o.company ==
+                            this.getById(
+                                this.companies,
+                                this.activeCompany,
+                                'name'
+                            )
                         )
-                    )
-                }
-                return true
-            })
+                    }
+                    return true
+                })
+                .filter(o => {
+                    if (this.activeDestination > -1) {
+                        return (
+                            o.destination ==
+                            this.getById(
+                                this.destinations,
+                                this.activeDestination,
+                                'name'
+                            )
+                        )
+                    }
+                    return true
+                })
         }
     },
     methods: {
