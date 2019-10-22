f65c5beef8f17f17bbd331b1915767da6041153f
kristjanjansen <kristjan.jansen@gmail.com>
2019-10-22 20:23:10 +0300

    adding sqlite db for testing

--- config/database.php
+++ config/database.php
@@ -83,6 +83,12 @@ return [
             'strict'    => false,
         ],
 
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+        ],
+
     ],
 
     /*
