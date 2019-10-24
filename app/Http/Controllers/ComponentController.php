9cabb34efa0c00d913c9d70d5a98c1a39b3ad194
kristjanjansen <kristjan.jansen@gmail.com>
2019-10-24 09:50:11 +0300

    components playground

--- app/Http/Controllers/ComponentController.php
+++ app/Http/Controllers/ComponentController.php
@@ -9,9 +9,7 @@ class ComponentController extends Controller
     public function components()
     {
         return collect(
-            Storage::disk('resources')->directories(
-                '/views/components'
-            )
+            Storage::disk('resources')->directories('/views/components')
         )->map(function ($dir) {
             return basename($dir);
         });
@@ -27,11 +25,7 @@ class ComponentController extends Controller
     public function isBladeComponent($c)
     {
         return Storage::disk('resources')->exists(
-            '/views/components/' .
-                $c .
-                '/' .
-                $c .
-                '.blade.php'
+            '/views/components/' . $c . '/' . $c . '.blade.php'
         );
     }
 
@@ -45,38 +39,9 @@ class ComponentController extends Controller
     public function componentCode($c)
     {
         return str_pad($c, 30, ' ') .
-            ($this->isBladeComponent($c)
-                ? 'blade   '
-                : '        ') .
-            ($this->isVueComponent($c)
-                ? '   vue'
-                : '      ') .
+            ($this->isBladeComponent($c) ? 'blade   ' : '        ') .
+            ($this->isVueComponent($c) ? '   vue' : '      ') .
             ($this->isCss($c) ? '   css' : '      ');
-    }
-
-    public function isFiltered($c)
-    {
-        return in_array($c, [
-            //'DestinationFacts',
-            //'ExperimentalGrid',
-            //'Form',
-            //'FormHorizontal',
-            //'ForumRowSmall',
-            //'Grid',
-            //'Grid2',
-            //'Grid3',
-            //'Grid4',
-            //'GridSplit',
-            //'Header',
-            //'HeaderLight',
-            //'HeaderTab',
-            //'HeaderTabs',
-            //'NewsCard',
-            //'Paginator',
-            //'PaginatorExtended',
-            //'PhotoResponsive',
-            //'PhotoRow'
-        ]);
     }
 
     public function index()
@@ -85,87 +50,55 @@ class ComponentController extends Controller
             ->with(
                 'content',
                 collect()
-                    ->push(
+                    ->pushWhen(!request()->has('c'),
                         component('Title')
                             ->is('large')
                             ->with('title', 'Components')
                     )
+                    ->pushWhen(
+                        !request()->has('list') && !request()->has('c'),
+                        component('Link')
+                            ->with('title', 'Show list')
+                            ->with('route', route('components', ['list']))
+                    )
+                    ->pushWhen(
+                        request()->has('list') && !request()->has('c'),
+                        component('Link')
+                            ->with('title', 'Show previews')
+                            ->with('route', route('components'))
+                    )
                     ->merge(
                         $this->components()
                             ->filter(function ($c) {
-                                return !starts_with(
-                                    $c,
-                                    'Aff'
-                                );
+                                if (request()->has('c')) {
+                                    return $c == request()->get('c');
+                                }
+                                return !starts_with($c, 'Aff');
                             })
                             ->map(function ($c) {
                                 return collect()
-                                    ->push('&nbsp;')
-                                    ->push(
+                                    ->pushWhen(!request()->has('c'),
                                         component('Code')
                                             ->is('gray')
                                             ->with(
+                                                'route',
+                                                route('components', ['c' => $c])
+                                            )
+                                            ->with(
                                                 'code',
-                                                $this->componentCode(
-                                                    $c
-                                                )
+                                                $this->componentCode($c)
                                             )
                                     )
                                     ->pushWhen(
-                                        !$this->isFiltered(
-                                            $c
-                                        ),
-                                        component($c)
-                                            ->with(
-                                                'title',
-                                                $c
-                                            )
-                                            // ->with(
-                                            //     'content',
-                                            //     $c
-                                            // )
-                                            // ->with(
-                                            //     'items',
-                                            //     []
-                                            // )
-                                            // ->with(
-                                            //     'facts',
-                                            //     []
-                                            // )
-                                            // ->with(
-                                            //     'fields',
-                                            //     []
-                                            // )
-                                            // ->with(
-                                            //     'meta',
-                                            //     ''
-                                            // )
-                                            // ->with(
-                                            //     'left_content',
-                                            //     []
-                                            // )
-                                            // ->with(
-                                            //     'right_content',
-                                            //     []
-                                            // )
-                                            
-                                            
+                                        !request()->has('list') ||
+                                            request()->has('c'),
+                                        component($c)->with('title', $c)
                                     )
                                     ->pushWhen(
-                                        !$this->isFiltered(
-                                            $c
-                                        ),
+                                        !request()->has('list'),
                                         component($c)
-                                            ->with(
-                                                'title',
-                                                $c
-                                            )
-                                            ->with(
-                                                'content',
-                                                ''
-                                            )
+                                            ->with('title', $c)
                                             ->vue()
-                                            
                                     );
                             })
                     )
--- resources/views/components/Code/Code.blade.php
+++ resources/views/components/Code/Code.blade.php
@@ -1,7 +1,17 @@
 @php
 
 $code = $code ?? '';
+$route = $route ?? '';
 
 @endphp
 
+
+@if($route)
+<a href="{{ $route }}">
+@endif
+
 <div class="Code {{ $isclasses }}">{{ $code }}</div>
+
+@if($route)
+</a>
+@endif
