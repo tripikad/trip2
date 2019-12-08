8c310c59157e288b48586f3ddf4fc2eacd931b06
kristjanjansen <kristjan.jansen@gmail.com>
2019-12-08 12:19:12 +0200

    fix

--- resources/views/vendor/mail/html/button.blade.php
+++ resources/views/vendor/mail/html/button.blade.php
@@ -1,4 +1,4 @@
-<table class="action {{ $is or '' }}" align="center" width="100%" cellpadding="0" cellspacing="0">
+<table class="action {{ $is ?? '' }}" align="center" width="100%" cellpadding="0" cellspacing="0">
     <tr>
         <td align="center">
             <table width="100%" border="0" cellpadding="0" cellspacing="0">
@@ -7,7 +7,7 @@
                         <table border="0" cellpadding="0" cellspacing="0">
                             <tr>
                                 <td>
-                                    <a href="{{ $url }}" class="button button-{{ $color or 'blue' }}" target="_blank">{{ $slot }}</a>
+                                    <a href="{{ $url }}" class="button button-{{ $color ?? 'blue' }}" target="_blank">{{ $slot }}</a>
                                 </td>
                             </tr>
                         </table>
