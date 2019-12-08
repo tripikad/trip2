745baf548e888c671afe0b7d960211b07f5ceedb
kristjanjansen <kristjan.jansen@gmail.com>
2019-12-08 18:43:36 +0200

    prune

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,6 +19,8 @@ class Kernel extends ConsoleKernel
 
         $schedule->command('search:index --optimize')->dailyAt('05:55');
 
+        $schedule->command('telescope:prune')->daily();
+
         //$schedule->command('newsletter:send --check-newsletters')
         //    ->cron('*/10 * * * *');
 
