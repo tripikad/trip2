0bcdd95f6b0d70d1aeb2d579699db8bf7e0398b3
kristjanjansen <kristjan.jansen@gmail.com>
2019-12-19 15:48:41 +0200

    improving meta tags

--- resources/views/utils/sharing.blade.php
+++ resources/views/utils/sharing.blade.php
@@ -1,7 +1,9 @@
 <title>{{ config('site.name') }} | @yield('title')</title>
 <meta name="description" content="@yield('head_description')">
 <meta property="og:title" content="@yield('head_title')">
+<meta property="twitter:title" content="@yield('head_title')">
 <meta property="og:description" content="@yield('head_description')">
+<meta property="twitter:description" content="@yield('head_description')">
 <meta property="og:image" content="@yield('head_image')">
 <meta property="og:image:width" content="@yield('head_image_width')">
 <meta property="og:image:height" content="@yield('head_image_height')">
