40d3ca8c8a59a1b60741371173b0dc021459335d
kristjanjansen <kristjan.jansen@gmail.com>
2020-01-12 16:34:01 +0100

    init

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -59,51 +59,10 @@ class OfferController extends Controller
             component('Section')
               ->withBackground('blue')
               ->withAlign('center')
-              ->withItems(
-                collect()
-                  ->spacer(8)
-                  ->push(
-                    component('Flex')->withItems(
-                      collect()
-                        ->push(
-                          component('Icon')
-                            ->is('white')
-                            ->withSize('xxl')
-                            ->withIcon('icon-pin')
-                        )
-                        ->push(
-                          component('Icon')
-                            ->is('white')
-                            ->withSize('xxl')
-                            ->withIcon('icon-tickets')
-                        )
-                        ->push(
-                          component('Icon')
-                            ->is('white')
-                            ->withSize('xxl')
-                            ->withIcon('icon-star')
-                        )
-                    )
-                  )
-                  ->spacer(2)
-                  ->push(
-                    component('Title')
-                      ->is('white')
-                      ->is('large')
-                      ->is('center')
-                      ->withTitle(trans('offer.index.company.title'))
-                  )
-                  ->spacer(2)
-                  ->push(
-                    component('Body')
-                      ->is('white')
-                      ->withBody(
-                        format_body(trans('offer.index.company.description', ['email' => config('offer.email')]))
-                      )
-                  )
-                  ->spacer(4)
-              )
+              ->withPadding(2)
+              ->withItems(component('Promo')->with('promo', 'body'))
           )
+          ->push(region('OfferCompanyPromo'))
           ->push(
             component('Section')
               ->withTag('footer')
@@ -310,6 +269,13 @@ class OfferController extends Controller
                   ->withTitle(trans('offer.show.goto'))
                   ->withRoute(route('booking.goto', [$id]))
               )
+          )
+          ->push(
+            component('Section')
+              ->withBackground('blue')
+              ->withAlign('center')
+              ->withPadding(2)
+              ->withItems(component('Promo')->with('promo', 'body'))
           )
           ->push(
             component('Section')
--- resources/lang/et/offer.php
+++ resources/lang/et/offer.php
@@ -3,7 +3,7 @@
 return [
   'index' => 'Reisipakkumised',
   'index.company.title' => 'Tahad siia oma reisifirma<br>pakkumisi lisada?',
-  'index.company.description' => 'Kirjuta :email ja me võtame sinuga ühendust.',
+  'index.company.description' => 'Kirjuta **:email** ja me võtame sinuga ühendust.',
   'show.back' => 'Kõik reisipakkumised',
   'show.book' => 'Broneeri reis Tripis',
   'show.book.title' => 'Broneeri reis',
