46b209c0813520bab3b784e3aeea1f02ea18b18c
kkallasm <kaspar68@gmail.com>
2020-05-19 23:27:29 +0300

    reisiparadiis sync

---
+++ database/migrations/2020_03_15_140554_create_ext_fields_to_offers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExtFieldsToOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('offers', function (Blueprint $table) {
+            $table->string('ext_id', 255)->nullable()->index();
+            $table->dateTime('ext_date_time')->nullable();
+
+            $table->unique(['ext_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('offers', function (Blueprint $table) {
+            $table->dropColumn(['ext_id', 'ext_date_time']);
+        });
+    }
+}
