f028faaf112e3570ad6833d406766a5b13486a18
kkallasm <kaspar68@gmail.com>
2021-02-11 22:15:11 +0200

    loading and live price search

---
+++ resources/views/components/Loading/Loading.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="Loading" v-if="visible">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        show: { default: false }
+    },
+
+    data() {
+        return {
+            visible: this.show
+        }
+    },
+    watch: {
+        show: function (show) {
+            this.visible = show
+        }
+    },
+}
+</script>
